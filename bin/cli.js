55189fcf04241d85a5dba3efd7c5dfce4a8df906
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-10-10 20:08:06 -0700

    Implemented add and delete with support for local and global config files

--- bin/cli.js
+++ bin/cli.js
@@ -1,46 +1,60 @@
 #!/usr/bin/env node
 
-var program = require("commander"),
-    config = require("./../lib/settings/config"),
-    createSite = require("./../lib/commands/create_site");
+var program    = require("commander"),
+    config     = require("./../lib/settings/config"),
+    createSite = require("./../lib/commands/create_site"),
+    deleteSite = require("./../lib/commands/delete_site"),
+    deploy     = require("./../lib/commands/deploy"),
+    publish    = require("./../lib/commands/publish"),
+    init       = require("./../lib/commands/init"),
+    updateSite = require("./../lib/commands/update_site");
 
 program
   .version("0.0.1")
-  .option("-t", "--access-token", "Override the default Access Token")
+  .option("-t --access-token <token>", "Override the default Access Token")
 
 program
   .command("init")
-  .description("Configure deployments for the current dir")
-  .action(function(cmd) {
-    console.log("initializing");
-  });
+  .description("Configure automated deployments for the current dir")
+  .action(config.wrap(program, init.cmd));
 
 program
   .command("create")
   .description("Create a new site")
-  .action(config.wrap(createSite.cmd))
+  .option("-n --name [name]", "Set <name>.netlify.com")
+  .option("-d --domain [domain]", "Set the custom domain for the site")
+  .option("-p --password [password]", "Set the password for the site")
+  .option("-w --write", "Save the site id in a local .netlify config file")
+  .action(config.wrap(program, createSite.cmd));
 
 program
   .command("update")
   .description("Updates attributes of a site")
-  .action(function(cmd) {
-    console.log("updating")
-  })
+  .option("-s --site-id [id]", "The site to update")
+  .option("-n --name [name]", "Set <name>.netlify.com")
+  .option("-d --domain [domain]", "Set the custom domain for the site")
+  .option("-p --password [password]", "Set the password for the site")
+  .action(config.wrap(program, updateSite.cmd));
 
 program
   .command("deploy [path]")
   .description("Deploy a new version")
-  .action(function(path, options) {
-    path = path || '.'
-    console.log("deploying %s", path)
-  });
+  .option("-s --site-id [id]", "Deploy to site with <id>")
+  .option("-d --draft", "Deploy as a draft without publishing")
+  .option("-w --write", "Save the site id in a local .netlify file")
+  .action(config.wrap(program, deploy.cmd));
+
+program
+  .command("publish <deploy_id>")
+  .description("Publish a specific deploy")
+  .action(config.wrap(program, publish.cmd));
 
 program
   .command("delete")
   .description("Deletes a site")
-  .action(function(cmd) {
-    console.log("deleting")
-  });
+  .option("-s --site-id [id]", "The id of the site to delete")
+  .option("-y --yes", "Don't prompt for confirmation")
+  .action(config.wrap(program, deleteSite.cmd));
 
 program.parse(process.argv);
 
