ae90448acae1cdaea173ddbfcf6caff3de01ecbd
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2018-06-21 23:50:44 -0700

    Add basic support for guessing yarn start/run/serve commands

--- src/utils/detect-server.js
+++ src/utils/detect-server.js
@@ -1,14 +1,41 @@
+const { existsSync, readFileSync } = require('fs')
+const { read } = require('qqjs')
+
 // Hardcoded for create react app right now, make this function look at cwd and find our from
 // heuristics like package.json, hugo config file, etc, what settings to return
 module.exports.serverSettings = () => {
-  const port = 8888
-  const proxyPort = 3000
-  return {
-    env: {...process.env, BROWSER: 'none', PORT: proxyPort},
-    proxyPort: proxyPort,
-    port: port,
-    cmd: 'yarn',
-    args: ['start'],
-    urlRegexp: new RegExp(`(http://)([^:]+:)${proxyPort}(/)?`, 'g')
+  let package = null;
+  if (existsSync('package.json')) {
+    package = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}))
   }
+
+  const settings = {
+    port: 8888,
+    proxyPort: 3000,
+    env: {...process.env, BROWSER: 'none', PORT: 3000},
+    args: [],
+    urlRegexp: new RegExp(`(http://)([^:]+:)${3000}(/)?`, 'g')
+  }
+
+  if (package) {
+    if (existsSync('yarn.lock')) {
+      settings.cmd = 'yarn'
+    } else {
+      settings.cmd = 'npm'
+      settings.args.push('run')
+    }
+
+    if (package.scripts.start) {
+      settings.args.push('start')
+    } else if (package.scripts.serve) {
+      settings.args.push('serve')
+    } else if (package.scripts.run) {
+      settings.args.push('run')
+    } else {
+      console.error('Couldn\'t determine the script to run. Use the -c flag.')
+      process.exit(1)
+    }
+  }
+
+  return settings;
 }
