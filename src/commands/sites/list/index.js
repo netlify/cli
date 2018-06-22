f1a449735dc591ffa4f2e80a9326ce5991879444
davidwells <david@inboundnow.com>
2018-06-22 16:23:45 -0700

    add siteList command

--- src/commands/sites/list/index.js
+++ src/commands/sites/list/index.js
@@ -1,19 +1,53 @@
-
-const { Command, flags } = require('@oclif/command')
+const netlify = require('netlifys_api_definition')
 const AsciiTable = require('ascii-table')
+const { Command, flags } = require('@oclif/command')
+const { CLIError } = require('@oclif/errors')
+
+const accessToken = process.env.NETLIFY_ACCESS_TOKEN // to be replaced by
+netlify.ApiClient.instance.authentications['netlifyAuth'].accessToken = accessToken;
+
+const api = new netlify.DefaultApi()
 
 class SitesListCommand extends Command {
   async run() {
-    const {flags} = this.parse(SitesListCommand)
-    const name = flags.name || 'world'
-    // this.log(`list sites`)
-    var table = new AsciiTable('Netlify Sites')
-    table.setHeading('Name', 'Url', 'id')
-    .addRow('my-site-xyz.netlify.com', 'my-custom-url.biz', '124123-ddhshs1212-1211')
-    .addRow('wowza.netlify.com', 'serious.biz', 'x3123-22345-1211')
-    .addRow('new-cli-docs.netlify.com', 'cli.netlify.com', 'weyhfd-hjjk-67533')
+    // const { flags } = this.parse(SitesListCommand)
 
-    console.log(table.toString())
+    // Temp check
+    if (!process.env.NETLIFY_ACCESS_TOKEN) {
+      throw new CLIError(`Please set NETLIFY_ACCESS_TOKEN in session
+
+Run the following command in your terminal:
+
+export NETLIFY_ACCESS_TOKEN=YourTokenHere`)
+    }
+
+    // Fetch all sites!
+    api.listSites(null, (err, sites) => {
+      if (err) {
+        throw new CLIError(err)
+      }
+
+      if (sites && sites.length) {
+        const logSites = sites.map((site) => {
+          return {
+            id: site.id,
+            name: site.name,
+            ssl_url: site.ssl_url
+          }
+        })
+
+        // Build a table out of sites
+        const table = new AsciiTable('Netlify Sites')
+        table.setHeading('Name', 'Url', 'id')
+
+        logSites.forEach((s) => {
+          table.addRow(s.name, s.ssl_url,  s.id)
+        })
+
+        // Log da sites
+        console.log(table.toString())
+      }
+    })
   }
 }
 
@@ -21,9 +55,5 @@ SitesListCommand.description = `list sites
 ...
 Extra documentation goes here
 `
-
-SitesListCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
-}
 
 module.exports = SitesListCommand
