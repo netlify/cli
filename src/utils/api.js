d02b03fcf14e720b7dea9023cf920f5eae360f8e
Bret Comnes <bcomnes@gmail.com>
2018-07-09 15:58:52 -0700

    Refactor api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 const NetlifysApiDefinition = require('netlifys_api_definition')
 const promisifyAll = require('util.promisify-all')
 const deploy = require('./deploy')
-const pWaitFor = require('p-wait-for')
-const pTimeout = require('p-timeout')
+const getAccessToken = require('access-token')
 
 class Netlify {
   constructor(accessToken) {
@@ -21,25 +20,13 @@ class Netlify {
   }
 
   async waitForAccessToken(ticket) {
-    const { id } = ticket
-
-    let authorizedTicket
-    await pTimeout(
-      pWaitFor(async () => {
-        const t = await this.api.showTicket(id)
-        if (t.authorized) authorizedTicket = t
-        return !!t.authorized
-      }, 1000), // poll every 1 second
-      3.6e6, // timeout after 1 hour
-      'Timeout while waiting for ticket grant'
-    )
-
-    const accessToken = await this.api.exchangeTicket(authorizedTicket.id)
+    const accessToken = await getAccessToken(this.api, ticket)
 
     // Update the API client with the access token
-    this.api.apiClient.authentications.netlifyAuth.accessToken = accessToken.access_token
-    this.accessToken = accessToken.access_token
-    return accessToken.access_token
+    this.api.apiClient.authentications.netlifyAuth.accessToken = accessToken
+    this.accessToken = accessToken
+
+    return accessToken
   }
 }
 
