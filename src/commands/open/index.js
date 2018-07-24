f9c942afe6f7548edcad0a4946f1541ee9128d9a
Bret Comnes <bcomnes@gmail.com>
2018-07-23 19:37:52 -0700

    Clean up more output

--- src/commands/open/index.js
+++ src/commands/open/index.js
@@ -1,20 +1,40 @@
-const { Command } = require('@oclif/command')
+const Command = require('../../base')
 const openBrowser = require('../../utils/open-browser')
 const renderShortDesc = require('../../utils/renderShortDescription')
 
 class OpenCommand extends Command {
   async run() {
     this.log(`Opening {SITE XYZ} admin in your default browser`)
-    openBrowser('https://app.netlify.com/sites/faunadb-example/overview')
+    const accessToken = this.global.get('accessToken')
+    const siteId = this.site.get('siteId')
+    if (!accessToken) {
+      this.error(`Not logged in. Log in to see site status.`)
+    }
+
+    if (!siteId) {
+      this.warn('Did you run `netlify link` yet?')
+      this.error(`You don't appear to be in a folder that is linked to a site`)
+    }
+
+    let site
+    try {
+      site = await this.netlify.api.getSite(siteId)
+    } catch (e) {
+      if (e.status === 401 /* unauthorized*/) {
+        this.warn(`Log in with a different account or re-link to a site you have permission for`)
+        this.error(`Not authorized to view the currently linked site (${siteId})`)
+      }
+      this.error(e)
+    }
+
+    openBrowser(site.admin_url)
     this.exit()
   }
 }
 
 OpenCommand.description = `${renderShortDesc('Opens current site admin UI in netlify')}`
 
-OpenCommand.examples = [
-  '$ netlify open',
-]
+OpenCommand.examples = ['$ netlify open']
 
 OpenCommand.hidden = true
 
--- src/commands/status/index.js
+++ src/commands/status/index.js
@@ -1,45 +1,46 @@
 const Command = require('../../base')
 const renderShortDesc = require('../../utils/renderShortDescription')
 const { CLIError } = require('@oclif/errors')
+const prettyjson = require('prettyjson')
 
 class StatusCommand extends Command {
   async run() {
     const accessToken = this.global.get('accessToken')
     const siteId = this.site.get('siteId')
+    let personal
     if (accessToken) {
       const accounts = await this.netlify.api.listAccountsForUser()
-      const personal = accounts.find(account => account.type === 'PERSONAL')
-      // TODO: make this better when we get the user endpoint
-      this.log(`Logged in as ${personal.name} (${personal.billing_email})`)
+      personal = accounts.find(account => account.type === 'PERSONAL')
+      // TODO: use users endpoint
     } else {
-      this.log(`Error: Not logged in. Log in to see site status.`)
-      this.exit()
+      this.error(`Not logged in. Log in to see site status.`)
     }
 
-    if (siteId) {
-      let site
-      try {
-        site = await this.netlify.api.getSite(siteId)
-      } catch (e) {
-        if (e.status === 401 /* unauthorized*/) {
-          this.log(`Error: Not authorized to view the currently linked site (${siteId})`)
-          this.log(`Log in with a different account or re-link to a site you have permission for`)
-          this.exit()
-        }
-        throw new CLIError(e)
+    if (!siteId) {
+      this.warn('Did you run `netlify link` yet?')
+      this.error(`You don't appear to be in a folder that is linked to a site`)
+    }
+
+    let site
+    try {
+      site = await this.netlify.api.getSite(siteId)
+    } catch (e) {
+      if (e.status === 401 /* unauthorized*/) {
+        this.warn(`Log in with a different account or re-link to a site you have permission for`)
+        this.error(`Not authorized to view the currently linked site (${siteId})`)
       }
-
-      this.log(`Current project linked to ${site.name} (${site.ssl_url})`)
-      this.log(`CLI Cache: ${this.site.path}`)
-      this.log(`Netlify TOML: ${this.site.tomlPath}`)
-      this.log(`Admin URL: ${site.admin_url}`)
-    } else {
-      this.log(`You don't appear to be in a folder that is linked to a site`)
-      this.log('Did you run `netlify link` yet?')
-      this.exit()
+      throw new CLIError(e)
     }
 
-    console.log(this.site.toml)
+    const statusData = {
+      'Logged in as': `${personal.name} (${personal.billing_email})`,
+      'Current project': `${site.name} (${site.ssl_url})`,
+      'CLI Cache': this.site.path,
+      'Netlify TOML': this.site.tomlPath,
+      'Admin URL': site.admin_url,
+      CWD: process.cwd()
+    }
+    this.log(prettyjson.render(statusData))
   }
 }
 
