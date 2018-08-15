6fcdb687b22c0e502f7ab305d2b2c471fc4d0184
DavidWells <hello@davidwells.io>
2018-08-15 12:03:29 -0700

    add account info to status

--- src/commands/status/index.js
+++ src/commands/status/index.js
@@ -1,15 +1,34 @@
 const Command = require('../../base')
 const renderShortDesc = require('../../utils/renderShortDescription')
 const prettyjson = require('prettyjson')
+const get = require('lodash.get')
+const clean = require('clean-deep')
 
 class StatusCommand extends Command {
   async run() {
     const accessToken = this.global.get('accessToken')
     const siteId = this.site.get('siteId')
+
     let personal
+    let accountData
     if (accessToken) {
       const accounts = await this.netlify.listAccountsForUser()
       personal = accounts.find(account => account.type === 'PERSONAL')
+      const teams = accounts.filter(account => account.type !== 'PERSONAL')
+      accountData = {
+        'Account name': get(personal, 'name'),
+        'Account slug': get(personal, 'slug'),
+        'Account id': get(personal, 'id'),
+        Name: get(personal, 'billing_name'),
+        Email: get(personal, 'billing_email')
+      }
+      const teamsData = {}
+
+      teams.forEach(team => {
+        return (teamsData[team.name] = team.roles_allowed.join(' '))
+      })
+
+      accountData.Teams = teamsData
       // TODO: use users endpoint
     } else {
       this.error(`Not logged in. Log in to see site status.`)
@@ -32,14 +51,22 @@ class StatusCommand extends Command {
     }
 
     const statusData = {
-      'Logged in as': `${personal.name} (${personal.billing_email})`,
       'Current project': `${site.name} (${site.ssl_url})`,
       'CLI Cache': this.site.path,
       'Netlify TOML': this.site.tomlPath,
       'Admin URL': site.admin_url,
       CWD: process.cwd()
     }
+    this.log(`──────────────────────┐
+ Current Netlify User │
+──────────────────────┘`)
+    this.log(prettyjson.render(clean(accountData)))
+    this.log(`────────────────────┐
+ Netlify Site Info  │
+────────────────────┘`)
+
     this.log(prettyjson.render(statusData))
+
   }
 }
 
