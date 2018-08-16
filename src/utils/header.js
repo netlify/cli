0432805ec49d7a4c685976af4517eb95daa3a8e0
Bret Comnes <bcomnes@gmail.com>
2018-08-16 16:38:42 -0700

    2.0.0-alpha.1

--- src/utils/header.js
+++ src/utils/header.js
@@ -2,11 +2,13 @@ const chalk = require('chalk')
 
 module.exports = function header(context) {
   const title = chalk.cyanBright.bold.underline('Netlify CLI')
+  const nextWarning = chalk.cyanBright.bold.underline('***ALPHA RELEASE: Not feature complete***')
   const docsMsg = `${chalk.greenBright('Read the docs:')} https://cli.netlify.com`
   const forumMsg = `${chalk.yellowBright('Discuss on the forums:')} https://gitter.netlify.com`
   if (!context.id) {
     console.log()
     console.log(`${title}`)
+    console.log(nextWarning)
     console.log(docsMsg)
     console.log(forumMsg)
     console.log()
