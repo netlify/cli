f2447c03cee843037ba66a994b2d3f2995aef919
Bret Comnes <bcomnes@gmail.com>
2018-08-28 09:52:34 -0700

    Walk the netlify.toml file before the deploy dir.

--- src/utils/api/deploy/hash-files.js
+++ src/utils/api/deploy/hash-files.js
@@ -14,7 +14,7 @@ async function hashFiles(dir, tomlPath, opts) {
   )
 
   if (!opts.filter) throw new Error('Missing filter function option')
-  const fileStream = walker([dir, tomlPath], { filter: opts.filter })
+  const fileStream = walker([tomlPath, dir], { filter: opts.filter })
   const filter = fileFilterCtor()
   const hasher = hasherCtor(opts)
   const fileNormalizer = fileNormalizerCtor(opts)
