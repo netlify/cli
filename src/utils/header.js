fbe75a5353ef0c10dfe06aced55a9a9511a672a6
Bret Comnes <bcomnes@gmail.com>
2018-08-28 15:39:10 -0700

    Fix Copy
    Closes https://github.com/netlify/cli/issues/56

--- src/utils/header.js
+++ src/utils/header.js
@@ -1,16 +1,19 @@
 const chalk = require('chalk')
+const pkg = require('../../package.json')
 
 module.exports = function header(context) {
   const title = chalk.cyanBright.bold.underline('Netlify CLI')
-  const nextWarning = chalk.redBright.bold('***ALPHA RELEASE**** (Not feature complete)')
+  const nextWarning = `${chalk.redBright.bold('****ALPHA RELEASE**** (Not feature complete)')}`
   const docsMsg = `${chalk.greenBright('Read the docs:')} https://cli.netlify.com`
   const forumMsg = `${chalk.yellowBright('Discuss on the forums:')} https://gitter.netlify.com`
+  const supportMsg = `${chalk.magentaBright('Support and bugs:')} ${pkg.bugs}`
   if (!context.id) {
     console.log()
-    console.log(`${title}`)
+    console.log(title)
     console.log(nextWarning)
     console.log(docsMsg)
     console.log(forumMsg)
+    console.log(supportMsg)
     console.log()
   }
 }
