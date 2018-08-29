4f4d25602ccd2ea7c238f6ca202ae681a17756c8
DavidWells <hello@davidwells.io>
2018-08-28 17:10:21 -0700

    update edit link

--- docs/_layout.js
+++ docs/_layout.js
@@ -68,16 +68,28 @@ export const MobileOnly = styled.div([], {
   },
 })
 
-export const MenuIcon = ({ size = 24, ...props }) =>
-  <svg
-    {...props}
-    viewBox='0 0 24 24'
-    width={size}
-    height={size}
-    fill='currentcolor'
-  >
-    <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
-  </svg>
+export const MenuIcon = ({ size = 24, ...props }) => {
+  return (
+    <svg
+      {...props}
+      viewBox='0 0 24 24'
+      width={size}
+      height={size}
+      fill='currentcolor'
+    >
+      <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
+    </svg>
+  )
+}
+
+
+const GithubIcon = () => {
+  return (
+    <svg aria-labelledby="simpleicons-github-icon" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
+    </svg>
+  )
+}
 
 export const Main = props =>
   <Box
@@ -160,11 +172,34 @@ const NavBar = ({
     {logo}
     <Heading
       px={2}
-      fontSize={1}>
+      fontSize={1}
+    >
       {title}
     </Heading>
     <Box mx='auto' />
   </Toolbar>
+
+
+  const EditLink = styled.div`
+    a {
+      display: flex;
+      align-items: center;
+    }
+    svg {
+      width: 18px;
+      height: 18px;
+      margin-left: 5px;
+      margin-bottom: 1px;
+      fill: #686868;
+    }
+    position: absolute;
+    right: 25px;
+    top: 25px;
+    @media (max-width: 768px) {
+      right: 25px;
+      top: 70px;
+    }
+  `;
 
 export const Nav = ({
   routes = [],
@@ -243,7 +278,8 @@ const MobileNav = ({
         m={0}
         mr='auto'
         title='Toggle Menu'
-        onClick={e => update(toggle('menu'))}>
+        onClick={e => update(toggle('menu'))}
+      >
         {logo || <MenuIcon />}
       </ButtonTransparent>
       <Heading fontSize={1}>
@@ -330,16 +366,20 @@ export default class Layout extends React.Component {
             />
           </Sidebar>
           <Main tabIndex={menu ? -1 : undefined}>
-            <div style={{ position: 'absolute', right: 20, top: 20 }}>
+            <EditLink>
               <a
                 style={{
                   color: '#333',
                   textDecoration: 'none',
                   fontSize: '12px'
                 }}
-                href={`${repoUrl}/edit/master/docs/${this.props.route.key}`}>
-                Edit the on github
+                href={`${repoUrl}/edit/master/docs/${this.props.route.key}`}
+              >
+                Edit this doc <GithubIcon/>
               </a>
+            </EditLink>
+            <div style={{ position: 'absolute', right: 20, top: 20 }}>
+
             </div>
             <Wrapper>
               <Content>
