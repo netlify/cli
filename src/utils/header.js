7d11b2d3a906f2de4ad3a5c966a815d570195500
Bret Comnes <bcomnes@gmail.com>
2018-08-31 16:41:45 -0700

    improve header output

--- src/utils/header.js
+++ src/utils/header.js
@@ -2,15 +2,13 @@ const chalk = require('chalk')
 const pkg = require('../../package.json')
 
 module.exports = function header(context) {
-  const title = chalk.cyanBright.bold.underline('Netlify CLI (BETA)')
+  const title = `${chalk.bgBlack.cyan('⬥ Netlify CLI')} ${chalk.red('(BETA)')}`
   const docsMsg = `${chalk.greenBright('Read the docs:')} https://cli.netlify.com`
-  const forumMsg = `${chalk.yellowBright('Discuss on the forums:')} https://gitter.netlify.com`
-  const supportMsg = `${chalk.magentaBright('Support and bugs:')} ${pkg.bugs}`
+  const supportMsg = `${chalk.magentaBright('Support and bugs:')} ${pkg.bugs.url}`
   if (!context.id) {
     console.log()
     console.log(title)
     console.log(docsMsg)
-    console.log(forumMsg)
     console.log(supportMsg)
     console.log()
   }
