6e2abcbcffc0c63d5ac30e111f6fa22e8393b544
DavidWells <hello@davidwells.io>
2018-08-31 15:08:22 -0700

    update sites list command

--- src/commands/sites/list.js
+++ src/commands/sites/list.js
@@ -1,5 +1,5 @@
-const AsciiTable = require('ascii-table')
 const { flags } = require('@oclif/command')
+const chalk = require('chalk')
 const Command = require('../../base')
 const renderShortDesc = require('../../utils/renderShortDescription')
 
@@ -12,33 +12,34 @@ class SitesListCommand extends Command {
 
     if (sites && sites.length) {
       const logSites = sites.map(site => {
+        //console.log('site', site)
         return {
           id: site.id,
           name: site.name,
-          ssl_url: site.ssl_url
+          ssl_url: site.ssl_url,
+          repo_url: site.build_settings.repo_url,
         }
       })
 
       // Json response for piping commands
       if (flags.json) {
         const redactedSites = sites.map(site => {
-          delete site.build_settings
+          delete site.build_settings.env
           return site
         })
         console.log(JSON.stringify(redactedSites, null, 2))
         return false
       }
 
-      // Build a table out of sites
-      const table = new AsciiTable('Netlify Sites')
-
-      table.setHeading('Name', 'Url', 'id')
+      console.log('Netlify Sites')
+      console.log(`──────────────────────────────────────────`)
 
       logSites.forEach(s => {
-        table.addRow(s.name, s.ssl_url, s.id)
+        console.log(`${chalk.greenBright(s.name)} (id: ${s.id})`)
+        console.log(`  ${chalk.whiteBright.bold('url:')}  ${chalk.yellowBright(s.ssl_url)}`)
+        console.log(`  ${chalk.whiteBright.bold('repo:')} ${chalk.white(s.repo_url)}`)
+        console.log(`──────────────────────────────────────────`)
       })
-      // Log da sites
-      console.log(table.toString())
     }
   }
 }
