033f87cc19a000cb1380a9c88e9304169f8e19c5
DavidWells <hello@davidwells.io>
2018-09-04 13:55:41 -0700

    update logs

--- src/utils/getRepoData.js
+++ src/utils/getRepoData.js
@@ -1,21 +1,35 @@
+const path = require('path')
+const findUp = require('find-up')
 const gitRepoInfo = require('git-repo-info')
 const parseGitRemote = require('parse-github-url')
 const gitRemoteOriginUrl = require('git-remote-origin-url')
 
 async function getRepoData() {
+  const cwd = process.cwd()
   let repo = {}
   try {
+
     const remoteUrl = await gitRemoteOriginUrl()
+    const gitDirectory = findUp.sync(['.git'], { cwd: cwd })
+    const baseGitPath = path.dirname(gitDirectory)
+
+    if (cwd !== baseGitPath) {
+      console.log(`git directory located in ${baseGitPath}`)
+      // TODO prompt for "is this the correct git remote"?
+      // If folder gitignored inside another git repo it could link to wrong repo.
+    }
+
     if (!remoteUrl) {
-      console.log('NO REPO')
+      console.log('NO REPO FOUND')
       // TODO throw here?
       return {}
     }
     const remoteData = parseGitRemote(remoteUrl)
     const repoData = gitRepoInfo()
 
-    // TODO refactor?
+    // TODO refactor shape
     repo = {
+      gitDirectoryPath: gitDirectory,
       remoteData: remoteData,
       repoData: repoData,
       repo_path: remoteData.path,
