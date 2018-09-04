477d5d892e137bbdcedc2618901b1bf6e4538044
DavidWells <hello@davidwells.io>
2018-09-04 10:59:20 -0700

    if block

--- src/utils/init/config-manual.js
+++ src/utils/init/config-manual.js
@@ -13,7 +13,10 @@ async function configManual(ctx, site, repo) {
       default: true
     }
   ])
-  if (!sshKeyAdded) ctx.exit()
+  
+  if (!sshKeyAdded) {
+    ctx.exit()
+  }
 
   repo.provider = 'manual'
   repo.deploy_key_id = key.id
