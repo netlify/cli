c84858ceb2430397a5752752dfe35fc55094b5c6
DavidWells <hello@davidwells.io>
2018-09-04 18:27:54 -0700

    remove gitter

--- src/utils/header.js
+++ src/utils/header.js
@@ -4,13 +4,11 @@ const pkg = require('../../package.json')
 module.exports = function header(context) {
   const title = chalk.cyanBright.bold.underline('Netlify CLI (BETA)')
   const docsMsg = `${chalk.greenBright('Read the docs:')} https://cli.netlify.com`
-  const forumMsg = `${chalk.yellowBright('Discuss on the forums:')} https://gitter.netlify.com`
   const supportMsg = `${chalk.magentaBright('Support and bugs:')} ${pkg.bugs}`
   if (!context.id) {
     console.log()
     console.log(title)
     console.log(docsMsg)
-    console.log(forumMsg)
     console.log(supportMsg)
     console.log()
   }
