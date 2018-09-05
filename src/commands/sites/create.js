84b6abe600bef5b3aef2d97da0e6091ae27dc872
DavidWells <hello@davidwells.io>
2018-09-04 18:11:15 -0700

    update logs and remove unused flags

--- src/commands/sites/create.js
+++ src/commands/sites/create.js
@@ -2,6 +2,7 @@ const { flags } = require('@oclif/command')
 const inquirer = require('inquirer')
 const isEmpty = require('lodash.isempty')
 const prettyjson = require('prettyjson')
+const chalk = require('chalk')
 const Command = require('../../base')
 const renderShortDesc = require('../../utils/renderShortDescription')
 
@@ -36,7 +37,9 @@ class SitesCreateCommand extends Command {
       delete results.accountSlug
 
       const site = await this.netlify.createSiteInTeam({ accountSlug, body: results })
-      this.log(`Site created`)
+      this.log()
+      this.log(chalk.greenBright.bold.underline(`Site Created`))
+      this.log()
       this.log(
         prettyjson.render({
           'Admin URL': site.admin_url,
@@ -55,26 +58,6 @@ SitesCreateCommand.flags = {
   name: flags.string({
     char: 'n',
     description: 'name of site'
-  }),
-  password: flags.string({
-    char: 'p',
-    description: 'password protect the site'
-  }),
-  'force-tls': flags.boolean({
-    char: 's',
-    description: 'force TLS connections'
-  }),
-  'session-id': flags.string({
-    char: 'i',
-    description: 'session ID for later site transfers'
-  }),
-  'account-slug': flags.string({
-    char: 'a',
-    description: 'account slug to create the site under'
-  }),
-  'custom-domain': flags.string({
-    char: 'c',
-    description: 'custom domain to use with the site'
   })
 }
 
