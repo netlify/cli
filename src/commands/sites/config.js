3d88301b16e4ff8e9318e0bc396d5a550dea0fd8
DavidWells <hello@davidwells.io>
2018-09-04 21:09:18 -0700

    rename site:config

---
+++ src/commands/sites/config.js
@@ -0,0 +1,25 @@
+const { Command, flags } = require('@oclif/command')
+
+class SitesConfigCommand extends Command {
+  async run() {
+    this.log(`update a site`)
+    this.log(`Implementation coming soon`)
+  }
+}
+
+SitesConfigCommand.description = `update a site
+...
+Extra documentation goes here
+`
+
+SitesConfigCommand.flags = {
+  name: flags.string({
+    char: 'n',
+    description: 'name to print'
+  }),
+}
+
+// TODO implement logic
+SitesConfigCommand.hidden = true
+
+module.exports = SitesConfigCommand
