74f94488e0660a361c4dfe1b993d24148e04af12
DavidWells <hello@davidwells.io>
2018-09-04 18:12:28 -0700

    update docs and flags

--- src/commands/link.js
+++ src/commands/link.js
@@ -1,47 +1,49 @@
 const Command = require('../base')
 const { flags } = require('@oclif/command')
 const renderShortDesc = require('../utils/renderShortDescription')
-const inquirer = require('inquirer')
 const path = require('path')
 const chalk = require('chalk')
-const getRepoData = require('../utils/getRepoData')
 const linkPrompt = require('../utils/link/link-by-prompt')
-const isEmpty = require('lodash.isempty')
+
 
 class LinkCommand extends Command {
   async run() {
     await this.authenticate()
     const { flags } = this.parse(LinkCommand)
     const siteId = this.site.get('siteId')
+    // const hasFlags = !isEmpty(flags)
+    let site
+    try {
+      site = await this.netlify.getSite({ siteId })
+    } catch (e) {
+      // silent api error
+    }
 
-    if (siteId && !flags.force) {
-      let siteInaccessible = false
-      let site
-      try {
-        site = await this.netlify.getSite({ siteId })
-      } catch (e) {
-        if (!e.ok) {
-          siteInaccessible = true
-        }
-      }
-      if (!siteInaccessible) {
-        this.log(`Site already linked to ${site.name}`)
-        this.log(`Link: ${site.admin_url}`)
-        this.log()
-        this.log(`To unlink this site, run: \`netlify unlink\``)
-        return this.exit()
-      }
+    // Site id is incorrect
+    if (siteId && !site) {
+      console.log(`No site "${siteId}" found in your netlify account.`)
+      console.log(`Please double check your siteID and which account you are logged into via \`netlify status\`.`)
+      return this.exit()
+    }
+
+    // If already linked to site. exit and prompt for unlink
+    if (site) {
+      this.log(`Site already linked to "${site.name}"`)
+      this.log(`Admin url: ${site.admin_url}`)
+      this.log()
+      this.log(`To unlink this site, run: ${chalk.cyanBright('netlify unlink')}`)
+      return this.exit()
     }
 
     if (flags.id) {
-      let site
       try {
         site = await this.netlify.getSite({ site_id: flags.id })
       } catch (e) {
         if (e.status === 404) {
           this.error(new Error(`Site id ${flags.id} not found`))
+        } else {
+          this.error(e)
         }
-        else this.error(e)
       }
       this.site.set('siteId', site.id)
       this.log(`Linked to ${site.name} in ${path.relative(path.join(process.cwd(), '..'), this.site.path)}`)
@@ -56,8 +58,11 @@ class LinkCommand extends Command {
           filter: 'all'
         })
       } catch (e) {
-        if (e.status === 404) this.error(new Error(`${flags.name} not found`))
-        else this.error(e)
+        if (e.status === 404) {
+          this.error(new Error(`${flags.name} not found`))
+        } else {
+          this.error(e)
+        }
       }
 
       if (results.length === 0) {
@@ -69,15 +74,17 @@ class LinkCommand extends Command {
       return this.exit()
     }
 
-    await linkPrompt(this)
+    site = await linkPrompt(this)
+    return site
   }
 }
 
 LinkCommand.description = `${renderShortDesc('Link a local repo or project folder to an existing site on Netlify')}`
 
 LinkCommand.examples = [
-  '$ netlify init --id 123-123-123-123',
-  '$ netlify init --name my-site-name'
+  'netlify link',
+  'netlify link --id 123-123-123-123',
+  'netlify link --name my-site-name'
 ]
 
 LinkCommand.flags = {
@@ -86,9 +93,6 @@ LinkCommand.flags = {
   }),
   name: flags.string({
     description: 'Name of site to link to'
-  }),
-  force: flags.boolean({
-    description: 'Force link a folder to a site, even if the folder is already linked'
   })
 }
 
