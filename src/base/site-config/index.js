0e498767239f83c7a1f75f63c6aeb30db8335218
DavidWells <hello@davidwells.io>
2018-09-06 19:07:55 -0700

    refactor site config

--- src/base/site-config/index.js
+++ src/base/site-config/index.js
@@ -1,132 +1,72 @@
-const findUp = require('find-up')
 const path = require('path')
-const makeDir = require('make-dir')
 const fs = require('fs')
-const writeFileAtomic = require('write-file-atomic')
 const dotProp = require('dot-prop')
 const TOML = require('@iarna/toml')
-
 const permissionError = "You don't have access to this file."
 
-class SiteConfig {
-  constructor(projectDir, opts) {
-    opts = Object.assign(
-      {
-        rootIndicators: ['.netlify', 'netlify.toml', '.git']
-      },
-      opts
-    )
-    const configPath = path.join('.netlify', 'config.json')
-    const rootIndicator = findUp.sync(opts.rootIndicators, { cwd: projectDir })
-    if (rootIndicator) {
-      const root = path.dirname(rootIndicator)
-      this.root = root
-      this.path = path.join(root, configPath)
-    } else {
-      this.root = projectDir
-      this.path = path.join(projectDir, configPath)
-    }
+function siteConfig(root, state) {
+  const configPath = getConfigPath(root)
+
+  const config = {
+    root: root,
+    configPath: configPath,
+    config: getConfigData(configPath),
+    get: (key) => {
+      if (key === 'siteId' || key === 'id') {
+        // Get ID from state
+        return state.get('siteId')
+      }
+      const currentConfig = siteConfig(root, state)
+      return dotProp.get(currentConfig, key)
+    },
+    // TODO set Set will need AST parser for yml support
   }
 
-  get toml() {
-    try {
-      return TOML.parse(fs.readFileSync(path.join(this.root, 'netlify.toml'), 'utf8'))
-    } catch (err) {
-      // Don't create if it doesn't exist
-      if (err.code === 'ENOENT') {
-        return {}
-      }
+  return config
+}
 
-      // Improve the message of permission errors
-      if (err.code === 'EACCES') {
-        err.message = `${err.message}\n${permissionError}\n`
-      }
-
-      throw err
-    }
+function getConfigData(configPath) {
+  // No config value. probably not in folder
+  if (!configPath) {
+    return {}
   }
 
-  get tomlPath() {
-    const p = path.join(this.root, 'netlify.toml')
-    try {
-      fs.readFileSync(path.join(this.root, 'netlify.toml'), 'utf8')
-      return p
-    } catch (_) {
-      return undefined
-    }
-  }
-
-  get all() {
-    try {
-      return JSON.parse(fs.readFileSync(this.path, 'utf8'))
-    } catch (err) {
-      // Don't create if it doesn't exist
-      if (err.code === 'ENOENT') {
-        return {}
-      }
-
-      // Improve the message of permission errors
-      if (err.code === 'EACCES') {
-        err.message = `${err.message}\n${permissionError}\n`
-      }
-
-      // Empty the file if it encounters invalid JSON
-      if (err.name === 'SyntaxError') {
-        writeFileAtomic.sync(this.path, '')
-        return {}
-      }
-
-      throw err
-    }
-  }
-
-  set all(val) {
-    try {
-      // Make sure the folder exists as it could have been deleted in the meantime
-      makeDir.sync(path.dirname(this.path))
-      writeFileAtomic.sync(this.path, JSON.stringify(val, null, '\t'))
-    } catch (err) {
-      // Improve the message of permission errors
-      if (err.code === 'EACCES') {
-        err.message = `${err.message}\n${permissionError}\n`
-      }
-
-      throw err
-    }
-  }
-
-  get size() {
-    return Object.keys(this.all || {}).length
-  }
-
-  get(key) {
-    return dotProp.get(this.all, key)
-  }
-  set(key, val) {
-    const config = this.all
-
-    if (arguments.length === 1) {
-      for (const k of Object.keys(key)) {
-        dotProp.set(config, k, key[k])
-      }
-    } else {
-      dotProp.set(config, key, val)
+  try {
+    // TODO support more formats
+    return TOML.parse(fs.readFileSync(configPath, 'utf8'))
+  } catch (err) {
+    // Don't create if it doesn't exist
+    if (err.code === 'ENOENT') {
+      return {}
     }
 
-    this.all = config
-  }
+    // Improve the message of permission errors
+    if (err.code === 'EACCES') {
+      err.message = `${err.message}\n${permissionError}\n`
+    }
 
-  has(key) {
-    return dotProp.has(this.all, key)
-  }
-  delete(key) {
-    const config = this.all
-    dotProp.delete(config, key)
-    this.all = config
-  }
-  clear() {
-    this.all = {}
+    throw err
   }
 }
 
-module.exports = SiteConfig
+function getConfigPath(root) {
+  // TODO support more formats
+  const tomlPath = path.join(root, 'netlify.toml')
+
+  if (fileExistsSync(tomlPath)) {
+    return tomlPath
+  }
+
+  return undefined
+}
+
+function fileExistsSync(filePath) {
+  try {
+    const stats = fs.lstatSync(filePath)
+    return stats.isFile()
+  } catch (e) {
+    return false
+  }
+}
+
+module.exports = siteConfig
