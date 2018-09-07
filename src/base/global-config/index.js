82dca478f238b7ddbd9029276f2d3ec8154bbcc0
DavidWells <hello@davidwells.io>
2018-09-07 15:04:47 -0700

    add cliId to global config

--- src/base/global-config/index.js
+++ src/base/global-config/index.js
@@ -1,11 +1,13 @@
 const Configstore = require('configstore')
 const os = require('os')
 const path = require('path')
-// const { toEnvCase, isDotProp } = require('./util')
+const uuidv4 = require('uuid/v4')
 
 const globalConfigDefaults = {
   /* disable stats from being sent to Netlify */
-  telemetryDisabled: false
+  telemetryDisabled: false,
+  /* cliId */
+  cliId: uuidv4()
 }
 
 const globalConfigOptions = {
@@ -13,21 +15,3 @@ const globalConfigOptions = {
 }
 
 module.exports = new Configstore(null, globalConfigDefaults, globalConfigOptions)
-
-/* disable proxy
-const envProxy = {}
-
-envProxy.get = (cs, prop) => {
-  if (prop === 'get') {
-    return key => {
-      if (isDotProp(key)) return cs.get(key)
-
-      return process.env[toEnvCase(key)] || cs.get(key)
-    }
-  }
-
-  return cs[prop]
-}
-
-const configStore = new Proxy(conf, envProxy)
-*/
