7dac3d232fbea3d4ae8adbd9be2b4d34ed097ac7
DavidWells <hello@davidwells.io>
2018-09-07 15:32:10 -0700

    update flag

--- src/hooks/init.js
+++ src/hooks/init.js
@@ -3,16 +3,16 @@ const globalConfig = require('../base/global-config')
 
 module.exports = async context => {
   // Enable/disable telemetry Global flags. TODO refactor where these fire
-  if (context.id === '--disable-telemetry') {
+  if (context.id === '--telemetry-disable') {
     globalConfig.set('telemetryDisabled', true)
     console.log('Netlify telemetry has been disabled')
-    console.log('You can renable it anytime with the --enable-telemetry flag')
+    console.log('You can renable it anytime with the --telemetry-enable flag')
     process.exit() // eslint-disable-line
   }
-  if (context.id === '--enable-telemetry') {
+  if (context.id === '--telemetry-enable') {
     globalConfig.set('telemetryDisabled', false)
     console.log('Netlify telemetry has been enabled')
-    console.log('You can disable it anytime with the --disable-telemetry flag')
+    console.log('You can disable it anytime with the --telemetry-disable flag')
     process.exit() // eslint-disable-line
   }
 
