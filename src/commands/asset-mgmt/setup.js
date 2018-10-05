d7f7c6e53d715a1a6ee681ab1a9d8d65dc242247
Keiko Oda <keiko713@gmail.com>
2018-10-05 12:15:53 -0700

    Introduce --skip-setup-repo flag

--- src/commands/asset-mgmt/setup.js
+++ src/commands/asset-mgmt/setup.js
@@ -1,4 +1,5 @@
 const Command = require('../../base')
+const { flags } = require('@oclif/command')
 const renderShortDesc = require('../../utils/renderShortDescription')
 const fs = require('fs')
 
@@ -7,39 +8,48 @@ const NETLIFY_GIT_LFS_SERVER_PATH = '/.netlify/functions/lfs'
 
 class AssetMgmtSetupCommand extends Command {
   async run() {
+    const { flags } = this.parse(AssetMgmtSetupCommand)
     const { api, site } = this.netlify
     const accessToken = this.getAuthToken()
+    let skipSetup = false
 
     if (!accessToken) {
       this.error(`Not logged in. Please run \`netlify login\` and try again`)
     }
 
-    const siteId = site.get('siteId')
-
-    if (!siteId) {
-      this.warn(`No Site ID found in current directory.
-Run \`netlify link\` to connect to this folder to a site`)
-      return false
+    if (flags['skip-setup-repo']) {
+      this.log('Setup Asset Management without setting up the repo')
+      skipSetup = true
     }
 
-    let siteData
-    try {
-      siteData = await api.getSite({ siteId })
-    } catch (e) {
-      console.log('e', e)
-      if (e.status === 401 /* unauthorized*/) {
-        this.warn(`Log in with a different account or re-link to a site you have permission for`)
-        this.error(`Not authorized to view the currently linked site (${siteId})`)
+    if (!skipSetup) {
+      const siteId = site.get('siteId')
+
+      if (!siteId) {
+        this.warn(`No Site ID found in current directory.
+  Run \`netlify link\` to connect to this folder to a site`)
+        return false
       }
-      if (e.status === 404 /* site not found */) {
-        this.log(`No site with id ${siteId} found.`)
-        this.log('Please double check this ID and verify you are logged in with the correct account')
-        this.log()
-        this.log('To fix this, run `netlify unlink` then `netlify link` to reconnect to the correct site ID')
-        this.log()
-        this.error(`Site (${siteId}) not found in account`)
+
+      let siteData
+      try {
+        siteData = await api.getSite({ siteId })
+      } catch (e) {
+        console.log('e', e)
+        if (e.status === 401 /* unauthorized*/) {
+          this.warn(`Log in with a different account or re-link to a site you have permission for`)
+          this.error(`Not authorized to view the currently linked site (${siteId})`)
+        }
+        if (e.status === 404 /* site not found */) {
+          this.log(`No site with id ${siteId} found.`)
+          this.log('Please double check this ID and verify you are logged in with the correct account')
+          this.log()
+          this.log('To fix this, run `netlify unlink` then `netlify link` to reconnect to the correct site ID')
+          this.log()
+          this.error(`Site (${siteId}) not found in account`)
+        }
+        this.error(e)
       }
-      this.error(e)
     }
 
     const exec = require('child_process').execFile
@@ -58,23 +68,25 @@ Run \`netlify link\` to connect to this folder to a site`)
       this.error('Please run the command in the git repository.', {exit: 1})
     }
 
-    if (!siteData.capabilities.asset_management) {
-      this.warn(`This site has not configured with Asset Management yet.
-Please visit admin UI and enable it first:
-> ${siteData.admin_url}`)
-      return false
-    }
-
-    this.log(`Setup Asset Management with site "${siteData.name}"`)
-
-    // .lfsconfig setup
-    this.log('Creating .lfsconfig file with the custom lfs url...')
-    const lfsUrl = `https://${siteData.id}:@${NETLIFY_GIT_LFS_SERVER_HOST}${NETLIFY_GIT_LFS_SERVER_PATH}`
-    exec('git', ['config', '-f', '.lfsconfig', 'lfs.url', lfsUrl], (error, stdout, stderr) => {
-      if (error) {
-        this.error('Failed to create .lfsconfig file.', {exit: 1})
+    if (!skipSetup) {
+      if (!siteData.capabilities.asset_management) {
+        this.warn(`This site has not configured with Asset Management yet.
+  Please visit admin UI and enable it first:
+  > ${siteData.admin_url}`)
+        return false
       }
-    })
+
+      this.log(`Setup Asset Management with site "${siteData.name}"`)
+
+      // .lfsconfig setup
+      this.log('Creating .lfsconfig file with the custom lfs url...')
+      const lfsUrl = `https://${siteData.id}:@${NETLIFY_GIT_LFS_SERVER_HOST}${NETLIFY_GIT_LFS_SERVER_PATH}`
+      exec('git', ['config', '-f', '.lfsconfig', 'lfs.url', lfsUrl], (error, stdout, stderr) => {
+        if (error) {
+          this.error('Failed to create .lfsconfig file.', {exit: 1})
+        }
+      })
+    }
 
     // global .gitconfig setup
     this.log('Setup global git config for the authentication with Netlify LFS server...')
@@ -87,13 +99,27 @@ Please visit admin UI and enable it first:
     })
 
     this.log('Asset Management setup completed successfully.')
-    this.log('Please make sure to git add .lfsconfig file.')
+    if (!skipSetup) {
+      this.log('Please make sure to git add .lfsconfig file.')
+      this.log('')
+      this.log('Pro tips 💡:')
+      this.log(' * Start tracking files using `git lfs track "*.jpg"`')
+      this.log(' * Migrate existing files using `git lfs migrate --everything`')
+      this.log('Do not forget!💡:')
+      this.log(' * Make sure to git add .lfsconfig and .gitattributes!')
+    }
     this.exit()
   }
 }
 
 AssetMgmtSetupCommand.description = `${renderShortDesc('Setup Netlify Asset Management with the repo')}`
 
-AssetMgmtSetupCommand.examples = ['netlify asset-mgmt:setup']
+AssetMgmtSetupCommand.examples = ['netlify asset-mgmt:setup --skip-setup-repo']
+
+AssetMgmtSetupCommand.flags = {
+  'skip-setup-repo': flags.boolean({
+    description: 'Setup without setting up the repo'
+  })
+}
 
 module.exports = AssetMgmtSetupCommand
