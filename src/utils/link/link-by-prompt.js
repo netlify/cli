a55774d64596e34ca86ffd4727d99f568f1d46c9
DavidWells <hello@davidwells.io>
2018-10-12 15:10:04 -0700

    update log

--- src/utils/link/link-by-prompt.js
+++ src/utils/link/link-by-prompt.js
@@ -199,7 +199,7 @@ Run ${chalk.cyanBright('`git remote -v`')} to see a list of your git remotes.`))
   context.log(`Site url:  ${chalk.cyanBright(site.ssl_url || site.url)}`)
   context.log()
 
-  context.log(`Site id saved to ${context.netlify.site.root}`)
+  context.log(`Site id saved to ${context.netlify.site.root}/.netlify/state.json`)
   // context.log(`Local Config: .netlify/config.json`)
   context.log()
   context.log(`You can now run other \`netlify\` cli commands in this directory`)
