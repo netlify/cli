e4fcbe8a9f66df2fd61f5cc86ca337f89cf4f478
Bret Comnes <bcomnes@gmail.com>
2018-12-04 17:43:48 -0800

    Check for updates more frequently

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const updateNotifier = require('update-notifier')
 const pkg = require('../package.json')
 
 try {
-  updateNotifier({ pkg }).notify()
+  updateNotifier({
+    pkg,
+    updateCheckInterval: 1000 * 60 * 60 * 12 // check every 1/2 day
+  }).notify()
 } catch (e) {
   console.log('Error checking for updates:')
   console.log(e)
