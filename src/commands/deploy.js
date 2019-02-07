0a4ea35ef20a47ddc8ea9da9862dc7c3c1078c59
Bret Comnes <bcomnes@gmail.com>
2019-02-07 15:19:26 -0800

    Try wrapping blocks for docs

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -213,9 +213,10 @@ Single file Node.js functions:
 
 Build dependency bundled Node.js lambda functions with tools like netlify-lambda, webpack or browserify into the function folder as part of your build process.
 
+\`\`\`
 functions/
 └── nameOfBundledNodeJSFunction.js
-
+\`\`\`
 
 Unbundled Node.js functions that have dependencies outside or inside of the functions folder:
 =============================================================================================
@@ -223,6 +224,7 @@ Unbundled Node.js functions that have dependencies outside or inside of the func
 You can ship unbundled Node.js functions with the CLI, utilizing top level project dependencies, or a nested package.json.
 If you use nested dependencies, be sure to populate the nested node_modules as part of your build process before deploying using npm or yarn.
 
+\`\`\`
 project/
 ├── functions
 |   ├── finctionName/
@@ -233,6 +235,7 @@ project/
 ├── package.json
 ├── netlify.toml
 └── node_modules/
+\`\`\`
 
 Any mix of these configurations works as well.
 
@@ -242,12 +245,13 @@ Node.js function entry points
 
 Function entry points are determined by the file name and name of the folder they are in:
 
+\`\`\`
 functions/
 ├── aFolderlessFunctionEntrypoint.js
 └── functionName/
     ├── notTheEntryPoint.js
     └── functionName.js
-
+\`\`\`
 
 Support for package.json's main field, and intrinsic index.js entrypoints are coming soon.
 `
