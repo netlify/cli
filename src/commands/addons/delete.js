9aa545584b98597c0eaa2233661885fbb25ed7dc
DavidWells <hello@davidwells.io>
2019-03-12 15:57:58 -0700

    only show deletion successful on 204 status

--- src/commands/addons/delete.js
+++ src/commands/addons/delete.js
@@ -47,13 +47,20 @@ class AddonsDeleteCommand extends Command {
       addon: addonName,
       instanceId: currentAddon.id
     }
+    // TODO update deleteAddon to https://open-api.netlify.com/#/default/deleteServiceInstance
     const addonResponse = await deleteAddon(settings, accessToken)
 
-    if (addonResponse.code === 404) {
+    if (addonResponse.status === 404) {
       this.log(`No addon "${addonName}" found. Please double check your add-on name and try again`)
       return false
     }
-    this.log(`Addon "${addonName}" deleted`)
+
+    /* Deleting addons must return with 204 status */
+    if (addonResponse.status === 204) {
+      this.log(`Addon "${addonName}" deleted`)
+    } else {
+      this.log(`Addon "${addonName}" was not deleted "${addonName}". Returned status: ${addonResponse.status}. Addon deletion must return status 204 from "${addonName}" provider.`)
+    }
   }
 }
 
