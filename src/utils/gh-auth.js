73904695dc5155931477dcc22fca0c0b018b7ccc
Raees Iqbal <raees.bhatti@gmail.com>
2019-07-09 07:10:10 +0500

    Add Github login through app.netlify.com

--- src/utils/gh-auth.js
+++ src/utils/gh-auth.js
@@ -1,11 +1,15 @@
 // A simple ghauth inspired library for getting a personal access token
 const Octokit = require('@octokit/rest')
 const inquirer = require('inquirer')
+const querystring = require('querystring')
 const get = require('lodash.get')
+const open = require('open')
+const http = require('http')
+const getPort = require('get-port')
 
-module.exports = createGithubPAT
+module.exports = getGitHubToken
 
-async function createGithubPAT(opts) {
+async function getGitHubToken(opts) {
   opts = Object.assign(
     {
       userAgent: 'Netlify-cli-octokit',
@@ -15,61 +19,116 @@ async function createGithubPAT(opts) {
     opts
   )
 
-  const { username, password } = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'username',
-      message: 'Your GitHub username:',
-      filter: input => input.trim()
-    },
-    {
-      type: 'password',
-      name: 'password',
-      message: 'Your GitHub password:',
-      mask: '*',
-      filter: input => input.trim()
-    }
-  ])
-
-
-  async function promptForOTP () {
+  async function promptForOTP() {
     const { otp } = await inquirer.prompt([
       {
         type: 'input',
         name: 'otp',
         message: 'Your GitHub OTP/2FA Code:',
-        filter: input => input.trim()
-      }
+        filter: input => input.trim(),
+      },
     ])
     return otp
   }
 
+  const authChoiceNetlify = 'Authorize with GitHub through app.netlify.com'
+  const authChoiceManual = 'Enter your GitHub credentials manually'
+  const authChoices = [authChoiceNetlify, authChoiceManual]
 
-  // configure basic auth
-  const octokit = new Octokit ({
-    auth: {
-      username,
-      password,
-      async on2fa () {
-        return promptForOTP()
+  const { initChoice } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'initChoice',
+      message: 'Netlify CLI needs access to your GitHub account to configure Webhooks and Deploy Keys. ' +
+        'What would you like to do?',
+      choices: authChoices,
+    },
+  ])
+
+  if (initChoice === authChoiceNetlify) {
+    const port = await getPort({ port: 3000 })
+    let deferredResolve
+    let deferredReject
+    const deferredPromise = new Promise(function (resolve, reject) {
+      deferredResolve = resolve
+      deferredReject = reject
+    })
+
+    const server = http.createServer(function (req, res) {
+      const parameters = querystring.parse(req.url.slice(req.url.indexOf('?') + 1))
+      if (parameters.token) {
+        deferredResolve(parameters)
+        res.end('OK')
+        server.close()
+        return
       }
-    }
-  })
+      res.end('BAD PARAMETERS')
+      server.close()
+      deferredReject(new Error('Got invalid parameters for CLI login'))
+    })
 
-  let response = await octokit.oauthAuthorizations.createAuthorization({
-    note: opts.note + ' (' + new Date().toJSON() + ')',
-    note_url: 'https://cli.netlify.com/',
-    scopes: opts.scopes,
-    headers: {
-      'User-Agent': opts.userAgent
-    }
-  })
+    await new Promise(function (resolve, reject) {
+      server.on('error', reject)
+      server.listen(port, resolve)
+    })
 
-  if (get(response, 'data.token')) {
-    return { user: username, token: get(response, 'data.token') }
+    const url = 'http://localhost:8080/cli?' + querystring.encode({
+      host: 'http://localhost:' + port,
+      provider: 'github',
+    })
+
+    try {
+      await open(url)
+    } catch (err) {
+      console.log('Netlify CLI could not open the browser for you.' +
+        ' Please visit this URL in a browser on this device: ' + url)
+    }
+
+    return await deferredPromise
   } else {
-    const error = new Error('Github authentication failed')
-    error.response = response
-    throw error
+    const { username, password } = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Your GitHub username:',
+        filter: input => input.trim(),
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Your GitHub password:',
+        mask: '*',
+        filter: input => input.trim(),
+      },
+    ])
+
+
+    // configure basic auth
+    const octokit = new Octokit({
+      auth: {
+        username,
+        password,
+        async on2fa() {
+          return promptForOTP()
+        },
+      },
+    })
+
+    let response = await octokit.oauthAuthorizations.createAuthorization({
+      note: opts.note + ' (' + new Date().toJSON() + ')',
+      note_url: 'https://cli.netlify.com/',
+      scopes: opts.scopes,
+      headers: {
+        'User-Agent': opts.userAgent,
+      },
+    })
+
+    if (get(response, 'data.token')) {
+      return { user: username, token: get(response, 'data.token') }
+    } else {
+      const error = new Error('Github authentication failed')
+      error.response = response
+      throw error
+    }
   }
 }
