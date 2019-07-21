41edece7c523c196b5294405d7622bd0d50108cb
Raees Iqbal <raees.bhatti@gmail.com>
2019-07-21 13:29:23 +0500

    Add postinstall message

---
+++ scripts/postinstall.js
@@ -0,0 +1,11 @@
+const chalk = require('chalk')
+
+console.log('')
+console.log(chalk.greenBright.bold.underline(`Success! Netlify CLI has been installed!`))
+console.log('')
+console.log('Your device is now configured to use Netlify CLI to deploy and manage your Netlify sites.')
+console.log('Next steps:')
+console.log('')
+console.log(`  ${chalk.cyanBright.bold('netlify init')}     Connect or create a Netlify site from current directory`)
+console.log(`  ${chalk.cyanBright.bold('netlify deploy')}   Deploy the latest changes to your Netlify site`)
+console.log('')
