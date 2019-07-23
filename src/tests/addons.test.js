be177be04bd227a67af5769cccc27480076cf812
Raees Iqbal <raees.bhatti@gmail.com>
2019-07-23 17:57:55 +0400

    Make addons tests work

--- src/tests/addons.test.js
+++ src/tests/addons.test.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const test = require('ava')
+const stripAnsi = require('strip-ansi')
 const cliPath = require('./utils/cliPath')
 const exec = require('./utils/exec')
 const sitePath = path.join(__dirname, 'dummy-site')
@@ -7,42 +8,52 @@ const sitePath = path.join(__dirname, 'dummy-site')
 const execOptions = {
   stdio: [0, 1, 2],
   cwd: sitePath,
-  env: {
-    ...process.env,
+  env: Object.assign(process.env, {
     NETLIFY_AUTH_TOKEN: process.env.NETLIFY_AUTH_TOKEN,
-    NETLIFY_SITE_ID: process.env.NETLIFY_SITE_ID
-  },
+  }),
 }
+
+const siteName = 'netlify-test-' + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 8)
 
 async function deleteAddon(name) {
   const cliResponse = await exec(`${cliPath} addons:delete ${name}`, execOptions)
   return cliResponse
 }
 
-// test.before(t => {
-//   // create site
-// })
+test.before(async t => {
+  console.log('creating new site for tests: ' + siteName)
+  const cliResponse = await exec(`${cliPath} sites:create --name="${siteName}" --account-slug="raeesbhatti"`, execOptions)
+  t.is(/Site Created/.test(cliResponse.stdout), true)
 
-test.serial('netlify addon:list', async (t) => {
-  const regex = /^No addons currently installed/
+  const matches = /Site ID:\s+([a-zA-Z0-9-]+)/m.exec(stripAnsi(cliResponse.stdout))
+  t.truthy(matches)
+  t.truthy(matches.hasOwnProperty(1))
+  t.truthy(matches[1])
+
+  // Set the site id
+  execOptions.env.NETLIFY_SITE_ID = matches[1]
+})
+
+test.serial('netlify addons:list', async (t) => {
+  const regex = /No addons currently installed/
   const cliResponse = await exec(`${cliPath} addons:list`, execOptions)
   t.is(regex.test(cliResponse.stdout), true)
 })
 
-test.serial('netlify addon:list --json', async (t) => {
+test.serial('netlify addons:list --json', async (t) => {
   const cliResponse = await exec(`${cliPath} addons:list --json`, execOptions)
   const json = JSON.parse(cliResponse.stdout)
   t.is(Array.isArray(json), true)
   t.is(json.length, 0)
 })
 
-test.serial('netlify addon:create demo', async (t) => {
+test.serial('netlify addons:create demo', async (t) => {
   const regex = /Add-on "demo" created/
   const cliResponse = await exec(`${cliPath} addons:create demo --TWILIO_ACCOUNT_SID lol`, execOptions)
   t.is(regex.test(cliResponse.stdout), true)
 })
 
-test.serial('After creation netlify addon:list --json', async (t) => {
+test.serial('After creation netlify addons:list --json', async (t) => {
   const cliResponse = await exec(`${cliPath} addons:list --json`, execOptions)
   const json = JSON.parse(cliResponse.stdout)
   t.is(Array.isArray(json), true)
@@ -60,4 +71,6 @@ test.after('cleanup', async t => {
   console.log('Performing cleanup')
   // Run cleanup
   await deleteAddon('demo')
+  console.log('deleting test site: '+ siteName)
+  await exec(`${cliPath} sites:delete ${siteName}`, execOptions)
 })
