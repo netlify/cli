c95576e1492cb68bef992ec4a161b9c7d2b103d2
swyx <shawnthe1@gmail.com>
2019-09-11 14:22:56 -0400

    Update token-hider.js

--- src/functions-templates/js/token-hider/token-hider.js
+++ src/functions-templates/js/token-hider/token-hider.js
@@ -5,21 +5,22 @@ exports.handler = async function(event, context) {
   // apply our function to the queryStringParameters and assign it to a variable
   const API_PARAMS = qs.stringify(event.queryStringParameters)
   // Get env var values defined in our Netlify site UI
-  const { API_SECRET = "shiba" } = process.env // TODO: change this
-  // In this example, the API Key needs to be passed in the params with a key of key.
-  // We're assuming that the ApiParams var will contain the initial ?
+  
+  // TODO: customize your URL and API keys set in the Netlify Dashboard
+  // this is secret too, your frontend won't see this
+  const { API_SECRET = "shiba" } = process.env
   const URL = `https://dog.ceo/api/breed/${API_SECRET}/images`
 
-  // // optional restriction to GET calls
-  // if (event.httpMethod !== "GET") return {statusCode: 404}
-  // Let's log some stuff we already have.
   console.log("Constructed URL is ...", URL)
 
   try {
     const { data } = await axios.get(URL)
+    // refer to axios docs for other methods if you need them
+    // for example if you want to POST data:
+    //    axios.post('/user', { firstName: 'Fred' })
     return {
       statusCode: 200,
-      body: JSON.stringify({ data }),
+      body: JSON.stringify(data),
     }
   } catch (error) {
     const { status, statusText, headers, data } = error.response
