636afe976c2f27f7e43ea431f8e32a111ad1ccc8
Tim Perry <pimterry@gmail.com>
2019-10-10 18:07:46 +0200

    Fix tests when run in a path containing spaces

--- src/tests/utils/cliPath.js
+++ src/tests/utils/cliPath.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
-const cliPath = path.join(__dirname, '..', '..', '..', 'bin/run')
+// Quoted so that commands work in paths with spaces
+const cliPath = '"' + path.join(__dirname, '..', '..', '..', 'bin/run') + '"'
 
 module.exports = cliPath
