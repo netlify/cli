1a1c7f8a03918d8ca323b4cafa6b9b13f7fa132b
DavidWells <hello@davidwells.io>
2019-11-19 14:35:19 -0800

    fix: dont run sync FS reads for everything

--- src/utils/detect-functions-builder.js
+++ src/utils/detect-functions-builder.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
-const detectors = require('fs')
-  .readdirSync(path.join(__dirname, '..', 'function-builder-detectors'))
-  .filter(x => x.endsWith('.js')) // only accept .js detector files
-  .map(det => require(path.join(__dirname, '..', `function-builder-detectors/${det}`)))
-
 module.exports.detectFunctionsBuilder = function() {
+  const detectors = require('fs')
+    .readdirSync(path.join(__dirname, '..', 'function-builder-detectors'))
+    .filter(x => x.endsWith('.js')) // only accept .js detector files
+    .map(det => require(path.join(__dirname, '..', `function-builder-detectors/${det}`)))
+
   for (const i in detectors) {
     const settings = detectors[i]()
     if (settings) {
