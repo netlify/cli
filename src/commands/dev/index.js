85e493591cbbde91f65576d9f82227b73721ca15
swyx <shawnthe1@gmail.com>
2019-12-10 15:06:09 -0500

    Update index.js

--- src/commands/dev/index.js
+++ src/commands/dev/index.js
@@ -338,7 +338,7 @@ class DevCommand extends Command {
     let settings = await serverSettings(Object.assign({}, config.dev, flags))
     
     if (!settings.proxyPort) {
-      settings.proxyPort = await getPort({ port: 3999 }) // in case detector is bypassed
+      settings.proxyPort = config.dev.proxyPort || 8080 // in case detector is bypassed
     }
     
     if (flags.dir || !(settings && settings.command)) {
