b4aa492cb2d7d75fae84a71800ebbe66290e3697
DavidWells <hello@davidwells.io>
2019-12-11 16:40:41 -0800

    docs: update site sync script. Update paths for site build

--- site/sync.js
+++ site/sync.js
@@ -1,14 +1,55 @@
 const path = require('path')
 const fs = require('fs-extra')
 const config = require('./config')
+const { promisify } = require('util')
+const readdirP = promisify(fs.readdir)
+const statP = promisify(fs.stat)
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+const deleteFile = promisify(fs.unlink)
 
-function syncLocalContent() {
+async function readDir(dir, allFiles = []) {
+  const files = (await readdirP(dir)).map(f => path.join(dir, f))
+  allFiles.push(...files)
+  await Promise.all(
+    files.map(
+      async f => (await statP(f)).isDirectory() && readDir(f, allFiles)
+    )
+  )
+  return allFiles
+}
+
+async function syncLocalContent() {
   const src = path.join(config.docs.srcPath)
   const destination = path.join(config.docs.outputPath)
 
-  return fs.copy(src, destination).then(() => {
-    console.log(`Docs synced to ${destination}`)
+  await fs.copy(src, destination)
+  console.log(`Docs synced to ${destination}`)
+
+  const files = await readDir(destination)
+  const mdFiles = files.filter((file) => {
+    return file.endsWith('.md')
+  }).map((path) => {
+    return removeMarkDownLinks(path)
   })
+
+  await Promise.all(mdFiles)
+}
+
+async function removeMarkDownLinks(filePath) {
+  const content = await readFile(filePath, 'utf-8')
+  const newContent = content.replace(/\.md#/gm, '#').replace(/\/docs\/commands\//gm, '/commands/')
+  // Rename README.md to index.md
+  if (path.basename(filePath) === 'README.md') {
+    const newPath = path.join(path.dirname(filePath), 'index.md')
+    // Delete README.md from docs site
+    await deleteFile(filePath)
+    // Write index.md
+    await writeFile(newPath, newContent)
+    return newPath
+  }
+  await writeFile(filePath, newContent)
+  return filePath
 }
 
 syncLocalContent().then(() => {
