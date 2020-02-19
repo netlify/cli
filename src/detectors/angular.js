be85e0d6e6355886c0010e5343104e00061063f5
Tara Z. Manicsic <tzmanics@gmail.com>
2020-02-19 12:06:21 -0500

    adds angular detector

---
+++ src/detectors/angular.js
@@ -0,0 +1,32 @@
+const { hasRequiredDeps, hasRequiredFiles, getYarnOrNPMCommand, scanScripts } = require('./utils/jsdetect')
+
+module.exports = function() {
+  // REQUIRED FILES
+  if (!hasRequiredFiles(['package.json'])) return false
+  if (!hasRequiredFiles(['angular.json'])) return false
+  // REQUIRED DEPS
+  if (!hasRequiredDeps(['@angular/cli'])) return false
+
+  /** everything below now assumes that we are within angular */
+
+  const possibleArgsArrs = scanScripts({
+    preferredScriptsArr: ['serve', 'start', 'build'],
+    preferredCommand: 'ng build --prod'
+  })
+
+  if (possibleArgsArrs.length === 0) {
+    // offer to run it when the user does not have any scripts setup! 🤯
+    possibleArgsArrs.push(['ng', 'build', '--prod'])
+  }
+
+  return {
+    type: '@angular/cli',
+    command: getYarnOrNPMCommand(),
+    port: 8888,
+    proxyPort: 4200,
+    env: { ...process.env },
+    possibleArgsArrs,
+    urlRegexp: new RegExp(`(http://)([^:]+:)${4200}(/)?`, 'g'),
+    dist: 'dist'
+  }
+}
