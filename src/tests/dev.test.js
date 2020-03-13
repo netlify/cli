978140711b7b1b510ceea549731f430fc580fc1a
Raees Iqbal <raees.bhatti@gmail.com>
2020-03-13 11:14:00 -0700

    🐛 Fix a loop for setting env vars (#748)
    * :bug: Fix a loop for setting env vars
    
    * Tests: Add funcitons dir in dummy site
    
    * Add tests for .env file

--- src/tests/dev.test.js
+++ src/tests/dev.test.js
@@ -15,7 +15,7 @@ test.before(async t => {
     ['dev'],
     {
       cwd: sitePath,
-      env: Object.assign({}, process.env),
+      env: Object.assign({}, process.env, { DUMMY_VAR: "true" }),
       detached: true,
       shell: true,
     }
@@ -46,7 +46,7 @@ test.before(async t => {
   )
 })
 
-test.serial('netlify dev functions timeout', async t => {
+test('netlify dev functions timeout', async t => {
   return new Promise((resolve, reject) => {
     const req = http.request({
       hostname: host,
@@ -62,6 +62,49 @@ test.serial('netlify dev functions timeout', async t => {
 
     req.end()
   })
+})
+
+test('netlify dev env file', async t => {
+  let data = ""
+  await new Promise((resolve, reject) => {
+    const req = http.request({
+      hostname: host,
+      port: port,
+      path: '/.netlify/functions/env',
+      method: 'GET',
+    }, (res) =>
+    {
+      res.on('data', (d) => {data += d.toString()})
+      res.on('end', resolve)
+    })
+    req.on('error', reject)
+
+    req.end()
+  })
+
+  t.is(data, "true")
+})
+
+
+test('netlify dev env file overriding prod var', async t => {
+  let data = ""
+  await new Promise((resolve, reject) => {
+    const req = http.request({
+      hostname: host,
+      port: port,
+      path: '/.netlify/functions/override-process-env',
+      method: 'GET',
+    }, (res) =>
+    {
+      res.on('data', (d) => {data += d.toString()})
+      res.on('end', resolve)
+    })
+    req.on('error', reject)
+
+    req.end()
+  })
+
+  t.is(data, "false")
 })
 
 test.after('cleanup', async t => {
