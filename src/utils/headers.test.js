fa86a6179419bf38022f4bcc36643b758d059736
Raees Iqbal <raees.bhatti@gmail.com>
2020-03-24 11:28:20 -0700

    Add support for _headers files (#754)
    * Rules Proxy: Export onChange function
    
    * _headers: Add function parseHeadersFile
    
    * Netlify Dev: Add support for _headers file
    
    * Var rename
    
    * Dummy site: Add _headers file
    
    * _headers: Add basic test
    
    * _headers: Parse file in dist afterwards
    
    * parseHeadersFile: Improve an error
    
    * parseHeadersFile: Trim header key as well
    
    * parseHeadersFile: Error if rule path has something after *
    
    * parseHeadersFile: Throw error if no path set
    
    * headers: Add new func matchPaths
    
    * headers: Add new func objectForPath
    
    * headers: Export parseHeadersFile and objectForPath
    
    * headers: Update tests

---
+++ src/utils/headers.test.js
@@ -0,0 +1,80 @@
+const test = require('ava')
+const path = require('path')
+const { parseHeadersFile, objectForPath }  = require('./headers.js')
+
+test('_headers: validate correct parsing', t => {
+    const sitePath = path.join(__dirname, '../tests/dummy-site')
+    const rules = parseHeadersFile(path.resolve(sitePath, '_headers'))
+
+    t.deepEqual(rules, {
+        "/":{
+            "X-Frame-Options":[
+                "SAMEORIGIN"
+            ]
+        },
+        "/*":{
+            "X-Frame-Thing":[
+                "SAMEORIGIN"
+            ]
+        },
+        "/something/*":{
+            "X-Frame-Options":[
+                "DENY"
+            ],
+            "X-XSS-Protection":[
+                "1; mode=block"
+            ],
+            "cache-control":[
+                "max-age=0",
+                "no-cache",
+                "no-store",
+                "must-revalidate"
+            ]
+        },
+        "/:ding/index.html":{
+            "X-Frame-Options":[
+                "SAMEORIGIN"
+            ]
+        }
+    })
+})
+
+test('_headers: rulesForPath testing', t => {
+    const sitePath = path.join(__dirname, '../tests/dummy-site')
+    const rules = parseHeadersFile(path.resolve(sitePath, '_headers'))
+    t.deepEqual(objectForPath(rules, '/'),  {
+        "X-Frame-Options":[
+            "SAMEORIGIN"
+        ]
+    })
+    t.deepEqual(objectForPath(rules, '/ding'),  {
+        "X-Frame-Thing":[
+            "SAMEORIGIN"
+        ]
+    })
+    t.deepEqual(objectForPath(rules, '/something/ding'),  {
+        "X-Frame-Thing":[
+            "SAMEORIGIN"
+        ],
+        "X-Frame-Options":[
+            "DENY"
+        ],
+        "X-XSS-Protection":[
+            "1; mode=block"
+        ],
+        "cache-control":[
+            "max-age=0",
+            "no-cache",
+            "no-store",
+            "must-revalidate"
+        ]
+    })
+    t.deepEqual(objectForPath(rules, '/ding/index.html'),  {
+        "X-Frame-Options":[
+            "SAMEORIGIN"
+        ],
+        "X-Frame-Thing":[
+            "SAMEORIGIN"
+        ]
+    })
+})
