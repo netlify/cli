e74526dece17ccd14b2a2eda12229861b33d16ce
Raees Iqbal <raees.bhatti@gmail.com>
2020-04-08 18:39:29 -0700

    Correct the wait time

--- src/tests/dev.test.js
+++ src/tests/dev.test.js
@@ -39,7 +39,7 @@ test.before(async t => {
     }
   })
 
-  // Wait 5 seconds for the Dev server to start, otherwise timeout
+  // Wait 10 seconds for the Dev server to start, otherwise timeout
   return Promise.race([
     p,
     new Promise((resolve, reject) => setTimeout(() => reject('Timedout waiting for Dev server to start'), 10000))]
