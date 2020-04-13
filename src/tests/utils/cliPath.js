3eafeef5c72d03d088f7e9e121cede363707dd17
Raees Iqbal <raees.bhatti@gmail.com>
2020-04-13 05:28:00 -0700

    Tests: Dont use shell

--- src/tests/utils/cliPath.js
+++ src/tests/utils/cliPath.js
@@ -1,5 +1,5 @@
 const path = require('path')
 
-const cliPath = path.join(__dirname, '..', '..', '..', 'bin', 'run')
+const cliPath = path.resolve(__dirname, '..', '..', '..', 'bin', process.platform === 'win32' ? 'run.cmd' : 'run')
 
 module.exports = cliPath
