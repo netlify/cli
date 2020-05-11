8a0dc87b1989a2ba4716a489268d2d7b0f9b791f
Raees Iqbal <raees.bhatti@gmail.com>
2020-05-11 14:16:09 -0700

    Add .env files support fo `netlify dev:exec`  (#895)
    * netlify dev:exec Fix a log
    
    * Remove .env file in dummy-site
    
    * Rewrite getEnvFile as getEnvSettings
    
    * dev:exec Add support .env files
    
    * getEnvSettings: Fix priority

--- src/utils/env.test.js
+++ src/utils/env.test.js
@@ -1,22 +1,31 @@
 const test = require('ava')
 const path = require('path')
-const { getEnvFile } = require('./env')
+const { getEnvSettings } = require('./env')
 
 const contextSite = path.join(__dirname, '..', '..', 'tests', 'context-site')
 const dummySite = path.join(__dirname, '..', '..', 'tests', 'dummy-site')
 const craSite = path.join(__dirname, '..', '..', 'tests', 'site-cra')
 
 test('no .env files', async t => {
-  const f = await getEnvFile(contextSite)
-  t.is(f, undefined)
+  const vars = await getEnvSettings(contextSite)
+  t.deepEqual(vars, {})
 })
 
 test('.env.development file', async t => {
-  const f = await getEnvFile(dummySite)
-  t.is(f, path.resolve(dummySite, '.env.development'))
+  const vars = await getEnvSettings(dummySite)
+  t.deepEqual(vars, {
+    file: path.resolve(dummySite, '.env.development'),
+    vars: {
+      EASY_VAR: 'true',
+      DUMMY_VAR: 'false',
+    },
+  })
 })
 
 test('.env file', async t => {
-  const f = await getEnvFile(craSite)
-  t.is(f, path.resolve(craSite, '.env'))
+  const vars = await getEnvSettings(craSite)
+  t.deepEqual(vars, {
+    file: path.resolve(craSite, '.env'),
+    vars: {},
+  })
 })
