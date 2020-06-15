ec4c462627a863fc37b4ff26e53d33faff5787e8
Raees Iqbal <raees.bhatti@gmail.com>
2020-06-15 00:16:13 -0700

    Add files to Form event

--- src/commands/dev/index.js
+++ src/commands/dev/index.js
@@ -209,6 +209,7 @@ async function startProxy(settings, siteInfo = {}, addonUrls, configPath, projec
             const form = new multiparty.Form({ encoding:  ct.parameters.charset || 'utf8' })
             form.parse(req, (err, Fields, Files) => {
               if (err) return reject(err)
+              Files = Object.entries(Files).reduce((prev, [name, values]) => ({ ...prev, [name]: values.map(v => ({filename: v['originalFilename'], size: v['size'], type: v['headers'] && v['headers']['content-type'], url: v['path']})) }), {})
               return resolve([
                 Object.entries(Fields).reduce((prev, [name, values]) => ({...prev, [name]: values.length > 1 ? values : values[0]}), {}),
                 Object.entries(Files).reduce((prev, [name, values]) => ({...prev, [name]: values.length > 1 ? values : values[0]}), {}),
@@ -223,21 +224,23 @@ async function startProxy(settings, siteInfo = {}, addonUrls, configPath, projec
       }
       const data = JSON.stringify({
         payload: {
-          company: fields[Object.keys(fields).find(name => ['company', 'business', 'employer'].includes(name.toLowerCase()))],
-          last_name: fields[Object.keys(fields).find(name => ['lastname', 'surname', 'byname'].includes(name.toLowerCase()))],
-          first_name: fields[Object.keys(fields).find(name => ['firstname', 'givenname', 'forename'].includes(name.toLowerCase()))],
-          name: fields[Object.keys(fields).find(name => ['name', 'fullname'].includes(name.toLowerCase()))],
-          email: fields[Object.keys(fields).find(name => ['email', 'mail', 'from', 'twitter', 'sender'].includes(name.toLowerCase()))],
-          title: fields[Object.keys(fields).find(name => ['title', 'subject'].includes(name.toLowerCase()))],
+          'company': fields[Object.keys(fields).find(name => ['company', 'business', 'employer'].includes(name.toLowerCase()))],
+          'last_name': fields[Object.keys(fields).find(name => ['lastname', 'surname', 'byname'].includes(name.toLowerCase()))],
+          'first_name': fields[Object.keys(fields).find(name => ['firstname', 'givenname', 'forename'].includes(name.toLowerCase()))],
+          'name': fields[Object.keys(fields).find(name => ['name', 'fullname'].includes(name.toLowerCase()))],
+          'email': fields[Object.keys(fields).find(name => ['email', 'mail', 'from', 'twitter', 'sender'].includes(name.toLowerCase()))],
+          'title': fields[Object.keys(fields).find(name => ['title', 'subject'].includes(name.toLowerCase()))],
           'data': {
             ...fields,
+            ...files,
             'ip': req.connection.remoteAddress,
             'user_agent': req.headers['user-agent'],
             'referrer': req.headers['referer'],
           },
           'created_at': new Date().toISOString(),
-          'human_fields': Object.entries(fields).reduce((prev, [key, val]) => ({ ...prev, [capitalize(key)]: val }), {}),
-          'ordered_human_fields': Object.entries(fields).map(([key, val]) => ({ title: capitalize(key), name: key, value: val })),
+          'human_fields': Object.entries({ ...fields, ...(Object.entries(files).reduce((prev, [name, data]) => ({...prev, [name]: data['url']}), {})) }).reduce((prev, [key, val]) => ({ ...prev, [capitalize(key)]: val }), {}),
+          'ordered_human_fields': Object.entries({ ...fields, ...(Object.entries(files).reduce((prev, [name, data]) => ({...prev, [name]: data['url']}), {})) }).map(([key, val]) => ({ title: capitalize(key), name: key, value: val })),
+          'site_url': siteInfo['ssl_url'],
         },
         site: siteInfo,
       })
