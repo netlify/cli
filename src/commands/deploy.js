efe3c5ff99a129b1ffdaee842bd2f9843ec631ba
Raees Iqbal <raees.bhatti@gmail.com>
2020-07-06 10:26:32 +0500

    Deploy: Force async on large deployments (#966)
    * Deploy: Force async deployment
    
    * Change the threshold for async deployment

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -207,6 +207,7 @@ class DeployCommand extends Command {
         draft: !deployToProduction && !alias,
         message: flags.message,
         deployTimeout: flags.timeout * 1000 || 1.2e6,
+        syncFileLimit: 100,
         branch: alias,
       })
     } catch (e) {
