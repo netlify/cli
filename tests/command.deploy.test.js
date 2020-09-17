031e46538d6f4a1dcbcdd798321282df62df5123
erezrokah <erezrokah@users.noreply.github.com>
2020-09-17 16:12:02 +0200

    test: fix tests on windows and node 8

--- tests/command.deploy.test.js
+++ tests/command.deploy.test.js
@@ -71,59 +71,63 @@ if (process.env.IS_FORK !== 'true') {
     })
   })
 
-  test.only('should deploy edge handlers when directory exists', async t => {
-    await withSiteBuilder('site-with-public-folder', async builder => {
-      const content = '<h1>⊂◉‿◉つ</h1>'
-      builder
-        .withContentFile({
-          path: 'public/index.html',
-          content,
-        })
-        .withNetlifyToml({
-          config: {
-            build: { publish: 'public', command: 'echo "no op"' },
-          },
-        })
-        .withEdgeHandlers({
-          handlers: {
-            onRequest: event => {
-              console.log(`Incoming request for ${event.request.url}`)
+  // the edge handlers plugin only works on node >= 10 and not on windows at the moment
+  const version = parseInt(process.version.substring(1).split('.')[0])
+  if (process.platform !== 'win32' && version >= 10) {
+    test.serial('should deploy edge handlers when directory exists', async t => {
+      await withSiteBuilder('site-with-public-folder', async builder => {
+        const content = '<h1>⊂◉‿◉つ</h1>'
+        builder
+          .withContentFile({
+            path: 'public/index.html',
+            content,
+          })
+          .withNetlifyToml({
+            config: {
+              build: { publish: 'public', command: 'echo "no op"' },
             },
+          })
+          .withEdgeHandlers({
+            handlers: {
+              onRequest: event => {
+                console.log(`Incoming request for ${event.request.url}`)
+              },
+            },
+          })
+
+        await builder.buildAsync()
+
+        const options = {
+          cwd: builder.directory,
+          env: { NETLIFY_SITE_ID: t.context.siteId },
+        }
+        // build the edge handlers first
+        await callCli(['build'], options)
+        const deploy = await callCli(['deploy', '--json'], options).then(output => JSON.parse(output))
+
+        validateDeploy({ deploy, siteName, content, t })
+
+        // validate edge handlers
+        // use this until we can use `netlify api`
+        const [apiToken] = getToken()
+        const resp = await fetch(`https://api.netlify.com/api/v1/deploys/${deploy.deploy_id}/edge_handlers`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiToken}`,
           },
         })
 
-      await builder.buildAsync()
-
-      const options = {
-        cwd: builder.directory,
-        env: { NETLIFY_SITE_ID: t.context.siteId },
-      }
-      // build the edge handlers first
-      await callCli(['build'], options)
-      const deploy = await callCli(['deploy', '--json'], options).then(output => JSON.parse(output))
-
-      validateDeploy({ deploy, siteName, content, t })
-
-      // validate edge handlers
-      // use this until we can use `netlify api`
-      const [apiToken] = getToken()
-      const resp = await fetch(`https://api.netlify.com/api/v1/deploys/${deploy.deploy_id}/edge_handlers`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiToken}`,
-        },
-      })
-
-      t.is(resp.status, 200)
-      const { created_at, sha, ...rest } = await resp.json()
-      t.deepEqual(rest, {
-        content_length: 445,
-        content_type: 'application/javascript',
-        handlers: ['index'],
-        valid: true,
+        t.is(resp.status, 200)
+        const { created_at, sha, ...rest } = await resp.json()
+        t.deepEqual(rest, {
+          content_length: 445,
+          content_type: 'application/javascript',
+          handlers: ['index'],
+          valid: true,
+        })
       })
     })
-  })
+  }
 
   test.after('cleanup', async t => {
     const { siteId } = t.context
