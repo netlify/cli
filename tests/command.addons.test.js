3920d84c0f13ccd82be362a6f30754b6b39c8628
erezrokah <erezrokah@users.noreply.github.com>
2020-10-08 22:28:01 +0200

    test: simplify tests

--- tests/command.addons.test.js
+++ tests/command.addons.test.js
@@ -23,32 +23,32 @@ if (process.env.IS_FORK !== 'true') {
   test.serial('netlify addons:list --json', async t => {
     const cliResponse = await callCli(['addons:list', '--json'], t.context.execOptions)
     const json = JSON.parse(cliResponse)
-    t.is(Array.isArray(json), true)
+    t.true(Array.isArray(json))
     t.is(json.length, 0)
   })
 
   test.serial('netlify addons:create demo', async t => {
     const cliResponse = await callCli(['addons:create', 'demo', '--TWILIO_ACCOUNT_SID', 'foo'], t.context.execOptions)
-    t.is(cliResponse.includes('Add-on "demo" created'), true)
+    t.true(cliResponse.includes('Add-on "demo" created'))
   })
 
   test.serial('After creation netlify addons:list --json', async t => {
     const cliResponse = await callCli(['addons:list', '--json'], t.context.execOptions)
     const json = JSON.parse(cliResponse)
-    t.is(Array.isArray(json), true)
+    t.true(Array.isArray(json))
     t.is(json.length, 1)
     t.is(json[0].service_slug, 'demo')
   })
 
   test.serial('netlify addons:config demo', async t => {
     const cliResponse = await callCli(['addons:config', 'demo', '--TWILIO_ACCOUNT_SID', 'bar'], t.context.execOptions)
-    t.is(cliResponse.includes('Updating demo add-on config values'), true)
-    t.is(cliResponse.includes('Add-on "demo" successfully updated'), true)
+    t.true(cliResponse.includes('Updating demo add-on config values'))
+    t.true(cliResponse.includes('Add-on "demo" successfully updated'))
   })
 
   test.serial('netlify addon:delete demo', async t => {
     const cliResponse = await callCli(['addons:delete', 'demo', '-f'], t.context.execOptions)
-    t.is(cliResponse.includes('Addon "demo" deleted'), true)
+    t.true(cliResponse.includes('Addon "demo" deleted'))
   })
 
   test.after('cleanup', async t => {
