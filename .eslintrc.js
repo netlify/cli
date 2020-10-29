2c377867e5c910d1a3eebee22f7deb2487e66d74
ehmicky <ehmicky@gmail.com>
2020-10-29 21:35:40 +0100

    chore(lint): add `eslint-plugin-promise`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 const { overrides } = require('@netlify/eslint-config-node')
 
 module.exports = {
-  extends: '@netlify/eslint-config-node',
+  extends: ['@netlify/eslint-config-node', 'plugin:promise/recommended'],
   rules: {
     // Those rules from @netlify/eslint-config-node are currently disabled
     // TODO: remove
@@ -18,6 +18,19 @@ module.exports = {
     'fp/no-mutation': 0,
     'import/max-dependencies': 0,
     'node/no-sync': 0,
+
+    // TODO: enable the disabled rules
+    'promise/always-return': 0,
+    'promise/avoid-new': 0,
+    'promise/catch-or-return': 0,
+    'promise/no-callback-in-promise': 0,
+    'promise/no-nesting': 0,
+    'promise/no-promise-in-callback': 2,
+    'promise/no-return-in-finally': 2,
+    'promise/no-return-wrap': 0,
+    'promise/prefer-await-to-callbacks': 0,
+    'promise/prefer-await-to-then': 0,
+    'promise/valid-params': 2,
 
     // TODO: harmonize with filename snake_case in other Netlify Dev projects
     'unicorn/filename-case': [2, { case: 'kebabCase', ignore: ['.*.md'] }],
