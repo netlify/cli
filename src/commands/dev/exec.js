5804724f182ce1471c7c2ea637bfda2d3eb27f67
Erez Rokah <erezrokah@users.noreply.github.com>
2020-11-02 20:16:33 +0200

    fix: don't override dot envs with site envs (#1490)

--- src/commands/dev/exec.js
+++ src/commands/dev/exec.js
@@ -1,43 +1,22 @@
-const fromEntries = require('@ungap/from-entries')
-const chalk = require('chalk')
 const execa = require('execa')
 
 const Command = require('../../utils/command')
-const { addEnvVariables } = require('../../utils/dev')
-const { getEnvSettings } = require('../../utils/env')
-const {
-  // NETLIFYDEV,
-  NETLIFYDEVLOG,
-  // NETLIFYDEVWARN,
-  NETLIFYDEVERR,
-} = require('../../utils/logo')
+const { getSiteInformation, addEnvVariables } = require('../../utils/dev')
 
 class ExecCommand extends Command {
   async run() {
-    const { site, api } = this.netlify
-    if (site.id) {
-      // just to show some visual response first
-      this.log(`${NETLIFYDEVLOG} Checking your site's environment variables...`)
-      await addEnvVariables(api, site)
-    } else {
-      this.log(
-        `${NETLIFYDEVERR} No Site ID detected. You probably forgot to run \`netlify link\` or \`netlify init\`. `,
-      )
-    }
-
-    const envSettings = await getEnvSettings({ projectDir: site.root, warn: this.warn })
-    if (envSettings.vars.length !== 0) {
-      this.log(
-        `${NETLIFYDEVLOG} Adding the following env variables from ${envSettings.files.map((file) =>
-          chalk.blue(file),
-        )}:`,
-        chalk.yellow(envSettings.vars.map(([key]) => key)),
-      )
-    }
+    const { log, warn, error, netlify } = this
+    const { site, api } = netlify
+    const { teamEnv, addonsEnv, siteEnv, dotFilesEnv } = await getSiteInformation({
+      api,
+      site,
+      warn,
+      error,
+    })
+    await addEnvVariables({ log, teamEnv, addonsEnv, siteEnv, dotFilesEnv })
 
     execa(this.argv[0], this.argv.slice(1), {
       stdio: 'inherit',
-      env: fromEntries(envSettings.vars),
     })
     await this.config.runHook('analytics', {
       eventName: 'command',
