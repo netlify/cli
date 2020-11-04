ba8a7a32f479de5469c0bb8e05f7eb607c339169
Erez Rokah <erezrokah@users.noreply.github.com>
2020-11-04 17:06:35 +0200

    fix(build): pass team,site, addons env variables to build command (#1519)

--- src/commands/build/index.js
+++ src/commands/build/index.js
@@ -9,8 +9,8 @@ class BuildCommand extends Command {
   // Run Netlify Build
   async run() {
     // Retrieve Netlify Build options
-    const options = getBuildOptions({
-      netlify: this.netlify,
+    const options = await getBuildOptions({
+      context: this,
       token: this.getConfigToken()[0],
       flags: this.parse(BuildCommand).flags,
     })
@@ -28,13 +28,11 @@ class BuildCommand extends Command {
   checkOptions({ cachedConfig, token }) {
     const { siteInfo = {} } = JSON.parse(cachedConfig)
     if (!siteInfo.id && process.env.NODE_ENV !== 'test') {
-      console.error('Could not find the site ID. Please run netlify link.')
-      this.exit(1)
+      this.error('Could not find the site ID. Please run netlify link.', { exit: 1 })
     }
 
     if (!token) {
-      console.error('Could not find the access token. Please run netlify login.')
-      this.exit(1)
+      this.error('Could not find the access token. Please run netlify login.', { exit: 1 })
     }
   }
 }
