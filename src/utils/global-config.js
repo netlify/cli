e97b1e573a5a53395165601b9c4085c24335b8be
Evans Hauser <evanshauser@gmail.com>
2020-11-05 14:08:38 -0800

    feat: default to XDG spec for global config
    when ~/.netlify/config.json is not present use
    $XDG_CONFIG_HOME/netlify/config.json, which falls back to
    ~/.config/netlify/config.json if $XDG_CONFIG_HOME is not set

--- src/utils/global-config.js
+++ src/utils/global-config.js
@@ -1,7 +1,11 @@
+const fs = require('fs')
+
 const Configstore = require('configstore')
 const { v4: uuidv4 } = require('uuid')
 
 const { getPathInHome } = require('../lib/settings')
+
+const CONFIG_NAME = 'netlify'
 
 const globalConfigDefaults = {
   /* disable stats from being sent to Netlify */
@@ -10,8 +14,16 @@ const globalConfigDefaults = {
   cliId: uuidv4(),
 }
 
+const legacyConfigPath = getPathInHome(['config.json'])
+const legacyConfigExists = fs.existsSync(legacyConfigPath)
+
+// Configuration location can be:
+// - ~/.netlify/config
+// - $XDG_CONFIG_HOME/netlify/config.json
+// - ~/.config/netlify/config.json
 const globalConfigOptions = {
-  configPath: getPathInHome(['config.json']),
+  configPath: legacyConfigExists ? legacyConfigPath : undefined,
+  globalConfigPath: true,
 }
 
-module.exports = new Configstore(null, globalConfigDefaults, globalConfigOptions)
+module.exports = new Configstore(CONFIG_NAME, globalConfigDefaults, globalConfigOptions)
