a612d9a1329fea5fc229f5eafa74fdf0f3e138bc
erezrokah <erezrokah@users.noreply.github.com>
2020-11-18 14:49:43 +0100

    fix(command-deploy): print relevant log error when bg functions are not supported

--- src/utils/serve-functions.js
+++ src/utils/serve-functions.js
@@ -16,6 +16,8 @@ const multiparty = require('multiparty')
 const getRawBody = require('raw-body')
 const winston = require('winston')
 
+const { getLogMessage } = require('../lib/log')
+
 const { detectFunctionsBuilder } = require('./detect-functions-builder')
 const { getFunctions } = require('./get-functions')
 const { NETLIFYDEVLOG, NETLIFYDEVWARN, NETLIFYDEVERR } = require('./logo')
@@ -168,18 +170,9 @@ const shouldBase64Encode = function (contentType) {
 
 const BASE_64_MIME_REGEXP = /image|audio|video|application\/pdf|application\/zip|applicaton\/octet-stream/i
 
-const RED_BACKGROUND = chalk.red('-background')
-const [PRO, BUSINESS, ENTERPRISE] = ['Pro', 'Business', 'Enterprise'].map((plan) => chalk.magenta(plan))
-const BACKGROUND_FUNCTIONS_WARNING = `A serverless function ending in \`${RED_BACKGROUND}\` was detected.
-Your team’s current plan doesn’t support Background Functions, which have names ending in \`${RED_BACKGROUND}\`.
-To be able to deploy this function successfully either:
-  - change the function name to remove \`${RED_BACKGROUND}\` and execute it synchronously
-  - upgrade your team plan to a level that supports Background Functions (${PRO}, ${BUSINESS}, or ${ENTERPRISE})
-`
-
 const validateFunctions = function ({ functions, capabilities, warn }) {
   if (!capabilities.backgroundFunctions && functions.some(({ isBackground }) => isBackground)) {
-    warn(BACKGROUND_FUNCTIONS_WARNING)
+    warn(getLogMessage('functions.backgroundNotSupported'))
   }
 }
 
