530243a743ba8ff52e44f68aca98d9088ff5a1f3
Seb Smolorz <sebastian.smolorz1@gmail.com>
2021-01-07 15:46:40 +0000

    Document: 37 character limit on alias flag

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -565,7 +565,7 @@ DeployCommand.flags = {
     exclusive: ['alias', 'branch'],
   }),
   alias: flagsLib.string({
-    description: 'Specifies the alias for deployment. Useful for creating predictable deployment URLs',
+    description: 'Specifies the alias for deployment. Useful for creating predictable deployment URLs. Maximum 37 characters.',
   }),
   branch: flagsLib.string({
     char: 'b',
