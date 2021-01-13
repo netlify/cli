bc7697343c3e0829a2ff3209fc86aa0fd7d7b0ed
Erez Rokah <erezrokah@users.noreply.github.com>
2021-01-13 17:52:06 +0200

    test: add CLI timeout (#1727)

--- tests/utils/call-cli.js
+++ tests/utils/call-cli.js
@@ -2,8 +2,13 @@ const execa = require('execa')
 
 const cliPath = require('./cli-path')
 
+const CLI_TIMEOUT = 3e5
+
 const callCli = async function (args, execOptions) {
-  const { stdout } = await execa(cliPath, args, { windowsHide: true, windowsVerbatimArguments: true, ...execOptions })
+  const { stdout } = await execa(cliPath, args, {
+    timeout: CLI_TIMEOUT,
+    ...execOptions,
+  })
   return stdout
 }
 
