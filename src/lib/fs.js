f3904e16c87615b5fb4d8ff3d01de3dbf2b54b2e
erezrokah <erezrokah@users.noreply.github.com>
2021-01-13 19:11:33 +0100

    fix: windows install

--- src/lib/fs.js
+++ src/lib/fs.js
@@ -11,7 +11,7 @@ const writeFileAsync = promisify(fs.writeFile)
 const rmFileAsync = promisify(fs.unlink)
 const copyFileAsync = promisify(fs.copyFile)
 const accessAsync = promisify(fs.access)
-const mkdtempAsync = promisify(fs.mkdtempSync)
+const mkdtempAsync = promisify(fs.mkdtemp)
 const appendFileAsync = promisify(fs.appendFile)
 
 const readFileAsyncCatchError = async (filepath) => {
