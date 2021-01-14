5fa9e3f4a01ddabf16cba0a10fac7edcbed2161f
Erez Rokah <erezrokah@users.noreply.github.com>
2021-01-14 08:30:05 -0800

    test: use got instead of node-fetch (#1733)

---
+++ tests/utils/got.js
@@ -0,0 +1,16 @@
+const got = require('got')
+
+const TIMEOUT = 3e5
+
+// Default got retry status code with the addition of 403
+// eslint-disable-next-line no-magic-numbers
+const STATUS_CODE = [403, 408, 413, 429, 500, 502, 503, 504, 521, 522, 524]
+
+const extendedGot = got.extend({
+  retry: {
+    statusCodes: STATUS_CODE,
+  },
+  timeout: TIMEOUT,
+})
+
+module.exports = extendedGot
