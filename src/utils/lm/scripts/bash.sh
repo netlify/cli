6e88890d3155e2a5599eb612fbcf752a4846de5b
erezrokah <erezrokah@users.noreply.github.com>
2021-02-08 18:14:29 +0100

    style: format bash.sh

--- src/utils/lm/scripts/bash.sh
+++ src/utils/lm/scripts/bash.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-script_link="$( command readlink "$BASH_SOURCE" )" || script_link="$BASH_SOURCE"
+script_link="$(command readlink "$BASH_SOURCE")" || script_link="$BASH_SOURCE"
 apparent_sdk_dir="${script_link%/*}"
+
 if [ "$apparent_sdk_dir" == "$script_link" ]; then
-apparent_sdk_dir=.
+  apparent_sdk_dir=.
 fi
-sdk_dir="$( command cd -P "$apparent_sdk_dir" > /dev/null && command pwd -P )"
+
+sdk_dir="$(command cd -P "$apparent_sdk_dir" >/dev/null && command pwd -P)"
 bin_path="$sdk_dir/bin"
+
 if [[ ":${PATH}:" != *":${bin_path}:"* ]]; then
-export PATH=$bin_path:$PATH
+  export PATH=$bin_path:$PATH
 fi
