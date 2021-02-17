5e6c013b7ede6fb7f3bc91c8628e14339df57e4f
erezrokah <erezrokah@users.noreply.github.com>
2021-02-17 18:48:54 +0100

    refactor: add comment explaining chosing first framework

--- src/utils/init/frameworks.js
+++ src/utils/init/frameworks.js
@@ -2,6 +2,7 @@ const { listFrameworks } = require('@netlify/framework-info')
 
 const getFrameworkInfo = async ({ siteRoot, nodeVersion }) => {
   const frameworks = await listFrameworks({ projectDir: siteRoot, nodeVersion })
+  // several frameworks can be detected - first one has highest priority
   if (frameworks.length !== 0) {
     const [
       {
