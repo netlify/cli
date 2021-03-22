624cc5f69b6e7eb3e9a3f0639a5b3298da0d98e6
Erez Rokah <erezrokah@users.noreply.github.com>
2021-03-22 12:34:27 +0100

    fix(command-link): don't modify gitignore if a pattern includes .netlify (#2031)

--- src/utils/gitignore.js
+++ src/utils/gitignore.js
@@ -92,7 +92,7 @@ const ensureNetlifyIgnore = async function (dir) {
     // ignore
   }
   /* Not ignoring .netlify folder. Add to .gitignore */
-  if (!ignorePatterns || !ignorePatterns.patterns.includes('.netlify')) {
+  if (!ignorePatterns || !ignorePatterns.patterns.some((pattern) => /(^|\/|\\)\.netlify($|\/|\\)/.test(pattern))) {
     const newContents = `${gitIgnoreContents}\n${ignoreContent}`
     await writeFileAsync(gitIgnorePath, newContents, 'utf8')
   }
