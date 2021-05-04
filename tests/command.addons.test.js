f95194e5cae3dfa87fa4e9decde0da2184b740cf
erezrokah <erezrokah@users.noreply.github.com>
2021-05-04 15:04:29 +0300

    test(command-addons): use mock api

--- tests/command.addons.test.js
+++ tests/command.addons.test.js
@@ -1,65 +1,162 @@
-const process = require('process')
-
 const test = require('ava')
 
 const callCli = require('./utils/call-cli')
-const { generateSiteName, createLiveTestSite } = require('./utils/create-live-test-site')
-const { createSiteBuilder } = require('./utils/site-builder')
+const { withMockApi } = require('./utils/mock-api')
+const { withSiteBuilder } = require('./utils/site-builder')
 
-const siteName = generateSiteName('netlify-test-addons-')
+const getCLIOptions = ({ builder: { directory: cwd }, apiUrl }) => ({
+  cwd,
+  env: { NETLIFY_AUTH_TOKEN: 'fake-token', NETLIFY_SITE_ID: 'site_id', NETLIFY_API_URL: apiUrl },
+})
 
-if (process.env.IS_FORK !== 'true') {
-  test.before(async (t) => {
-    const { siteId } = await createLiveTestSite(siteName)
-    const builder = createSiteBuilder({ siteName: 'site-with-addons' })
+const siteInfo = {
+  account_slug: 'test-account',
+  id: 'site_id',
+  name: 'site-name',
+}
+const routes = [
+  { path: 'sites/site_id', response: siteInfo },
+  { path: 'sites/site_id/service-instances', response: [] },
+  {
+    path: 'accounts',
+    response: [{ slug: siteInfo.account_slug }],
+  },
+]
+
+test('netlify addons:list', async (t) => {
+  await withSiteBuilder('site-with-addons', async (builder) => {
     await builder.buildAsync()
 
-    t.context.execOptions = { cwd: builder.directory, env: { NETLIFY_SITE_ID: siteId } }
-    t.context.builder = builder
+    await withMockApi(routes, async ({ apiUrl }) => {
+      const cliResponse = await callCli(['addons:list'], getCLIOptions({ builder, apiUrl }))
+      t.true(cliResponse.includes('No addons currently installed'))
+    })
   })
+})
 
-  test.serial('netlify addons:list', async (t) => {
-    const cliResponse = await callCli(['addons:list'], t.context.execOptions)
-    t.true(cliResponse.includes('No addons currently installed'))
+test('netlify addons:list --json', async (t) => {
+  await withSiteBuilder('site-with-addons', async (builder) => {
+    await builder.buildAsync()
+
+    await withMockApi(routes, async ({ apiUrl }) => {
+      const cliResponse = await callCli(['addons:list', '--json'], getCLIOptions({ builder, apiUrl }))
+      const json = JSON.parse(cliResponse)
+      t.true(Array.isArray(json))
+      t.is(json.length, 0)
+    })
   })
+})
 
-  test.serial('netlify addons:list --json', async (t) => {
-    const cliResponse = await callCli(['addons:list', '--json'], t.context.execOptions)
-    const json = JSON.parse(cliResponse)
-    t.true(Array.isArray(json))
-    t.is(json.length, 0)
+test('netlify addons:create demo', async (t) => {
+  await withSiteBuilder('site-with-addons', async (builder) => {
+    await builder.buildAsync()
+
+    const createRoutes = [
+      ...routes,
+      { path: 'services/demo/manifest', response: {} },
+      {
+        path: 'sites/site_id/services/demo/instances',
+        response: {},
+        method: 'POST',
+        requestBody: JSON.stringify({ config: { TWILIO_ACCOUNT_SID: 'foo' } }),
+      },
+    ]
+
+    await withMockApi(createRoutes, async ({ apiUrl }) => {
+      const cliResponse = await callCli(
+        ['addons:create', 'demo', '--TWILIO_ACCOUNT_SID', 'foo'],
+        getCLIOptions({ builder, apiUrl }),
+      )
+      t.true(cliResponse.includes('Add-on "demo" created'))
+    })
   })
+})
 
-  test.serial('netlify addons:create demo', async (t) => {
-    const cliResponse = await callCli(['addons:create', 'demo', '--TWILIO_ACCOUNT_SID', 'foo'], t.context.execOptions)
-    t.true(cliResponse.includes('Add-on "demo" created'))
+test('After creation netlify addons:list --json', async (t) => {
+  await withSiteBuilder('site-with-addons', async (builder) => {
+    await builder.buildAsync()
+
+    const withExistingAddon = [
+      { path: 'sites/site_id', response: siteInfo },
+      {
+        path: 'sites/site_id/service-instances',
+        response: [{ service_slug: 'demo' }],
+      },
+      {
+        path: 'accounts',
+        response: [{ slug: siteInfo.account_slug }],
+      },
+    ]
+
+    await withMockApi(withExistingAddon, async ({ apiUrl }) => {
+      const cliResponse = await callCli(['addons:list', '--json'], getCLIOptions({ builder, apiUrl }))
+      const json = JSON.parse(cliResponse)
+      t.true(Array.isArray(json))
+      t.is(json.length, 1)
+      t.is(json[0].service_slug, 'demo')
+    })
   })
+})
 
-  test.serial('After creation netlify addons:list --json', async (t) => {
-    const cliResponse = await callCli(['addons:list', '--json'], t.context.execOptions)
-    const json = JSON.parse(cliResponse)
-    t.true(Array.isArray(json))
-    t.is(json.length, 1)
-    t.is(json[0].service_slug, 'demo')
+test('netlify addons:config demo', async (t) => {
+  await withSiteBuilder('site-with-addons', async (builder) => {
+    await builder.buildAsync()
+
+    const configRoutes = [
+      { path: 'sites/site_id', response: siteInfo },
+      {
+        path: 'sites/site_id/service-instances',
+        response: [{ id: 'demo', service_slug: 'demo', config: { TWILIO_ACCOUNT_SID: 'foo' } }],
+      },
+      {
+        path: 'accounts',
+        response: [{ slug: siteInfo.account_slug }],
+      },
+      { path: 'services/demo/manifest', response: { config: { TWILIO_ACCOUNT_SID: '' } } },
+      {
+        path: 'sites/site_id/services/demo/instances/demo',
+        response: {},
+        method: 'PUT',
+        requestBody: JSON.stringify({ config: { TWILIO_ACCOUNT_SID: 'bar' } }),
+      },
+    ]
+
+    await withMockApi(configRoutes, async ({ apiUrl }) => {
+      const cliResponse = await callCli(
+        ['addons:config', 'demo', '--TWILIO_ACCOUNT_SID', 'bar'],
+        getCLIOptions({ builder, apiUrl }),
+      )
+      t.true(cliResponse.includes('Updating demo add-on config values'))
+      t.true(cliResponse.includes('Add-on "demo" successfully updated'))
+    })
   })
+})
 
-  test.serial('netlify addons:config demo', async (t) => {
-    const cliResponse = await callCli(['addons:config', 'demo', '--TWILIO_ACCOUNT_SID', 'bar'], t.context.execOptions)
-    t.true(cliResponse.includes('Updating demo add-on config values'))
-    t.true(cliResponse.includes('Add-on "demo" successfully updated'))
+test('netlify addon:delete demo', async (t) => {
+  await withSiteBuilder('site-with-addons', async (builder) => {
+    await builder.buildAsync()
+
+    const deleteRoutes = [
+      { path: 'sites/site_id', response: siteInfo },
+      {
+        path: 'sites/site_id/service-instances',
+        response: [{ id: 'demo', service_slug: 'demo', config: { TWILIO_ACCOUNT_SID: 'foo' } }],
+      },
+      {
+        path: 'accounts',
+        response: [{ slug: siteInfo.account_slug }],
+      },
+      { path: 'services/demo/manifest', response: {} },
+      {
+        path: 'sites/site_id/services/demo/instances/demo',
+        response: {},
+        method: 'DELETE',
+      },
+    ]
+
+    await withMockApi(deleteRoutes, async ({ apiUrl }) => {
+      const cliResponse = await callCli(['addons:delete', 'demo', '-f'], getCLIOptions({ builder, apiUrl }))
+      t.true(cliResponse.includes('Addon "demo" deleted'))
+    })
   })
-
-  test.serial('netlify addon:delete demo', async (t) => {
-    const cliResponse = await callCli(['addons:delete', 'demo', '-f'], t.context.execOptions)
-    t.true(cliResponse.includes('Addon "demo" deleted'))
-  })
-
-  test.after('cleanup', async (t) => {
-    const { execOptions, builder } = t.context
-    console.log('Performing cleanup')
-    console.log(`deleting test site "${siteName}". ${execOptions.env.NETLIFY_SITE_ID}`)
-    await callCli(['sites:delete', execOptions.env.NETLIFY_SITE_ID, '--force'], execOptions)
-
-    await builder.cleanupAsync()
-  })
-}
+})
