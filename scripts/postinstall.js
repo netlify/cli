b725f9cc4f128c69f50d4e09785a0353a1824c78
Erez Rokah <erezrokah@users.noreply.github.com>
2021-05-11 17:16:30 +0300

    fix(postinstall): wrap chalk require (#2330)

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,15 +1,34 @@
-const chalk = require('chalk')
+const id = (message) => message
 
-console.log('')
-console.log(chalk.greenBright.bold.underline(`Success! Netlify CLI has been installed!`))
-console.log('')
-console.log('Your device is now configured to use Netlify CLI to deploy and manage your Netlify sites.')
-console.log('')
-console.log('Next steps:')
-console.log('')
-console.log(`  ${chalk.cyanBright.bold('netlify init')}     Connect or create a Netlify site from current directory`)
-console.log(`  ${chalk.cyanBright.bold('netlify deploy')}   Deploy the latest changes to your Netlify site`)
-console.log('')
-console.log(`For more information on the CLI run ${chalk.cyanBright.bold('netlify help')}`)
-console.log(`Or visit the docs at ${chalk.cyanBright.bold('https://cli.netlify.com')}`)
-console.log('')
+const format = (message, styles) => {
+  let func = id
+  try {
+    // this fails sometimes on outdated npm versions
+    // eslint-disable-next-line node/global-require
+    func = require('chalk')
+    styles.forEach((style) => {
+      func = func[style]
+    })
+  } catch (_) {}
+  return func(message)
+}
+
+const postInstall = () => {
+  console.log('')
+  console.log(format('Success! Netlify CLI has been installed!', ['greenBright', 'bold', 'underline']))
+  console.log('')
+  console.log('Your device is now configured to use Netlify CLI to deploy and manage your Netlify sites.')
+  console.log('')
+  console.log('Next steps:')
+  console.log('')
+  console.log(
+    `  ${format('netlify init', ['cyanBright', 'bold'])}     Connect or create a Netlify site from current directory`,
+  )
+  console.log(`  ${format('netlify deploy', ['cyanBright', 'bold'])}   Deploy the latest changes to your Netlify site`)
+  console.log('')
+  console.log(`For more information on the CLI run ${format('netlify help', ['cyanBright', 'bold'])}`)
+  console.log(`Or visit the docs at ${format('https://cli.netlify.com', ['cyanBright', 'bold'])}`)
+  console.log('')
+}
+
+postInstall()
