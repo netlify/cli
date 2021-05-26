7e7b24cd27317bb99c7e90949e16f37a6779dd9e
erezrokah <erezrokah@users.noreply.github.com>
2021-05-26 11:16:39 +0300

    test(command-deploy): improve hash files/fns tests

--- src/utils/deploy/hash-files.test.js
+++ src/utils/deploy/hash-files.test.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 const test = require('ava')
 
 const { withSiteBuilder } = require('../../../tests/utils/site-builder')
@@ -8,53 +6,36 @@ const { DEFAULT_CONCURRENT_HASH } = require('./constants')
 const { hashFiles } = require('./hash-files')
 
 test('Hashes files in a folder', async (t) => {
-  await withSiteBuilder('site-with-override', async (builder) => {
-    builder
-      .withNetlifyToml({ config: { functions: { directory: 'functions' } } })
+  await withSiteBuilder('site-with-content', async (builder) => {
+    await builder
+      .withNetlifyToml({ config: { build: { publish: 'public' } } })
       .withContentFile({
-        path: 'lib/util.js',
-        content: `module.exports = { one: 1 }`,
+        path: 'public/index.html',
+        content: `Root page`,
       })
-      .withContentFiles([
-        {
-          path: 'functions/function-1.js',
-          content: `
-const { one } = require('../../lib/util')
+      .buildAsync()
 
-module.exports.handler = async () => ({ statusCode: 200, body: one })
-`,
-        },
-        {
-          path: 'functions/function-2.js',
-          content: `
-const { one } = require('../../lib/util')
-
-module.exports.handler = async () => ({ statusCode: 200, body: one })
-`,
-        },
-      ])
-
-    await builder.buildAsync()
-
-    const expectedFiles = ['netlify.toml', 'functions/function-1.js', 'functions/function-2.js', 'lib/util.js']
-    const { files, filesShaMap } = await hashFiles(builder.directory, path.join(builder.directory, 'netlify.toml'), {
+    const expectedFiles = ['netlify.toml', 'public/index.html']
+    const { files, filesShaMap } = await hashFiles(builder.directory, `${builder.directory}/netlify.toml`, {
       filter: () => true,
       concurrentHash: DEFAULT_CONCURRENT_HASH,
       statusCb() {},
     })
 
-    expectedFiles.forEach((filePath) => {
-      t.truthy(files[filePath], `includes the ${filePath} file`)
-    })
+    t.is(Object.entries(files).length, expectedFiles.length)
+    t.is(Object.entries(filesShaMap).length, expectedFiles.length)
 
-    t.truthy(files['netlify.toml'], 'includes the netlify.toml file')
-    Object.keys(files).forEach((filePath) => {
-      t.truthy(filePath, 'each file has a path')
-    })
-    t.truthy(filesShaMap, 'filesShaMap is returned')
-    Object.values(filesShaMap).forEach((fileObjArray) => {
+    expectedFiles.forEach((filePath) => {
+      const sha = files[filePath]
+      t.truthy(sha, `includes the ${filePath} file`)
+
+      const fileObjArray = filesShaMap[sha]
       fileObjArray.forEach((fileObj) => {
-        t.truthy(fileObj.normalizedPath, 'fileObj have a normalizedPath field')
+        t.is(
+          fileObj.normalizedPath,
+          filePath,
+          'fileObj normalizedPath property should equal to file path from files array',
+        )
       })
     })
   })
