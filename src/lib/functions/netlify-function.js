ca33be4c2ec3abbf6f95eccd99a9179a476866a7
Eduardo Bouças <mail@eduardoboucas.com>
2021-06-22 15:55:41 +0100

    feat: add build command to functions

--- src/lib/functions/netlify-function.js
+++ src/lib/functions/netlify-function.js
@@ -1,18 +1,86 @@
 const BACKGROUND_SUFFIX = '-background'
 
 class NetlifyFunction {
-  constructor({ mainFile, name, runtime, timeoutBackground, timeoutSynchronous }) {
+  constructor({
+    config,
+    errorExit,
+    functionsDirectory,
+    mainFile,
+    name,
+    projectRoot,
+    runtime,
+    timeoutBackground,
+    timeoutSynchronous,
+  }) {
+    this.config = config
+    this.errorExit = errorExit
+    this.functionsDirectory = functionsDirectory
     this.mainFile = mainFile
     this.name = name
+    this.projectRoot = projectRoot
     this.runtime = runtime
     this.timeoutBackground = timeoutBackground
     this.timeoutSynchronous = timeoutSynchronous
 
+    // Determines whether this is a background function based on the function
+    // name.
     this.isBackground = name.endsWith(BACKGROUND_SUFFIX)
-    this.urlPath = `/.netlify/functions/${name}`
+
+    // List of the function's source files. This starts out as an empty set
+    // and will get populated on every build.
+    this.srcFiles = new Set()
+  }
+
+  async build() {
+    const buildFunction = await this.runtime.getBuildFunction({
+      config: this.config,
+      errorExit: this.errorExit,
+      func: this,
+      functionsDirectory: this.functionsDirectory,
+      projectRoot: this.projectRoot,
+    })
+
+    this.buildQueue = typeof buildFunction === 'function' ? buildFunction(this) : undefined
+
+    try {
+      const { srcFiles, ...buildData } = (await this.buildQueue) || {}
+      const srcFilesSet = new Set(srcFiles)
+      const srcFilesDiff = this.getSrcFilesDiff(srcFilesSet)
+
+      this.buildData = buildData
+      this.srcFiles = srcFilesSet
+
+      return { srcFilesDiff }
+    } catch (error) {
+      return { error }
+    }
+  }
+
+  getSrcFilesDiff(srcFiles) {
+    const added = new Set()
+    const deleted = new Set()
+
+    srcFiles.forEach((path) => {
+      if (!this.srcFiles.has(path)) {
+        added.add(path)
+      }
+    })
+
+    this.srcFiles.forEach((path) => {
+      if (!srcFiles.has(path)) {
+        deleted.add(path)
+      }
+    })
+
+    return {
+      added,
+      deleted,
+    }
   }
 
   async invoke(event, context) {
+    await this.buildQueue
+
     const timeout = this.isBackground ? this.timeoutBackground : this.timeoutSynchronous
 
     let error = null
@@ -22,7 +90,7 @@ class NetlifyFunction {
       result = await this.runtime.invokeFunction({
         context,
         event,
-        mainFile: this.mainFile,
+        func: this,
         timeout,
       })
     } catch (functionError) {
