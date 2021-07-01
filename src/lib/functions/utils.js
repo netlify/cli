4eb8cc3c694f9671e67c384f561fe0f3701dd7b5
Eduardo Bouças <mail@eduardoboucas.com>
2021-07-01 23:15:12 +0100

    refactor: remove unused utils

--- src/lib/functions/utils.js
+++ src/lib/functions/utils.js
@@ -1,6 +1,5 @@
 const chalk = require('chalk')
 
-const { NETLIFYDEVLOG } = require('../../utils/logo')
 const { getLogMessage } = require('../log')
 
 const BASE_64_MIME_REGEXP = /image|audio|video|application\/pdf|application\/zip|applicaton\/octet-stream/i
@@ -21,34 +20,17 @@ const detectAwsSdkError = ({ error, warn }) => {
 
 const formatLambdaError = (err) => chalk.red(`${err.errorType}: ${err.errorMessage}`)
 
-const logAfterAction = ({ path, action }) => {
-  console.log(`${NETLIFYDEVLOG} ${path} ${action}, successfully reloaded!`)
-}
-
-const logBeforeAction = ({ path, action }) => {
-  console.log(`${NETLIFYDEVLOG} ${path} ${action}, reloading...`)
-}
-
 const shouldBase64Encode = function (contentType) {
   return Boolean(contentType) && BASE_64_MIME_REGEXP.test(contentType)
 }
 
 const styleFunctionName = (name) => chalk.magenta(name)
 
-const validateFunctions = function ({ functions, capabilities, warn }) {
-  if (!capabilities.backgroundFunctions && functions.some(({ isBackground }) => isBackground)) {
-    warn(getLogMessage('functions.backgroundNotSupported'))
-  }
-}
-
 module.exports = {
   detectAwsSdkError,
   DEFAULT_LAMBDA_OPTIONS,
   formatLambdaError,
-  logAfterAction,
-  logBeforeAction,
   SECONDS_TO_MILLISECONDS,
   shouldBase64Encode,
   styleFunctionName,
-  validateFunctions,
 }
