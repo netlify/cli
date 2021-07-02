454085cd77165ed8bda471474befc32651410ee6
Eduardo Bouças <mail@eduardoboucas.com>
2021-07-02 12:38:58 +0100

    refactor: reduce debounce wait

--- src/lib/functions/watcher.js
+++ src/lib/functions/watcher.js
@@ -3,50 +3,34 @@ const decache = require('decache')
 const debounce = require('lodash/debounce')
 const pEvent = require('p-event')
 
-const DEBOUNCE_WAIT = 300
+const DEBOUNCE_WAIT = 100
 
 const watchDebounced = async (target, { depth, onAdd, onChange, onUnlink }) => {
   const watcher = chokidar.watch(target, { depth, ignored: /node_modules/, ignoreInitial: true })
-  const debounceOptions = {
-    leading: false,
-    trailing: true,
-  }
 
   await pEvent(watcher, 'ready')
 
-  const debouncedOnChange = debounce(
-    (path) => {
-      decache(path)
+  const debouncedOnChange = debounce((path) => {
+    decache(path)
 
-      if (typeof onChange === 'function') {
-        onChange(path)
-      }
-    },
-    DEBOUNCE_WAIT,
-    debounceOptions,
-  )
-  const debouncedOnUnlink = debounce(
-    (path) => {
-      decache(path)
+    if (typeof onChange === 'function') {
+      onChange(path)
+    }
+  }, DEBOUNCE_WAIT)
+  const debouncedOnUnlink = debounce((path) => {
+    decache(path)
 
-      if (typeof onUnlink === 'function') {
-        onUnlink(path)
-      }
-    },
-    DEBOUNCE_WAIT,
-    debounceOptions,
-  )
-  const debouncedOnAdd = debounce(
-    (path) => {
-      decache(path)
+    if (typeof onUnlink === 'function') {
+      onUnlink(path)
+    }
+  }, DEBOUNCE_WAIT)
+  const debouncedOnAdd = debounce((path) => {
+    decache(path)
 
-      if (typeof onAdd === 'function') {
-        onAdd(path)
-      }
-    },
-    DEBOUNCE_WAIT,
-    debounceOptions,
-  )
+    if (typeof onAdd === 'function') {
+      onAdd(path)
+    }
+  }, DEBOUNCE_WAIT)
 
   watcher.on('change', debouncedOnChange).on('unlink', debouncedOnUnlink).on('add', debouncedOnAdd)
 
