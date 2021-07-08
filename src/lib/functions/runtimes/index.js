d318345747cf242969000e21dbf04272ec3a0ab1
Eduardo Bouças <mail@eduardoboucas.com>
2021-07-08 09:46:43 +0100

    chore: annotate runtimes interface with JSDoc

--- src/lib/functions/runtimes/index.js
+++ src/lib/functions/runtimes/index.js
@@ -1,32 +1,29 @@
 const js = require('./js')
 
-// A runtime must adhere to the following interface:
-//
-// getBuildFunction: ({
-//   config,
-//   errorExit,
-//   func,
-//   functionsDirectory,
-//   projectRoot
-// }) => Promise<(func) => Promise<({ srcFiles, ...buildData })>>
-//
-// - Returns a callback that prepares a function for invocation. This callback
-//   receives a function and returns an object with the source files associated
-//   with the function as well as additional metadata needed by the runtime.
-//
-// invokeFunction: ({
-//   context,
-//   event,
-//   func,
-//   timeout
-// }) => Promise<({ body, statusCode })>
-//
-// - Invokes the function and returns the resulting body and status code.
-//
-// name: string
-//
-//  - Holds the name of the runtime, compatible with the `runtime` property
-//    used in the output of zip-it-and-ship (e.g. "js", "go").
+/**
+ * @callback BuildFunction
+ * @param {object} func
+ * @returns {Promise<{srcFiles: string[], buildPath?: string>}
+ */
+
+/**
+ * @callback GetBuildFunction
+ * @param  {{ config: object, context: object, errorExit: function, func: object, functionsDirectory: string, projectRoot: string }} params
+ * @returns {Promise<BuildFunction>}
+ */
+
+/**
+ * @callback InvokeFunction
+ * @param  {{ context: object, event: object, func: object, timeout: number }} params
+ * @returns {Promise<{ body: object, statusCode: number }>}
+ */
+
+/**
+ * @typedef {object} Runtime
+ * @property {GetBuildFunction} getBuildFunction
+ * @property {InvokeFunction} invokeFunction
+ * @property {string} name
+ */
 
 const runtimes = [js].reduce((res, runtime) => ({ ...res, [runtime.name]: runtime }), {})
 
