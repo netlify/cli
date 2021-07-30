bc4b00bbee5bca8522136ae1fad31c9838d512c4
Eduardo Bouças <mail@eduardoboucas.com>
2021-07-30 13:45:47 +0100

    chore: remove Go function source from test

--- tests/serving-functions-go.test.js
+++ tests/serving-functions-go.test.js
@@ -12,26 +12,6 @@ const WAIT_TIMEOUT = 3000
 const WAIT_WRITE = 1000
 
 test('Updates a Go function when a file is modified', async (t) => {
-  const goSource = `
-package main
-
-import (
-  "github.com/aws/aws-lambda-go/events"
-  "github.com/aws/aws-lambda-go/lambda"
-)
-
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-  return &events.APIGatewayProxyResponse{
-    StatusCode:      200,
-    Headers:         map[string]string{"Content-Type": "text/plain"},
-    Body:            "Hello, world!",
-    IsBase64Encoded: false,
-  }, nil
-}
-
-func main() {
-  lambda.Start(handler)
-}`
   await withSiteBuilder('go-function-update', async (builder) => {
     await builder
       .withNetlifyToml({
@@ -43,46 +23,21 @@ func main() {
       .withContentFiles([
         {
           path: 'functions/go-func/go.mod',
-          content: `
-module github.com/netlify/local-functions-poc-marcus/funcs/test
-
-go 1.15
-
-require github.com/aws/aws-lambda-go v1.20.0`,
+          content: `<mock go.mod>`,
         },
         {
           path: 'functions/go-func/go.sum',
-          content: `
-        github.com/BurntSushi/toml v0.3.1/go.mod h1:xHWCNGjB5oqiDr8zfno3MHue2Ht5sIBksp03qcyfWMU=
-        github.com/aws/aws-lambda-go v1.20.0 h1:ZSweJx/Hy9BoIDXKBEh16vbHH0t0dehnF8MKpMiOWc0=
-        github.com/aws/aws-lambda-go v1.20.0/go.mod h1:jJmlefzPfGnckuHdXX7/80O3BvUUi12XOkbv4w9SGLU=
-        github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d/go.mod h1:maD7wRr/U5Z6m/iR4s+kqSMx2CaBsrgA7czyZG/E6dU=
-        github.com/cpuguy83/go-md2man/v2 v2.0.0/go.mod h1:maD7wRr/U5Z6m/iR4s+kqSMx2CaBsrgA7czyZG/E6dU=
-        github.com/davecgh/go-spew v1.1.0/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
-        github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c=
-        github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
-        github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
-        github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
-        github.com/russross/blackfriday/v2 v2.0.1/go.mod h1:+Rmxgy9KzJVeS9/2gXHxylqXiyQDYRxCVz55jmeOWTM=
-        github.com/shurcooL/sanitized_anchor_name v1.0.0/go.mod h1:1NzhyTcUVG4SuEtjjoZeVRXNmyL/1OwPU0+IJeTBvfc=
-        github.com/stretchr/objx v0.1.0/go.mod h1:HFkY916IF+rwdDfMAkV7OtwuqBVzrE8GR6GFx+wExME=
-        github.com/stretchr/testify v1.6.1 h1:hDPOHmpOpP40lSULcqw7IrRb/u7w6RpDC9399XyoNd0=
-        github.com/stretchr/testify v1.6.1/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/h/Wwjteg=
-        github.com/urfave/cli/v2 v2.2.0/go.mod h1:SE9GqnLQmjVa0iPEY0f1w3ygNIYcIJ0OKPMoW2caLfQ=
-        gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 h1:yhCVgyC4o1eVCa2tZl7eS0r+SDo693bJlVdllGtEeKM=
-        gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
-        gopkg.in/yaml.v2 v2.2.2/go.mod h1:hI93XBmqTisBFMUTm0b8Fm+jr3Dg1NNxqwp+5A1VGuI=
-        gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=
-        gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 h1:tQIYjPdBoyREyB9XMu+nnTclpTYkz2zFM+lzLJFO4gQ=
-        gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=
-`,
+          content: `<mock go.sum>`,
         },
         {
           path: 'functions/go-func/main.go',
-          content: goSource,
+          content: `<mock main.go>`,
         },
       ])
       .buildAsync()
+
+    const originalBody = 'Hello, world!'
+    const updatedBody = 'Hello, Netlify!'
 
     const [execaMock, removeExecaMock] = await createExecaMock(`
       const { writeFileSync } = require('fs')
@@ -105,7 +60,7 @@ require github.com/aws/aws-lambda-go v1.20.0`,
           proxyCallCount++
 
           const response = {
-            body: proxyCallCount === 1 ? 'Hello, world!' : 'Hello, Netlify!',
+            body: proxyCallCount === 1 ? '${originalBody}' : '${updatedBody}',
             statusCode: 200
           }
 
@@ -124,13 +79,13 @@ require github.com/aws/aws-lambda-go v1.20.0`,
       },
       async ({ port, outputBuffer }) => {
         await tryAndLogOutput(async () => {
-          t.is(await got(`http://localhost:${port}/.netlify/functions/go-func`).text(), 'Hello, world!')
+          t.is(await got(`http://localhost:${port}/.netlify/functions/go-func`).text(), originalBody)
         }, outputBuffer)
 
         await pause(WAIT_WRITE)
 
         await builder
-          .withContentFile({ path: 'functions/go-func/main.go', content: goSource.replace('world', 'Netlify') })
+          .withContentFile({ path: 'functions/go-func/main.go', content: `<updated mock main.go>` })
           .buildAsync()
 
         await tryAndLogOutput(
@@ -139,7 +94,7 @@ require github.com/aws/aws-lambda-go v1.20.0`,
               async () => {
                 const response = await got(`http://localhost:${port}/.netlify/functions/go-func`).text()
 
-                return response === 'Hello, Netlify!'
+                return response === updatedBody
               },
               { interval: WAIT_INTERVAL, timeout: WAIT_TIMEOUT },
             ),
