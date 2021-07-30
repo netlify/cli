5426129adb724acfc244e626fea85c5eab2c4661
Eduardo Bouças <mail@eduardoboucas.com>
2021-07-30 14:34:47 +0100

    chore: add try/finally to test

--- tests/serving-functions-go.test.js
+++ tests/serving-functions-go.test.js
@@ -12,97 +12,98 @@ const WAIT_TIMEOUT = 3000
 const WAIT_WRITE = 1000
 
 test('Updates a Go function when a file is modified', async (t) => {
-  await withSiteBuilder('go-function-update', async (builder) => {
-    await builder
-      .withNetlifyToml({
-        config: {
-          build: { publish: 'public' },
-          functions: { directory: 'functions' },
-        },
-      })
-      .withContentFiles([
-        {
-          path: 'functions/go-func/go.mod',
-          content: `<mock go.mod>`,
-        },
-        {
-          path: 'functions/go-func/go.sum',
-          content: `<mock go.sum>`,
-        },
-        {
-          path: 'functions/go-func/main.go',
-          content: `<mock main.go>`,
-        },
-      ])
-      .buildAsync()
+  const originalBody = 'Hello, world!'
+  const updatedBody = 'Hello, Netlify!'
+  const [execaMock, removeExecaMock] = await createExecaMock(`
+    const { writeFileSync } = require('fs')
 
-    const originalBody = 'Hello, world!'
-    const updatedBody = 'Hello, Netlify!'
+    let proxyCallCount = 0
 
-    const [execaMock, removeExecaMock] = await createExecaMock(`
-      const { writeFileSync } = require('fs')
+    module.exports = async (...args) => {
+      if (args[0] === 'go') {
+        const binaryPath = args[1][2]
 
-      let proxyCallCount = 0
+        writeFileSync(binaryPath, '')
 
-      module.exports = async (...args) => {
-        if (args[0] === 'go') {
-          const binaryPath = args[1][2]
-
-          writeFileSync(binaryPath, '')
-
-          return {
-            stderr: '',
-            stdout: ''
-          }
-        }
-        
-        if (args[0].includes('local-functions-proxy')) {
-          proxyCallCount++
-
-          const response = {
-            body: proxyCallCount === 1 ? '${originalBody}' : '${updatedBody}',
-            statusCode: 200
-          }
-
-          return {
-            stderr: '',
-            stdout: JSON.stringify(response)
-          }
+        return {
+          stderr: '',
+          stdout: ''
         }
       }
-    `)
+      
+      if (args[0].includes('local-functions-proxy')) {
+        proxyCallCount++
 
-    await withDevServer(
-      {
-        cwd: builder.directory,
-        env: { ...execaMock, NETLIFY_EXPERIMENTAL_BUILD_GO_SOURCE: 'true' },
-      },
-      async ({ port, outputBuffer }) => {
-        await tryAndLogOutput(async () => {
-          t.is(await got(`http://localhost:${port}/.netlify/functions/go-func`).text(), originalBody)
-        }, outputBuffer)
+        const response = {
+          body: proxyCallCount === 1 ? '${originalBody}' : '${updatedBody}',
+          statusCode: 200
+        }
 
-        await pause(WAIT_WRITE)
+        return {
+          stderr: '',
+          stdout: JSON.stringify(response)
+        }
+      }
+    }
+  `)
 
-        await builder
-          .withContentFile({ path: 'functions/go-func/main.go', content: `<updated mock main.go>` })
-          .buildAsync()
+  await withSiteBuilder('go-function-update', async (builder) => {
+    try {
+      await builder
+        .withNetlifyToml({
+          config: {
+            build: { publish: 'public' },
+            functions: { directory: 'functions' },
+          },
+        })
+        .withContentFiles([
+          {
+            path: 'functions/go-func/go.mod',
+            content: `<mock go.mod>`,
+          },
+          {
+            path: 'functions/go-func/go.sum',
+            content: `<mock go.sum>`,
+          },
+          {
+            path: 'functions/go-func/main.go',
+            content: `<mock main.go>`,
+          },
+        ])
+        .buildAsync()
 
-        await tryAndLogOutput(
-          () =>
-            pWaitFor(
-              async () => {
-                const response = await got(`http://localhost:${port}/.netlify/functions/go-func`).text()
+      await withDevServer(
+        {
+          cwd: builder.directory,
+          env: { ...execaMock, NETLIFY_EXPERIMENTAL_BUILD_GO_SOURCE: 'true' },
+        },
+        async ({ port, outputBuffer }) => {
+          await tryAndLogOutput(async () => {
+            t.is(await got(`http://localhost:${port}/.netlify/functions/go-func`).text(), originalBody)
+          }, outputBuffer)
 
-                return response === updatedBody
-              },
-              { interval: WAIT_INTERVAL, timeout: WAIT_TIMEOUT },
-            ),
-          outputBuffer,
-        )
+          await pause(WAIT_WRITE)
 
-        await removeExecaMock()
-      },
-    )
+          await builder
+            .withContentFile({ path: 'functions/go-func/main.go', content: `<updated mock main.go>` })
+            .buildAsync()
+
+          await tryAndLogOutput(
+            () =>
+              pWaitFor(
+                async () => {
+                  const response = await got(`http://localhost:${port}/.netlify/functions/go-func`).text()
+
+                  return response === updatedBody
+                },
+                { interval: WAIT_INTERVAL, timeout: WAIT_TIMEOUT },
+              ),
+            outputBuffer,
+          )
+        },
+      )
+    } finally {
+      await removeExecaMock()
+    }
   })
 })
