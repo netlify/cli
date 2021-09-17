5022c287151324fd19001e777e847c960c2fe14f
Kunal Kundu <51631122+tinfoil-knight@users.noreply.github.com>
2021-09-17 18:57:09 +0530

    refactor: replace inherited log utils in src/command/(dev|sites|open) (#3349)
    * refactor: replace inherited log, exit utils in commands.dev
    
    * refactor: replace inherited log, exit utils in commands.open
    
    * refactor: replace inherited log, exit utils in commands.sites

--- src/commands/open/site.js
+++ src/commands/open/site.js
@@ -1,5 +1,5 @@
 const Command = require('../../utils/command')
-const { log } = require('../../utils/command-helpers')
+const { log, warn, error, exit } = require('../../utils/command-helpers')
 const openBrowser = require('../../utils/open-browser')
 
 class OpenAdminCommand extends Command {
@@ -10,7 +10,7 @@ class OpenAdminCommand extends Command {
     const siteId = site.id
 
     if (!siteId) {
-      this.warn(`No Site ID found in current directory.
+      warn(`No Site ID found in current directory.
 Run \`netlify link\` to connect to this folder to a site`)
       return false
     }
@@ -22,17 +22,17 @@ Run \`netlify link\` to connect to this folder to a site`)
       url = siteData.ssl_url || siteData.url
       log(`Opening "${siteData.name}" site url:`)
       log(`> ${url}`)
-    } catch (error) {
+    } catch (error_) {
       // unauthorized
-      if (error.status === 401) {
-        this.warn(`Log in with a different account or re-link to a site you have permission for`)
-        this.error(`Not authorized to view the currently linked site (${siteId})`)
+      if (error_.status === 401) {
+        warn(`Log in with a different account or re-link to a site you have permission for`)
+        error(`Not authorized to view the currently linked site (${siteId})`)
       }
-      this.error(error)
+      error(error_)
     }
 
     await openBrowser({ url })
-    this.exit()
+    exit()
   }
 }
 
