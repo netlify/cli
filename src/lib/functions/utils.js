32cfd9872514c8fb28a55c28e971c2e89a96f251
Kunal Kundu <kunal99kundu@gmail.com>
2021-09-22 19:36:04 +0530

    refactor: remove unused function detectAwsSdkError

--- src/lib/functions/utils.js
+++ src/lib/functions/utils.js
@@ -1,7 +1,5 @@
 const chalk = require('chalk')
 
-const { getLogMessage } = require('../log')
-
 const BASE_64_MIME_REGEXP = /image|audio|video|application\/pdf|application\/zip|applicaton\/octet-stream/i
 
 const DEFAULT_LAMBDA_OPTIONS = {
@@ -9,14 +7,6 @@ const DEFAULT_LAMBDA_OPTIONS = {
 }
 
 const SECONDS_TO_MILLISECONDS = 1000
-
-const detectAwsSdkError = ({ error, warn }) => {
-  const isAwsSdkError = error.errorMessage && error.errorMessage.includes("Cannot find module 'aws-sdk'")
-
-  if (isAwsSdkError) {
-    warn(getLogMessage('functions.missingAwsSdk'))
-  }
-}
 
 const formatLambdaError = (err) => chalk.red(`${err.errorType}: ${err.errorMessage}`)
 
@@ -27,7 +17,6 @@ const shouldBase64Encode = function (contentType) {
 const styleFunctionName = (name) => chalk.magenta(name)
 
 module.exports = {
-  detectAwsSdkError,
   DEFAULT_LAMBDA_OPTIONS,
   formatLambdaError,
   SECONDS_TO_MILLISECONDS,
