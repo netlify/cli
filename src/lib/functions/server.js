5a0705506248924d40d4fb749ca7ee617662531f
Emily Zhang <emilyzhang@users.noreply.github.com>
2021-10-18 10:17:43 -0700

    feat: support running builder functions locally (#3507)

--- src/lib/functions/server.js
+++ src/lib/functions/server.js
@@ -40,7 +40,7 @@ const buildClientContext = function (headers) {
 const createHandler = function ({ functionsRegistry }) {
   return async function handler(request, response) {
     // handle proxies without path re-writes (http-servr)
-    const cleanPath = request.path.replace(/^\/.netlify\/functions/, '')
+    const cleanPath = request.path.replace(/^\/.netlify\/(functions|builders)/, '')
     const functionName = cleanPath.split('/').find(Boolean)
     const func = functionsRegistry.get(functionName)
 
@@ -108,18 +108,30 @@ const createHandler = function ({ functionsRegistry }) {
     } else {
       const { error, result } = await func.invoke(event, clientContext)
 
+      // check for existence of metadata if this is a builder function
+      if (/^\/.netlify\/(builders)/.test(request.path) && (!result.metadata || !result.metadata.builder_function)) {
+        response.status(400).send({
+          message:
+            'Function is not an on-demand builder. See https://ntl.fyi/create-builder for how to convert a function to a builder.',
+        })
+        response.end()
+        return
+      }
+
       handleSynchronousFunction(error, result, response)
     }
   }
 }
 
-const getFunctionsServer = function ({ functionsRegistry, siteUrl, prefix }) {
+const getFunctionsServer = function ({ functionsRegistry, siteUrl, functionsPrefix, buildersPrefix }) {
   // performance optimization, load express on demand
   // eslint-disable-next-line node/global-require
   const express = require('express')
   // eslint-disable-next-line node/global-require
   const expressLogging = require('express-logging')
   const app = express()
+  const functionHandler = createHandler({ functionsRegistry })
+
   app.set('query parser', 'simple')
 
   app.use(
@@ -140,12 +152,22 @@ const getFunctionsServer = function ({ functionsRegistry, siteUrl, prefix }) {
     res.status(204).end()
   })
 
-  app.all(`${prefix}*`, createHandler({ functionsRegistry }))
+  app.all(`${functionsPrefix}*`, functionHandler)
+  app.all(`${buildersPrefix}*`, functionHandler)
 
   return app
 }
 
-const startFunctionsServer = async ({ config, settings, site, siteUrl, capabilities, timeouts, prefix = '' }) => {
+const startFunctionsServer = async ({
+  config,
+  settings,
+  site,
+  siteUrl,
+  capabilities,
+  timeouts,
+  functionsPrefix = '',
+  buildersPrefix = '',
+}) => {
   const internalFunctionsDir = await getInternalFunctionsDir({ base: site.root })
 
   // The order of the function directories matters. Leftmost directories take
@@ -165,7 +187,8 @@ const startFunctionsServer = async ({ config, settings, site, siteUrl, capabilit
     const server = getFunctionsServer({
       functionsRegistry,
       siteUrl,
-      prefix,
+      functionsPrefix,
+      buildersPrefix,
     })
 
     await startWebServer({ server, settings })
