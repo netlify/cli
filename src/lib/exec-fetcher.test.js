74a36a1e43b5e77a7cca3b3072f090af43e998d3
erezrokah <erezrokah@users.noreply.github.com>
2021-10-19 19:56:07 +0200

    chore: add environment variables

--- src/lib/exec-fetcher.test.js
+++ src/lib/exec-fetcher.test.js
@@ -1,21 +1,8 @@
-const path = require('path')
 const process = require('process')
 
 const test = require('ava')
-const tempDirectory = require('temp-dir')
-const { v4: uuid } = require('uuid')
 
-const { fetchLatestVersion, getExecName, shouldFetchLatestVersion } = require('./exec-fetcher')
-const fs = require('./fs')
-
-test.beforeEach((t) => {
-  const directory = path.join(tempDirectory, `netlify-cli-exec-fetcher`, uuid())
-  t.context.binPath = directory
-})
-
-test.afterEach(async (t) => {
-  await fs.rmdirRecursiveAsync(t.context.binPath)
-})
+const { getExecName } = require('./exec-fetcher')
 
 test(`should postix exec with .exe on windows`, (t) => {
   const execName = 'some-binary-file'
@@ -25,45 +12,3 @@ test(`should postix exec with .exe on windows`, (t) => {
     t.is(getExecName({ execName }), execName)
   }
 })
-
-const packages = [
-  // Disabled since failing on CI due to GitHub API limits when fetching releases
-  // TODO: Re-enabled when we can think of a solution
-  // {
-  //   packageName: 'traffic-mesh-agent',
-  //   execName: 'traffic-mesh',
-  //   execArgs: ['--version'],
-  //   pattern: '\\sv(.+)',
-  //   extension: 'zip',
-  // },
-]
-
-packages.forEach(({ execArgs, execName, extension, packageName, pattern }) => {
-  test(`${packageName} - should return true on empty directory`, async (t) => {
-    const { binPath } = t.context
-    const actual = await shouldFetchLatestVersion({ binPath, packageName, execName, execArgs, pattern })
-    t.is(actual, true)
-  })
-
-  test(`${packageName} - should return false after latest version is fetched`, async (t) => {
-    const { binPath } = t.context
-
-    await fetchLatestVersion({ packageName, execName, destination: binPath, extension })
-
-    const actual = await shouldFetchLatestVersion({ binPath, packageName, execName, execArgs, pattern })
-    t.is(actual, false)
-  })
-
-  test(`${packageName} - should download latest version on empty directory`, async (t) => {
-    const { binPath } = t.context
-
-    await fetchLatestVersion({ packageName, execName, destination: binPath, extension })
-
-    const execPath = path.join(binPath, getExecName({ execName }))
-    const stats = await fs.statAsync(execPath)
-    t.is(stats.size >= FILE_MIN_SIZE, true)
-  })
-})
-
-// 5 KiB
-const FILE_MIN_SIZE = 5e3
