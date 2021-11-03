fd880296d89ced594864ca0739fbc82cf597e666
ashalfarhan <ashalfarhan8@gmail.com>
2021-11-04 00:06:33 +0700

    feat: add transfer subcommand register

--- src/commands/env/index.js
+++ src/commands/env/index.js
@@ -20,6 +20,7 @@ EnvCommand.examples = [
   'netlify env:set VAR_NAME value',
   'netlify env:unset VAR_NAME',
   'netlify env:import fileName',
+  'netlify env:transfer <from-site-(id|name)> <to-site-(id|name)>',
 ]
 
 module.exports = EnvCommand
