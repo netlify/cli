e63caa33d5f78e0098729234d9bdc9c1c885dbe3
Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
2021-11-12 09:45:01 +0100

    fix: resemble proxy's base64 encoding logic
    see https://github.com/netlify/proxy/blob/main/pkg/functions/request.go#L119

--- src/lib/functions/utils.js
+++ src/lib/functions/utils.js
@@ -3,8 +3,6 @@ const chalk = require('chalk')
 const { warn } = require('../../utils/command-helpers')
 const { getLogMessage } = require('../log')
 
-const BASE_64_MIME_REGEXP = /image|audio|video|application\/pdf|application\/zip|applicaton\/octet-stream/i
-
 const DEFAULT_LAMBDA_OPTIONS = {
   verboseLevel: 3,
 }
@@ -21,8 +19,40 @@ const detectAwsSdkError = ({ error }) => {
 
 const formatLambdaError = (err) => chalk.red(`${err.errorType}: ${err.errorMessage}`)
 
+const exceptionsList = new Set([
+  'application/csp-report',
+  'application/graphql',
+  'application/json',
+  'application/javascript',
+  'application/x-www-form-urlencoded',
+  'application/x-ndjson',
+  'application/xml',
+])
+
+/**
+ * @param {string | undefined} contentType
+ * @returns {boolean}
+ */
 const shouldBase64Encode = function (contentType) {
-  return Boolean(contentType) && BASE_64_MIME_REGEXP.test(contentType)
+  if (!contentType) {
+    return true
+  }
+
+  contentType = contentType.toLowerCase()
+
+  if (contentType.startsWith('text/')) {
+    return false
+  }
+
+  if (contentType.endsWith('+json') || contentType.endsWith('+xml')) {
+    return false
+  }
+
+  if (exceptionsList.has(contentType)) {
+    return false
+  }
+
+  return true
 }
 
 const styleFunctionName = (name) => chalk.magenta(name)
