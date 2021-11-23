304239cb77eb9f1c71306dd42e14775d676d8c59
Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
2021-11-23 14:10:13 +0100

    chore: rename to scheduled.js

--- src/lib/functions/server.js
+++ src/lib/functions/server.js
@@ -6,7 +6,7 @@ const { NETLIFYDEVERR, NETLIFYDEVLOG, error: errorExit, getInternalFunctionsDir,
 const { handleBackgroundFunction, handleBackgroundFunctionResult } = require('./background')
 const { createFormSubmissionHandler } = require('./form-submissions-handler')
 const { FunctionsRegistry } = require('./registry')
-const { handleScheduledFunction, handleScheduledFunctionResult } = require('./schedule')
+const { handleScheduledFunction, handleScheduledFunctionResult } = require('./scheduled')
 const { handleSynchronousFunction } = require('./synchronous')
 const { shouldBase64Encode } = require('./utils')
 
