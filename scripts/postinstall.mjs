432066ce423117a10e29e94c5ef19d6331c9a9c0
Lukas Holzer <lukas.holzer@netlify.com>
2022-01-28 14:10:50 +0100

    chore: simplify script

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -5,29 +5,25 @@ import chalk from 'chalk'
 import { createMainCommand } from '../src/commands/index.mjs'
 import completion from '../src/lib/completion/index.js'
 
-const postInstall = () => {
-  // yarn plug and play seems to have an issue with reading an esm file by building up the cache.
-  // as yarn pnp analyzes everything inside the postinstall
-  // yarn pnp executes it out of a .yarn folder .yarn/unplugged/netlify-cli-file-fb026a3a6d/node_modules/netlify-cli/scripts/postinstall.js
-  if (!argv[1].includes('.yarn')) {
-    // create or update the autocompletion definition
-    const program = createMainCommand()
-    completion.createAutocompletion(program)
-  }
-
-  console.log('')
-  console.log(chalk.greenBright.bold.underline('Success! Netlify CLI has been installed!'))
-  console.log('')
-  console.log('Your device is now configured to use Netlify CLI to deploy and manage your Netlify sites.')
-  console.log('')
-  console.log('Next steps:')
-  console.log('')
-  console.log(`  ${chalk.cyanBright.bold('netlify init')}     Connect or create a Netlify site from current directory`)
-  console.log(`  ${chalk.cyanBright.bold('netlify deploy')}   Deploy the latest changes to your Netlify site`)
-  console.log('')
-  console.log(`For more information on the CLI run ${chalk.cyanBright.bold('netlify help')}`)
-  console.log(`Or visit the docs at ${chalk.cyanBright.bold('https://cli.netlify.com')}`)
-  console.log('')
+// yarn plug and play seems to have an issue with reading an esm file by building up the cache.
+// as yarn pnp analyzes everything inside the postinstall
+// yarn pnp executes it out of a .yarn folder .yarn/unplugged/netlify-cli-file-fb026a3a6d/node_modules/netlify-cli/scripts/postinstall.js
+if (!argv[1].includes('.yarn')) {
+  // create or update the autocompletion definition
+  const program = createMainCommand()
+  completion.createAutocompletion(program)
 }
 
-postInstall()
+console.log('')
+console.log(chalk.greenBright.bold.underline('Success! Netlify CLI has been installed!'))
+console.log('')
+console.log('Your device is now configured to use Netlify CLI to deploy and manage your Netlify sites.')
+console.log('')
+console.log('Next steps:')
+console.log('')
+console.log(`  ${chalk.cyanBright.bold('netlify init')}     Connect or create a Netlify site from current directory`)
+console.log(`  ${chalk.cyanBright.bold('netlify deploy')}   Deploy the latest changes to your Netlify site`)
+console.log('')
+console.log(`For more information on the CLI run ${chalk.cyanBright.bold('netlify help')}`)
+console.log(`Or visit the docs at ${chalk.cyanBright.bold('https://cli.netlify.com')}`)
+console.log('')
