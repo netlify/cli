2be9f326ca444b4ad2203ae75997aa36b328ef95
Alexander Karagulamos <karagulamos@gmail.com>
2022-01-31 14:45:18 +0100

    chore: add helpful comments to SF-TS template

--- src/functions-templates/typescript/scheduled-function/{{name}}.ts
+++ src/functions-templates/typescript/scheduled-function/{{name}}.ts
@@ -1,5 +1,14 @@
 import { schedule } from '@netlify/functions';
 
+// Sample function scheduled to run every minute using conventional cron syntax.
+//
+// Other supported extensions:
+// - @yearly: once a year, on January 1st 00:00 (0 0 1 1 *)
+// - @monthly: every month, on the first day of the month, at 00:00 (0 0 1 * *)
+// - @weekly: every Monday, 00:00 (0 0 * * 0)
+// - @daily: once a day, at 00:00 (0 0 * * *)
+// - @hourly: every hour, at 0 seconds (0 * * * *)
+//
 export const handler = schedule("* * * * *", () => {
     console.log(`Function executed at ${new Date()}.`);
 
