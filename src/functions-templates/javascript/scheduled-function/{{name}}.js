3a6dc959424a28d55f0e8ee59642db0c227b8a82
Karagulamos <karagulamos@gmail.com>
2022-01-31 12:27:55 +0100

    Update src/functions-templates/javascript/scheduled-function/{{name}}.js
    Co-authored-by: Eduardo Bouças <mail@eduardoboucas.com>

--- src/functions-templates/javascript/scheduled-function/{{name}}.js
+++ src/functions-templates/javascript/scheduled-function/{{name}}.js
@@ -3,7 +3,7 @@ const { schedule } = require('@netlify/functions')
 const formatAsDateTime = (date) => `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
 
 module.exports.handler = schedule('* * * * *', async () => {
-  const currentExecutionTime = formatAsDateTime(new Date(Date.now()))
+  const currentExecutionTime = formatAsDateTime(new Date())
   console.log(`Function executed at ${currentExecutionTime}.`)
 
   return {
