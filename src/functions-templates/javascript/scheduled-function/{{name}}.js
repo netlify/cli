9b25b532dfcc7b62142a3c4ed1445139860f4a15
Alexander Karagulamos <karagulamos@gmail.com>
2022-01-31 14:43:53 +0100

    chore: add helpful comments to SF-JS template

--- src/functions-templates/javascript/scheduled-function/{{name}}.js
+++ src/functions-templates/javascript/scheduled-function/{{name}}.js
@@ -1,5 +1,14 @@
 const { schedule } = require('@netlify/functions')
 
+// Sample function scheduled to run every minute using conventional cron syntax.
+//
+// Other supported extensions:
+// - @yearly: once a year, on January 1st 00:00 (0 0 1 1 *)
+// - @monthly: every month, on the first day of the month, at 00:00 (0 0 1 * *)
+// - @weekly: every Monday, 00:00 (0 0 * * 0)
+// - @daily: once a day, at 00:00 (0 0 * * *)
+// - @hourly: every hour, at 0 seconds (0 * * * *)
+//
 module.exports.handler = schedule('* * * * *', async () => {
   console.log(`Function executed at ${new Date()}.`)
 
