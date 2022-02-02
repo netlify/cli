3055019cfe8e06a3aac58ef2347aac2ed6cb6390
Alexander Karagulamos <karagulamos@gmail.com>
2022-02-02 18:56:14 +0100

    chore: provide meaningful comments and examples

--- src/functions-templates/typescript/scheduled-function/{{name}}.ts
+++ src/functions-templates/typescript/scheduled-function/{{name}}.ts
@@ -1,16 +1,10 @@
 import { schedule } from '@netlify/functions';
 
-// Sample function scheduled to run every minute using conventional cron syntax.
-//
-// Other supported extensions:
-// - @yearly: once a year, on January 1st 00:00 (0 0 1 1 *)
-// - @monthly: every month, on the first day of the month, at 00:00 (0 0 1 * *)
-// - @weekly: every Monday, 00:00 (0 0 * * 0)
-// - @daily: once a day, at 00:00 (0 0 * * *)
-// - @hourly: every hour, at 0 seconds (0 * * * *)
-//
-export const handler = schedule("* * * * *", () => {
-    console.log(`Function executed at ${new Date()}.`);
+// To learn about scheduled functions and supported cron extensions, 
+// see: https://ntl.fyi/sched-func
+export const handler = schedule("@hourly", (event) => {
+    const eventBody = JSON.parse(event.body);
+    console.log(`Next function run at ${eventBody.next_run}.`);
 
     return {
         statusCode: 200
