5aae39f66210aab22087601d62f2a61e32916e4a
Sean Grove <sean@bushi.do>
2022-02-13 13:11:42 -0800

    feat: use fuzzy search list for graph:handler

--- src/commands/graph/graph-handler.js
+++ src/commands/graph/graph-handler.js
@@ -1,20 +1,34 @@
 // @ts-check
+const inquirer = require('inquirer')
+const inquirerFuzzySearchPrompt = require('inquirer-search-list')
+const { GraphQL } = require('netlify-onegraph-internal')
+
 const {
   buildSchema,
+  defaultExampleOperationsDoc,
+  extractFunctionsFromOperationDoc,
   generateHandlerByOperationName,
   getNetlifyGraphConfig,
+  readGraphQLOperationsSourceFile,
   readGraphQLSchemaFile,
 } = require('../../lib/one-graph/cli-netlify-graph')
 const { error, log } = require('../../utils')
 
+const { parse } = GraphQL
+
+// @ts-ignore
+inquirer.registerPrompt('search-list', inquirerFuzzySearchPrompt);
+
+
 /**
  * Creates the `netlify graph:handler` command
- * @param {string} operationName
+ * @param {string} userOperationName
  * @param {import('commander').OptionValues} options
  * @param {import('../base-command').BaseCommand} command
  * @returns
  */
-const graphHandler = async (operationName, options, command) => {
+const graphHandler = async (userOperationName, options, command) => {
+
   const netlifyGraphConfig = await getNetlifyGraphConfig({ command, options })
 
   const schemaString = readGraphQLSchemaFile(netlifyGraphConfig)
@@ -31,6 +45,47 @@ const graphHandler = async (operationName, options, command) => {
     error(`Failed to parse Netlify GraphQL schema`)
   }
 
+  let operationName
+  if (!userOperationName) {
+    try {
+      let currentOperationsDoc = readGraphQLOperationsSourceFile(netlifyGraphConfig)
+      if (currentOperationsDoc.trim().length === 0) {
+        currentOperationsDoc = defaultExampleOperationsDoc
+      }
+
+      const parsedDoc = parse(currentOperationsDoc)
+      const { functions } = extractFunctionsFromOperationDoc(parsedDoc)
+
+      const sorted = Object.values(functions)
+        .sort((aItem, bItem) => aItem.operationName.localeCompare(bItem.operationName))
+
+      const perPage = 50
+
+      const { selectedOperation: selectedOperationName } = await inquirer.prompt([
+        {
+          type: 'search-list',
+          name: 'selectedOperation',
+          message: 'For which operation would you like to generate a handler?',
+          paginated: true,
+          pageSize: perPage,
+          choices: sorted.map((operation) => ({ name: `${operation.operationName} (${operation.kind})`, value: operation.operationName })),
+        },
+      ])
+
+      if (selectedOperationName) {
+        operationName = selectedOperationName
+      }
+
+
+    } catch (parseError) {
+      parseError(`Error parsing operations library: ${parseError}`)
+    }
+  }
+
+  if (!operationName) {
+    error(`No operation name provided`)
+  }
+
   generateHandlerByOperationName({ logger: log, netlifyGraphConfig, schema, operationName, handlerOptions: {} })
 }
 
@@ -42,7 +97,7 @@ const graphHandler = async (operationName, options, command) => {
 const createGraphHandlerCommand = (program) =>
   program
     .command('graph:handler')
-    .argument('<name>', 'Operation name')
+    .argument('[name]', 'Operation name')
     .description(
       'Generate a handler for a Graph operation given its name. See `graph:operations` for a list of operations.',
     )
