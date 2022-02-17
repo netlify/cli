316e367521c099cf62b5dc7332ed095545c5adb3
Ibrahima <ibracool99@gmail.com>
2022-02-17 16:34:09 +0100

    chore: comma separated list for env option

--- src/commands/dev/dev.js
+++ src/commands/dev/dev.js
@@ -465,7 +465,7 @@ const createDevCommand = (program) => {
     .option('-o ,--offline', 'disables any features that require network access')
     .option('-l, --live', 'start a public live session', false)
     .option('--functionsPort <port>', 'port of functions server', (value) => Number.parseInt(value))
-    .option('-e, --env <env>', 'set the env files priority', (value) =>value.split(','))
+    .option('-e, --env <env>', 'set the env files priority', (value) => value.split(','))
     .addOption(
       new Option('--staticServerPort <port>', 'port of the static app server used when no framework is detected')
         .argParser((value) => Number.parseInt(value))
