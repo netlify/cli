ce2f68fc34450e72b72bf2fdb67e344b86600ab7
Ibrahima <ibracool99@gmail.com>
2022-02-18 21:31:15 +0100

    refactor: env_files -> envFiles

--- tests/integration/300.command.dev.test.js
+++ tests/integration/300.command.dev.test.js
@@ -262,7 +262,7 @@ testMatrix.forEach(({ args }) => {
     await withSiteBuilder('site-with-env-files', async (builder) => {
       builder
         .withNetlifyToml({
-          config: { env: { env_files: ['.env.development', '.env'] }, functions: { directory: 'functions' } },
+          config: { env: { envFiles: ['.env.development', '.env'] }, functions: { directory: 'functions' } },
         })
         .withEnvFile({ path: '.env.development', env: { TEST: 'FROM_ENV_DEV_FILE' } })
         .withEnvFile({ path: '.env', env: { TEST: 'FROM_ENV_FILE' } })
