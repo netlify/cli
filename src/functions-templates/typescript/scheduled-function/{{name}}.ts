b74cf05f47bc4fef62465f36241e15881af6f2bb
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-06-01 16:29:32 +0100

    fix: fix scheduled function template to use async (#4658)
    Without the async keyword, typescript instantly errors as the handler (according to the types
    of `schedule()` ) needs to return a Promise.

--- src/functions-templates/typescript/scheduled-function/{{name}}.ts
+++ src/functions-templates/typescript/scheduled-function/{{name}}.ts
@@ -2,7 +2,7 @@ import { schedule } from '@netlify/functions';
 
 // To learn about scheduled functions and supported cron extensions, 
 // see: https://ntl.fyi/sched-func
-export const handler = schedule("@hourly", (event) => {
+export const handler = schedule("@hourly", async (event) => {
     const eventBody = JSON.parse(event.body);
     console.log(`Next function run at ${eventBody.next_run}.`);
 
