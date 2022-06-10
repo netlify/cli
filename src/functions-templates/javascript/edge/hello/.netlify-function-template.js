aae2dc77898791eb7534133a71e0391c190b761c
Jenae Janzen <101715009+jenae-janzen@users.noreply.github.com>
2022-06-10 12:52:31 -0400

    feat: add edge func template

--- src/functions-templates/javascript/edge/hello/.netlify-function-template.js
+++ src/functions-templates/javascript/edge/hello/.netlify-function-template.js
@@ -1,4 +1,4 @@
 module.exports = {
-    name: 'hello',
-    description: 'Basic function demonstrating an edge function',
+  name: 'hello',
+  description: 'Basic function demonstrating an edge function',
 }
--- src/functions-templates/javascript/edge/hello/{{name}}.js
+++ src/functions-templates/javascript/edge/hello/{{name}}.js
@@ -1,3 +1,4 @@
-const hello = async (Request) => new Response("Hello, World!", { headers: { "content-type": "text/html" }});
+// eslint-disable-next-line no-undef
+const hello = async () => new Response('Hello, World!', { headers: { 'content-type': 'text/html' } })
 
 module.exports = { hello }
