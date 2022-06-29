aecafa3b3b8847b8bba3ffdf4ffe4d0d58061be0
Jason Barry <jb@netlify.com>
2022-06-29 10:12:52 -0700

    chore: add param defaults to helper methods

--- src/utils/env/index.js
+++ src/utils/env/index.js
@@ -3,7 +3,7 @@
  * @param {object} env - The site's env as it exists in Mongo
  * @returns {Array<object>} The array of Envelope env vars
  */
-const translateFromMongoToEnvelope = (env) => {
+const translateFromMongoToEnvelope = (env = {}) => {
   const envVars = Object.entries(env).map(([key, value]) => ({
     key,
     scopes: ['builds', 'functions', 'runtime', 'post_processing'],
@@ -23,7 +23,7 @@ const translateFromMongoToEnvelope = (env) => {
  * @param {Array<object>} envVars - The array of Envelope env vars
  * @returns {object} The env object as compatible with Mongo
  */
-const translateFromEnvelopeToMongo = (envVars) =>
+const translateFromEnvelopeToMongo = (envVars = []) =>
   envVars
     .sort((left, right) => (left.key.toLowerCase() < right.key.toLowerCase() ? -1 : 1))
     .reduce((acc, cur) => {
