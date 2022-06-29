47a33bf9f80abe7df834cfe579b715debefc2587
Jason Barry <jb@netlify.com>
2022-06-29 10:37:22 -0700

    feat: support envelope in env:migrate

--- src/commands/env/env-migrate.js
+++ src/commands/env/env-migrate.js
@@ -2,7 +2,13 @@
 
 const { isEmpty } = require('lodash')
 
-const { chalk, error: logError, log } = require('../../utils')
+const {
+  chalk,
+  error: logError,
+  log,
+  translateFromEnvelopeToMongo,
+  translateFromMongoToEnvelope,
+} = require('../../utils')
 
 const safeGetSite = async (api, siteId) => {
   try {
@@ -51,6 +57,27 @@ const envMigrate = async (options, command) => {
     return false
   }
 
+  // determine if siteFrom and/or siteTo is on Envelope
+  let method
+  if (!siteFrom.use_envelope && !siteTo.use_envelope) {
+    method = mongoToMongo
+  } else if (!siteFrom.use_envelope && siteTo.use_envelope) {
+    method = mongoToEnvelope
+  } else if (siteFrom.use_envelope && !siteTo.use_envelope) {
+    method = envelopeToMongo
+  } else {
+    method = envelopeToEnvelope
+  }
+  await method({ api, siteFrom, siteTo })
+
+  log(
+    `Successfully migrated environment variables from ${chalk.greenBright(siteFrom.name)} to ${chalk.greenBright(
+      siteTo.name,
+    )}`,
+  )
+}
+
+const mongoToMongo = async ({ api, siteFrom, siteTo }) => {
   const [
     {
       build_settings: { env: envFrom = {} },
@@ -73,19 +100,95 @@ const envMigrate = async (options, command) => {
 
   // Apply environment variable updates
   await api.updateSite({
-    siteId: siteId.to,
+    siteId: siteTo.id,
     body: {
       build_settings: {
         env: mergedEnv,
       },
     },
   })
+}
 
-  log(
-    `Successfully migrated environment variables from ${chalk.greenBright(siteFrom.name)} to ${chalk.greenBright(
-      siteTo.name,
-    )}`,
-  )
+const mongoToEnvelope = async ({ api, siteFrom, siteTo }) => {
+  const envFrom = siteFrom.build_settings.env || {}
+  const keysFrom = Object.keys(envFrom)
+
+  if (isEmpty(envFrom)) {
+    log(`${chalk.greenBright(siteFrom.name)} has no environment variables, nothing to migrate`)
+    return false
+  }
+
+  const accountId = siteTo.account_slug
+  const siteId = siteTo.id
+
+  const envelopeTo = await api.getEnvVars({ accountId, siteId })
+
+  const keysToDelete = envelopeTo.filter(({ key }) => keysFrom.includes(key))
+  // delete marked env vars in parallel
+  await Promise.all(keysToDelete.map((key) => api.deleteEnvVar({ accountId, siteId, key })))
+
+  // hit create endpoint
+  const body = translateFromMongoToEnvelope(envFrom)
+  try {
+    await api.createEnvVars({ accountId, siteId, body })
+  } catch (error) {
+    throw error.json.msg
+  }
+}
+
+const envelopeToMongo = async ({ api, siteFrom, siteTo }) => {
+  const envelopeVariables = await api.getEnvVars({ accountId: siteFrom.account_slug, siteId: siteFrom.id })
+  const envFrom = translateFromEnvelopeToMongo(envelopeVariables)
+
+  if (isEmpty(envFrom)) {
+    log(`${chalk.greenBright(siteFrom.name)} has no environment variables, nothing to migrate`)
+    return false
+  }
+
+  const envTo = siteTo.build_settings.env || {}
+
+  // Merge from site A to site B
+  const mergedEnv = {
+    ...envTo,
+    ...envFrom,
+  }
+
+  // Apply environment variable updates
+  await api.updateSite({
+    siteId: siteTo.id,
+    body: {
+      build_settings: {
+        env: mergedEnv,
+      },
+    },
+  })
+}
+
+const envelopeToEnvelope = async ({ api, siteFrom, siteTo }) => {
+  const [envelopeFrom, envelopeTo] = await Promise.all([
+    api.getEnvVars({ accountId: siteFrom.account_slug, siteId: siteFrom.id }),
+    api.getEnvVars({ accountId: siteTo.account_slug, siteId: siteTo.id }),
+  ])
+
+  const keysFrom = envelopeFrom.map(({ key }) => key)
+
+  if (isEmpty(keysFrom)) {
+    log(`${chalk.greenBright(siteFrom.name)} has no environment variables, nothing to migrate`)
+    return false
+  }
+
+  const accountId = siteTo.account_slug
+  const siteId = siteTo.id
+  const keysToDelete = envelopeTo.filter(({ key }) => keysFrom.includes(key))
+  // delete marked env vars in parallel
+  await Promise.all(keysToDelete.map((key) => api.deleteEnvVar({ accountId, siteId, key })))
+
+  // hit create endpoint
+  try {
+    await api.createEnvVars({ accountId, siteId, body: envelopeFrom })
+  } catch (error) {
+    throw error.json.msg
+  }
 }
 
 /**
