20f14f842419dd0e4984c58c394d56f6a36f34eb
Jordan Whistler <whistler.5@osu.edu>
2022-07-01 11:51:55 -0400

    fix: refactor tests

--- tests/unit/lib/functions/registry.test.js
+++ tests/unit/lib/functions/registry.test.js
@@ -1,4 +1,5 @@
-const { mkdirSync, mkdtempSync, writeFileSync } = require('fs')
+// Node 12 requires rmdir, when upping to v14+ use rm from 'fs/promises'
+const { mkdir, mkdtemp, rm, writeFile } = require('fs').promises
 const { tmpdir } = require('os')
 const { join } = require('path')
 
@@ -7,14 +8,27 @@ const sinon = require('sinon')
 
 const { FunctionsRegistry } = require('../../../../src/lib/functions/registry')
 
-test('registry should only pass functions config to zip it ship it', async (t) => {
-  const projectRoot = mkdtempSync(join(tmpdir(), 'functions-project-root'))
-  const functionsDirectory = join(projectRoot, 'functions')
-  mkdirSync(functionsDirectory)
+let projectRoot
+let functionsDirectory
+
+const createFunction = async () => {
+  projectRoot = await mkdtemp(join(tmpdir(), 'functions-project-root'))
+  functionsDirectory = join(projectRoot, 'functions')
+  await mkdir(functionsDirectory)
   const mainFile = join(functionsDirectory, 'horse.js')
 
-  writeFileSync(mainFile, `exports.handler = async (event) => ({ statusCode: 200, body: event.rawUrl })`)
+  await writeFile(mainFile, `exports.handler = async (event) => ({ statusCode: 200, body: event.rawUrl })`)
+}
 
+test.beforeEach(async () => {
+  await createFunction()
+})
+
+test.afterEach(async () => {
+  await rm(projectRoot, { recursive: true, force: true })
+})
+
+test('registry should only pass functions config to zip-it-and-ship-it', async (t) => {
   const functionsRegistry = new FunctionsRegistry({
     projectRoot,
     config: { functions: { '*': {} }, plugins: ['test'] },
