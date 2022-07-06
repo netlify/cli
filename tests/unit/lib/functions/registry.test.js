c3d27bc6418720891fc4a4f669b8a7d4f201f8a5
Jordan Whistler <63815686+JWhist@users.noreply.github.com>
2022-07-06 13:44:23 -0400

    Update tests/unit/lib/functions/registry.test.js
    Co-authored-by: Daniel Tschinder <231804+danez@users.noreply.github.com>

--- tests/unit/lib/functions/registry.test.js
+++ tests/unit/lib/functions/registry.test.js
@@ -8,28 +8,25 @@ const sinon = require('sinon')
 const { FunctionsRegistry } = require('../../../../src/lib/functions/registry')
 
 test('registry should only pass functions config to zip-it-and-ship-it', async (t) => {
-  const projectRoot = await mkdtemp(join(tmpdir(), 'functions-project-root'))
-  const functionsDirectory = join(projectRoot, 'functions')
-  await mkdir(functionsDirectory)
-  const mainFile = join(functionsDirectory, 'horse.js')
-
-  await writeFile(mainFile, `exports.handler = async (event) => ({ statusCode: 200, body: event.rawUrl })`)
   const functionsRegistry = new FunctionsRegistry({
-    projectRoot,
+    projectRoot: '/projectRoot',
     config: { functions: { '*': {} }, plugins: ['test'] },
-    timeouts: { syncFunctions: 1, backgroundFunctions: 1 },
-    // eslint-disable-next-line no-magic-numbers
-    settings: { port: 8888 },
   })
-  // To verify that only the functions config is passed to zip-it-ship-it
-  sinon.spy(functionsRegistry, 'listFunctions')
-
+  const prepareDirectoryScanStub = sinon.stub(FunctionsRegistry, 'prepareDirectoryScan')
+  const setupDirectoryWatcherStub = sinon.stub(functionsRegistry, 'setupDirectoryWatcher')
+   // To verify that only the functions config is passed to zip-it-ship-it
+  const listFunctionsStub = sinon.stub(functionsRegistry, 'listFunctions')
+  listFunctionsStub.returns(Promise.resolved([]))
+  
   await functionsRegistry.scan([functionsDirectory])
 
-  const spyCall = functionsRegistry.listFunctions.getCall(0)
+  const spyCall = listFunctionsStub.getCall(0)
 
   t.is(spyCall.lastArg.config, functionsRegistry.config.functions)
+  
   t.teardown(async () => {
-    await rm(projectRoot, { recursive: true, force: true })
+    listFunctionsStub.restore()
+    setupDirectoryWatcherStub.restore()
+    prepareDirectoryScanStub.restore()
   })
 })
