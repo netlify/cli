b950bb205f4e679d7cdab5a9ba314fcc0b09b478
Jason Barry <jb@netlify.com>
2022-07-20 15:53:36 -0700

    feat: support scopes and contexts in env:list

--- src/utils/env/index.js
+++ src/utils/env/index.js
@@ -1,3 +1,100 @@
+const findValueFromContext = (values, context) => values.find((val) => [context, 'all'].includes(val.context))
+
+const getRawEnvelope = async function ({ accountId, api, siteId }) {
+  if (accountId === undefined) {
+    return {}
+  }
+  try {
+    const envelopeItems = await api.getEnvVars({ accountId, siteId })
+    return envelopeItems
+  } catch {
+    // Collaborators aren't allowed to read shared env vars,
+    // so return an empty array in that case
+    return []
+  }
+}
+
+const getFilteredAndSortedEnvelope = function ({ context = 'dev', envelopeItems = [], scope = 'any' }) {
+  return (
+    envelopeItems
+      // filter by context
+      .filter(({ values }) => Boolean(findValueFromContext(values, context)))
+      // filter by scope
+      .filter(({ scopes }) => (scope === 'any' ? true : scopes.includes(scope)))
+      // sort alphabetically, case insensitive
+      .sort((left, right) => (left.key.toLowerCase() < right.key.toLowerCase() ? -1 : 1))
+  )
+}
+
+/**
+ *
+ * Outputs in a format like
+ * {
+ *   FOO: 'bar',
+ *   BAZ: 'bang',
+ * }
+ *
+ **/
+const getEnvelopeDictionary = function ({ context = 'dev', envelopeItems = [], scope = 'any' }) {
+  return getFilteredAndSortedEnvelope({ context, envelopeItems, scope }).reduce((acc, cur) => {
+    const { value } = findValueFromContext(cur.values, context)
+    return {
+      ...acc,
+      [cur.key]: value,
+    }
+  }, {})
+}
+
+/**
+ *
+ * Outputs in a format like
+ * {
+ *   FOO: {
+ *     context: 'dev',
+ *     scopes: ['builds', 'functions'],
+ *     value: 'bar',
+ *   },
+ *   BAZ: {
+ *     context: 'dev',
+ *     scopes: ['functions'],
+ *     value: 'bang',
+ *   },
+ * }
+ *
+ **/
+const getEnvelopeMetadata = function ({ context = 'dev', envelopeItems = [], scope = 'any' }) {
+  return getFilteredAndSortedEnvelope({ context, envelopeItems, scope }).reduce((acc, cur) => {
+    const { value } = findValueFromContext(cur.values, context)
+    return {
+      ...acc,
+      [cur.key]: {
+        context,
+        scopes: cur.scopes,
+        value,
+      },
+    }
+  }, {})
+}
+
+const getScopes = (scopes) => {
+  const AVAILABLE_SCOPES = {
+    builds: 'Builds',
+    functions: 'Functions',
+    post_processing: 'Post processing',
+    runtime: 'Runtime',
+  }
+  if (!scopes) {
+    // if `scopes` is not available, the env var comes from netlify.toml
+    // env vars specified in netlify.toml are present in the `builds` and `post_processing` scope
+    return 'Builds, Post processing'
+  }
+  if (scopes.length === Object.keys(AVAILABLE_SCOPES).length) {
+    // shorthand instead of listing every available scope
+    return 'All'
+  }
+  return scopes.map((scope) => AVAILABLE_SCOPES[scope]).join(', ')
+}
+
 /**
  * Translates a Mongo env into an Envelope env
  * @param {object} env - The site's env as it exists in Mongo
@@ -38,6 +135,10 @@ const translateFromEnvelopeToMongo = (envVars = []) =>
     }, {})
 
 module.exports = {
-  translateFromMongoToEnvelope,
+  getEnvelopeDictionary,
+  getEnvelopeMetadata,
+  getRawEnvelope,
+  getScopes,
   translateFromEnvelopeToMongo,
+  translateFromMongoToEnvelope,
 }
