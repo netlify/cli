41f6a782329dd89072f62dba643bfa855c9d2770
Eduardo Bouças <mail@eduardoboucas.com>
2022-08-19 12:53:29 +0100

    chore: update test

--- tests/integration/420.command.init.test.js
+++ tests/integration/420.command.init.test.js
@@ -11,6 +11,8 @@ const { CONFIRM, DOWN, answerWithValue, handleQuestions } = require('./utils/han
 const { withMockApi } = require('./utils/mock-api')
 const { withSiteBuilder } = require('./utils/site-builder')
 
+const defaultFunctionsDirectory = 'netlify/functions'
+
 // TODO: Flaky tests enable once fixed
 /**
  * As some of the tests are flaky on windows machines I will skip them for now
@@ -90,7 +92,7 @@ test('netlify init existing site', async (t) => {
           provider: 'manual',
           repo_branch: 'main',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: 'netlify/functions',
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
@@ -112,20 +114,20 @@ test('netlify init existing site', async (t) => {
 
       await childProcess
 
-      await assertNetlifyToml(t, builder.directory, { command, functions: 'netlify/functions', publish })
+      await assertNetlifyToml(t, builder.directory, { command, functions: defaultFunctionsDirectory, publish })
     })
   })
 })
 
 test('netlify init new site', async (t) => {
-  const [command, publish, functions] = ['custom-build-command', 'custom-publish', 'custom-functions']
+  const [command, publish] = ['custom-build-command', 'custom-publish']
   const initQuestions = [
     {
       question: 'Create & configure a new site',
       answer: answerWithValue(DOWN),
     },
     { question: 'Team: (Use arrow keys)', answer: CONFIRM },
-    { question: 'Site name (optional)', answer: answerWithValue('test-site-name') },
+    { question: 'Site name (you can change it later)', answer: answerWithValue('test-site-name') },
     {
       question: 'Your build command (hugo build/yarn run build/etc)',
       answer: answerWithValue(command),
@@ -133,10 +135,6 @@ test('netlify init new site', async (t) => {
     {
       question: 'Directory to deploy (blank for current dir)',
       answer: answerWithValue(publish),
-    },
-    {
-      question: 'Netlify functions folder',
-      answer: answerWithValue(functions),
     },
     {
       question: 'No netlify.toml detected',
@@ -188,7 +186,7 @@ test('netlify init new site', async (t) => {
           provider: 'manual',
           repo_branch: 'main',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: functions,
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
@@ -209,20 +207,20 @@ test('netlify init new site', async (t) => {
 
       await childProcess
 
-      await assertNetlifyToml(t, builder.directory, { command, functions, publish })
+      await assertNetlifyToml(t, builder.directory, { command, functions: defaultFunctionsDirectory, publish })
     })
   })
 })
 
 test('netlify init new Next.js site', async (t) => {
-  const [command, publish, functions] = ['custom-build-command', 'custom-publish', 'custom-functions']
+  const [command, publish] = ['custom-build-command', 'custom-publish']
   const initQuestions = [
     {
       question: 'Create & configure a new site',
       answer: answerWithValue(DOWN),
     },
     { question: 'Team: (Use arrow keys)', answer: CONFIRM },
-    { question: 'Site name (optional)', answer: answerWithValue('test-site-name') },
+    { question: 'Site name (you can change it later)', answer: answerWithValue('test-site-name') },
     {
       question: 'Your build command (hugo build/yarn run build/etc)',
       answer: answerWithValue(command),
@@ -232,10 +230,6 @@ test('netlify init new Next.js site', async (t) => {
       answer: answerWithValue(publish),
     },
     {
-      question: 'Netlify functions folder',
-      answer: answerWithValue(functions),
-    },
-    {
       question: 'OK to install',
       answer: CONFIRM,
     },
@@ -290,7 +284,7 @@ test('netlify init new Next.js site', async (t) => {
           provider: 'manual',
           repo_branch: 'main',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: functions,
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
@@ -313,20 +307,20 @@ test('netlify init new Next.js site', async (t) => {
 
       await childProcess
 
-      await assertNetlifyToml(t, builder.directory, { command, functions, publish })
+      await assertNetlifyToml(t, builder.directory, { command, functions: defaultFunctionsDirectory, publish })
     })
   })
 })
 
 test('netlify init new Next.js site with correct default build directory and build command', async (t) => {
-  const [command, publish, functions] = ['next build', '.next', 'custom-functions']
+  const [command, publish] = ['next build', '.next']
   const initQuestions = [
     {
       question: 'Create & configure a new site',
       answer: answerWithValue(DOWN),
     },
     { question: 'Team: (Use arrow keys)', answer: CONFIRM },
-    { question: 'Site name (optional)', answer: answerWithValue('test-site-name') },
+    { question: 'Site name (you can change it later)', answer: answerWithValue('test-site-name') },
     {
       question: 'Your build command (hugo build/yarn run build/etc)',
       answer: CONFIRM,
@@ -336,10 +330,6 @@ test('netlify init new Next.js site with correct default build directory and bui
       answer: CONFIRM,
     },
     {
-      question: 'Netlify functions folder',
-      answer: answerWithValue(functions),
-    },
-    {
       question: 'OK to install',
       answer: CONFIRM,
     },
@@ -394,7 +384,7 @@ test('netlify init new Next.js site with correct default build directory and bui
           provider: 'manual',
           repo_branch: 'main',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: functions,
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
@@ -417,7 +407,7 @@ test('netlify init new Next.js site with correct default build directory and bui
 
       await childProcess
 
-      await assertNetlifyToml(t, builder.directory, { command, functions, publish })
+      await assertNetlifyToml(t, builder.directory, { command, functions: defaultFunctionsDirectory, publish })
     })
   })
 })
@@ -512,14 +502,14 @@ test('netlify init existing Next.js site with existing plugins', async () => {
 })
 
 test('netlify init new Gatsby site with correct default build directory and build command', async (t) => {
-  const [command, publish, functions] = ['gatsby build', 'public', 'custom-functions']
+  const [command, publish] = ['gatsby build', 'public']
   const initQuestions = [
     {
       question: 'Create & configure a new site',
       answer: answerWithValue(DOWN),
     },
     { question: 'Team: (Use arrow keys)', answer: CONFIRM },
-    { question: 'Site name (optional)', answer: answerWithValue('test-site-name') },
+    { question: 'Site name (you can change it later)', answer: answerWithValue('test-site-name') },
     {
       question: 'Your build command (hugo build/yarn run build/etc)',
       answer: CONFIRM,
@@ -527,10 +517,6 @@ test('netlify init new Gatsby site with correct default build directory and buil
     {
       question: 'Directory to deploy (blank for current dir)',
       answer: CONFIRM,
-    },
-    {
-      question: 'Netlify functions folder',
-      answer: answerWithValue(functions),
     },
     {
       question: 'OK to install',
@@ -587,7 +573,7 @@ test('netlify init new Gatsby site with correct default build directory and buil
           provider: 'manual',
           repo_branch: 'main',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: functions,
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
@@ -614,7 +600,7 @@ test('netlify init new Gatsby site with correct default build directory and buil
 
       await childProcess
 
-      await assertNetlifyToml(t, builder.directory, { command, functions, publish })
+      await assertNetlifyToml(t, builder.directory, { command, functions: defaultFunctionsDirectory, publish })
     })
   })
 })
@@ -626,7 +612,7 @@ windowsSkip('netlify init monorepo root and sub directory without netlify.toml',
       answer: answerWithValue(DOWN),
     },
     { question: 'Team: (Use arrow keys)', answer: CONFIRM },
-    { question: 'Site name (optional)', answer: answerWithValue('test-site-name') },
+    { question: 'Site name (you can change it later)', answer: answerWithValue('test-site-name') },
     {
       question: 'Base directory (e.g. projects/frontend):',
       answer: CONFIRM,
@@ -693,7 +679,7 @@ windowsSkip('netlify init monorepo root and sub directory without netlify.toml',
           repo_branch: 'main',
           base: 'projects/project1',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: 'netlify/functions',
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
@@ -732,7 +718,7 @@ windowsSkip('netlify init monorepo root and sub directory without netlify.toml',
 
       await assertNetlifyToml(t, `${builder.directory}/projects/project1`, {
         command: '# no build command',
-        functions: 'netlify/functions',
+        functions: defaultFunctionsDirectory,
         publish: '.',
       })
     })
@@ -746,7 +732,7 @@ test('netlify init monorepo root with netlify.toml, subdirectory without netlify
       answer: answerWithValue(DOWN),
     },
     { question: 'Team: (Use arrow keys)', answer: CONFIRM },
-    { question: 'Site name (optional)', answer: answerWithValue('test-site-name') },
+    { question: 'Site name (you can change it later)', answer: answerWithValue('test-site-name') },
     {
       question: 'Base directory (e.g. projects/frontend):',
       answer: CONFIRM,
@@ -814,7 +800,7 @@ test('netlify init monorepo root with netlify.toml, subdirectory without netlify
           repo_branch: 'main',
           base: 'projects/project2',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: 'netlify/functions',
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
@@ -854,7 +840,7 @@ test('netlify init monorepo root with netlify.toml, subdirectory without netlify
 
       await assertNetlifyToml(t, `${builder.directory}/projects/project2`, {
         command: '# no build command',
-        functions: 'netlify/functions',
+        functions: defaultFunctionsDirectory,
         publish: '.',
       })
     })
@@ -868,7 +854,7 @@ windowsSkip('netlify init monorepo root and sub directory with netlify.toml', as
       answer: answerWithValue(DOWN),
     },
     { question: 'Team: (Use arrow keys)', answer: CONFIRM },
-    { question: 'Site name (optional)', answer: answerWithValue('test-site-name') },
+    { question: 'Site name (you can change it later)', answer: answerWithValue('test-site-name') },
     {
       question: 'Base directory (e.g. projects/frontend):',
       answer: CONFIRM,
@@ -932,7 +918,7 @@ windowsSkip('netlify init monorepo root and sub directory with netlify.toml', as
           repo_branch: 'main',
           base: 'projects/project2',
           repo_path: 'git@github.com:owner/repo.git',
-          functions_dir: 'netlify/functions',
+          functions_dir: defaultFunctionsDirectory,
         },
       },
     },
