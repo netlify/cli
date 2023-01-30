5eff7c1931de4e0e4d21fce09631b3ffbd484293
Kunal Kundu <kunal99kundu@gmail.com>
2023-01-30 16:51:12 +0530

    fix: update got & remove outdated workaround (#5437)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- tests/integration/utils/got.cjs
+++ tests/integration/utils/got.cjs
@@ -1,15 +1,5 @@
 const got = require('got')
 
-const STATUS_CODE_LIMIT_OK = 299
-const STATUS_CODE_LIMIT_REDIRECT = 399
-
-const isResponseOk = (response) => {
-  const { statusCode } = response
-  const limitStatusCode = response.request.options.followRedirect ? STATUS_CODE_LIMIT_OK : STATUS_CODE_LIMIT_REDIRECT
-
-  return (statusCode >= 200 && statusCode <= limitStatusCode) || statusCode === 304
-}
-
 const TIMEOUT = 3e5
 
 // Default got retry status code with the addition of 403
@@ -20,19 +10,6 @@ const extendedGot = got.extend({
     statusCodes: STATUS_CODE,
   },
   timeout: TIMEOUT,
-  // TODO: remove when https://github.com/sindresorhus/got/issues/1489 is fixed
-  // see https://github.com/sindresorhus/got/issues/1489#issuecomment-805485731
-  hooks: {
-    afterResponse: [
-      (response) => {
-        if (isResponseOk(response)) {
-          response.request.destroy()
-        }
-
-        return response
-      },
-    ],
-  },
 })
 
 module.exports = extendedGot
