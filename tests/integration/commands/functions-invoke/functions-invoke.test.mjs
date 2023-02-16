5011be59af69ca2bc748ee38032944b04c810455
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-02-16 12:10:16 +0100

    chore: new fixtures and new setup for dev-server tests (#5461)
    * chore: new fixtures and new setup for dev-server tests
    
    * chore: fix lint
    
    * chore: add missing files
    
    ---------
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- tests/integration/commands/functions-invoke/functions-invoke.test.mjs
+++ tests/integration/commands/functions-invoke/functions-invoke.test.mjs
@@ -1,162 +1,72 @@
+import got from 'got'
 import { describe, expect, test } from 'vitest'
 
-import callCli from '../../utils/call-cli.cjs'
-import { withDevServer } from '../../utils/dev-server.cjs'
-import got from '../../utils/got.cjs'
-import { withSiteBuilder } from '../../utils/site-builder.cjs'
+import { setupFixtureTests } from '../../utils/fixture.mjs'
 
 describe('functions:invoke command', () => {
-  test('should return function response when invoked with no identity argument', async () => {
-    await withSiteBuilder('function-invoke-with-no-identity-argument', async (builder) => {
-      builder.withNetlifyToml({ config: { functions: { directory: 'functions' } } }).withFunction({
-        path: 'test-invoke.js',
-        handler: async () => ({
-          statusCode: 200,
-          body: 'success',
-        }),
-      })
-
-      await builder.buildAsync()
-
-      await withDevServer({ cwd: builder.directory }, async (server) => {
-        const stdout = await callCli(['functions:invoke', 'test-invoke', `--port=${server.port}`], {
-          cwd: builder.directory,
-        })
-
-        expect(stdout).toBe('success')
-      })
-    })
-  })
-
-  test('should return function response when invoked', async () => {
-    await withSiteBuilder('site-with-ping-function', async (builder) => {
-      builder.withNetlifyToml({ config: { functions: { directory: 'functions' } } }).withFunction({
-        path: 'ping.js',
-        handler: async () => ({
-          statusCode: 200,
-          body: 'ping',
-        }),
-      })
-
-      await builder.buildAsync()
-
-      await withDevServer({ cwd: builder.directory }, async (server) => {
-        const stdout = await callCli(['functions:invoke', 'ping', '--identity', `--port=${server.port}`], {
-          cwd: builder.directory,
-        })
+  setupFixtureTests({ devServer: true, fixture: 'dev-server-with-functions' }, () => {
+    test.concurrent(
+      'should return function response when invoked with no identity argument',
+      async ({ devServer, fixture }) => {
+        const stdout = await fixture.callCli(['functions:invoke', 'ping', `--port=${devServer.port}`])
 
         expect(stdout).toBe('ping')
-      })
+      },
+    )
+
+    test.concurrent('should return function response when invoked', async ({ devServer, fixture }) => {
+      const stdout = await fixture.callCli(['functions:invoke', 'ping', '--identity', `--port=${devServer.port}`])
+
+      expect(stdout).toBe('ping')
     })
-  })
 
-  test('should trigger background function from event', async () => {
-    await withSiteBuilder('site-with-ping-function', async (builder) => {
-      await builder
-        .withNetlifyToml({ config: { functions: { directory: 'functions' } } })
-        .withFunction({
-          path: 'identity-validate-background.js',
-          handler: async (event) => ({
-            statusCode: 200,
-            body: JSON.stringify(event.body),
-          }),
-        })
-        .buildAsync()
+    test.concurrent('should trigger background function from event', async ({ devServer, fixture }) => {
+      const stdout = await fixture.callCli([
+        'functions:invoke',
+        'identity-validate-background',
+        '--identity',
+        `--port=${devServer.port}`,
+      ])
 
-      await withDevServer({ cwd: builder.directory }, async (server) => {
-        const stdout = await callCli(
-          ['functions:invoke', 'identity-validate-background', '--identity', `--port=${server.port}`],
-          {
-            cwd: builder.directory,
-          },
-        )
-
-        // background functions always return an empty response
-        expect(stdout).toBe('')
-      })
+      // background functions always return an empty response
+      expect(stdout).toBe('')
     })
-  })
 
-  test('should serve helpful tips and tricks', async () => {
-    await withSiteBuilder('site-with-isc-ping-function', async (builder) => {
-      await builder
-        .withNetlifyToml({
-          config: { functions: { directory: 'functions' } },
-        })
-        // mocking until https://github.com/netlify/functions/pull/226 landed
-        .withContentFile({
-          path: 'node_modules/@netlify/functions/package.json',
-          content: `{}`,
-        })
-        .withContentFile({
-          path: 'node_modules/@netlify/functions/index.js',
-          content: `module.exports.schedule = (schedule, handler) => handler`,
-        })
-        .withContentFile({
-          path: 'functions/hello-world.js',
-          content: `
-          const { schedule } = require('@netlify/functions')
-
-          module.exports.handler = schedule('@daily', async () => {
-            return {
-              statusCode: 200,
-              body: "hello world"
-            }
-          })
-          `.trim(),
-        })
-        .buildAsync()
-
-      await withDevServer({ cwd: builder.directory }, async (server) => {
-        const plainTextResponse = await got(`http://localhost:${server.port}/.netlify/functions/hello-world`, {
-          throwHttpErrors: false,
-          retry: null,
-        })
-
-        const youReturnedBodyRegex = /.*Your function returned `body`. Is this an accident\?.*/
-        expect(plainTextResponse.body).toMatch(youReturnedBodyRegex)
-        expect(plainTextResponse.body).toMatch(/.*You performed an HTTP request.*/)
-        expect(plainTextResponse.statusCode).toBe(200)
-
-        const htmlResponse = await got(`http://localhost:${server.port}/.netlify/functions/hello-world`, {
-          throwHttpErrors: false,
-          retry: null,
-          headers: {
-            accept: 'text/html',
-          },
-        })
-
-        expect(htmlResponse.body).toMatch(/.*<link.*/)
-        expect(htmlResponse.statusCode).toBe(200)
-
-        const stdout = await callCli(['functions:invoke', 'hello-world', '--identity', `--port=${server.port}`], {
-          cwd: builder.directory,
-        })
-        expect(stdout).toMatch(youReturnedBodyRegex)
+    test.concurrent('should serve helpful tips and tricks', async ({ devServer, fixture }) => {
+      const plainTextResponse = await got(`http://localhost:${devServer.port}/.netlify/functions/scheduled-isc`, {
+        throwHttpErrors: false,
+        retry: null,
       })
+
+      const youReturnedBodyRegex = /.*Your function returned `body`. Is this an accident\?.*/
+      expect(plainTextResponse.body).toMatch(youReturnedBodyRegex)
+      expect(plainTextResponse.body).toMatch(/.*You performed an HTTP request.*/)
+      expect(plainTextResponse.statusCode).toBe(200)
+
+      const htmlResponse = await got(`http://localhost:${devServer.port}/.netlify/functions/scheduled-isc`, {
+        throwHttpErrors: false,
+        retry: null,
+        headers: {
+          accept: 'text/html',
+        },
+      })
+
+      expect(htmlResponse.body).toMatch(/.*<link.*/)
+      expect(htmlResponse.statusCode).toBe(200)
+
+      const stdout = await fixture.callCli([
+        'functions:invoke',
+        'scheduled-isc',
+        '--identity',
+        `--port=${devServer.port}`,
+      ])
+      expect(stdout).toMatch(youReturnedBodyRegex)
     })
-  })
 
-  test('should detect netlify-toml defined scheduled functions', async () => {
-    await withSiteBuilder('site-with-netlify-toml-ping-function', async (builder) => {
-      await builder
-        .withNetlifyToml({
-          config: { functions: { directory: 'functions', 'test-1': { schedule: '@daily' } } },
-        })
-        .withFunction({
-          path: 'test-1.js',
-          handler: async () => ({
-            statusCode: 200,
-          }),
-        })
-        .buildAsync()
+    test.concurrent('should detect netlify-toml defined scheduled functions', async ({ devServer, fixture }) => {
+      const stdout = await fixture.callCli(['functions:invoke', 'scheduled', `--port=${devServer.port}`])
 
-      await withDevServer({ cwd: builder.directory }, async (server) => {
-        const stdout = await callCli(['functions:invoke', 'test-1', `--port=${server.port}`], {
-          cwd: builder.directory,
-        })
-        expect(stdout).toBe('')
-      })
+      expect(stdout).toBe('')
     })
   })
 })
