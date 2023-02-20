d0227f71455c0aa8291cd3717760ebb590fc510f
Eduardo Bouças <mail@eduardoboucas.com>
2023-02-20 11:50:00 +0000

    refactor: replace UUID with ULID

--- src/utils/request-id.mjs
+++ src/utils/request-id.mjs
@@ -1,6 +1,4 @@
 // @ts-check
-import { v4 as generateUUID } from 'uuid'
+import { ulid } from 'ulid'
 
-// Transform a v4 UUID to match the format used in production — alphanumeric
-// characters, all uppercase, 26 characters.
-export const generateRequestID = () => generateUUID().replace(/-/g, '').toUpperCase().slice(0, 26)
+export const generateRequestID = () => ulid()
