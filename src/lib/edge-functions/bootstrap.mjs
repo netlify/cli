78a1314ec087ae04d3b39a5c2bfd646f1abb09ce
Eduardo Bouças <mail@eduardoboucas.com>
2023-03-14 12:20:57 +0000

    fix: update edge functions bootstrap to `641064c685071e00083eb682`

--- src/lib/edge-functions/bootstrap.mjs
+++ src/lib/edge-functions/bootstrap.mjs
@@ -1,5 +1,5 @@
 import { env } from 'process'
 
-const latestBootstrapURL = 'https://640b5b066a2b9b0008e88cb0--edge.netlify.com/bootstrap/index-combined.ts'
+const latestBootstrapURL = 'https://641064c685071e00083eb682--edge.netlify.com/bootstrap/index-combined.ts'
 
 export const getBootstrapURL = () => env.NETLIFY_EDGE_BOOTSTRAP || latestBootstrapURL
