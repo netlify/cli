e1185555cb37b9606ce55704e17c0655a1fb0425
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-03-20 19:07:37 +0100

    chore: improve on getPackageJson tests (#5582)

--- tests/unit/utils/get-package-json.test.mjs
+++ tests/unit/utils/get-package-json.test.mjs
@@ -2,8 +2,6 @@ import { readFile } from 'fs/promises'
 
 import { beforeEach, describe, expect, test, vi } from 'vitest'
 
-import getPackageJson from '../../../src/utils/get-package-json.mjs'
-
 vi.mock('fs/promises', async (importActual) => {
   const fs = await importActual()
 
@@ -14,8 +12,12 @@ vi.mock('fs/promises', async (importActual) => {
 })
 
 describe('getPackageJson', () => {
-  beforeEach(() => {
+  let getPackageJson
+  beforeEach(async () => {
     vi.clearAllMocks()
+    vi.resetModules()
+    const newImport = await import('../../../src/utils/get-package-json.mjs')
+    getPackageJson = newImport.default
   })
 
   test('should return the package.json of netlify-cli', async () => {
@@ -23,11 +25,15 @@ describe('getPackageJson', () => {
 
     expect(packageJson).not.toBeUndefined()
     expect(packageJson.name).toBe('netlify-cli')
-
-    expect(readFile).toHaveBeenCalledOnce()
   })
 
-  test('should not reread package.json', async () => {
+  test('should not re-read package.json', async () => {
+    // first call reads from file-system
+    await getPackageJson()
+    expect(readFile).toHaveBeenCalledOnce()
+    vi.mocked(readFile).mockClear()
+
+    // second call should cache and not read from file-system
     const packageJson = await getPackageJson()
 
     expect(packageJson).not.toBeUndefined()
