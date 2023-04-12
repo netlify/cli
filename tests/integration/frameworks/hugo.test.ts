2cd9fb7006af7b91750ee25e62d466090ab3f4c0
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-04-12 17:35:19 +0200

    chore: fix hugo

---
+++ tests/integration/frameworks/hugo.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest'
+
+import { FixtureTestContext, setupFixtureTests } from '../utils/fixture.js'
+import got from '../utils/got.cjs'
+
+setupFixtureTests('hugo-site', { devServer: true }, () => {
+  test<FixtureTestContext>('should not infinite redirect when -d flag is passed', async ({ devServer: { url } }) => {
+    const response = await got(`${url}/`).text()
+
+    expect(response).toContain('Home page!')
+  })
+})
