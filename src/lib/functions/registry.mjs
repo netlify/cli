fce894926be22c29dbdd8b9de02e33bdcc651b29
Karin Hendrikse <30577427+khendrikse@users.noreply.github.com>
2023-04-13 14:18:43 +0200

    Update src/lib/functions/registry.mjs
    Co-authored-by: Eduardo Bouças <mail@eduardoboucas.com>

--- src/lib/functions/registry.mjs
+++ src/lib/functions/registry.mjs
@@ -212,7 +212,7 @@ export class FunctionsRegistry {
     await Promise.all(deletedFunctions.map((func) => this.unregisterFunction(func.name)))
 
     await Promise.all(
-      // zip-it-ship-it returns an array sorted based on which extension should have precedence.
+      // zip-it-and-ship-it returns an array sorted based on which extension should have precedence,
       // where the last ones precede the previous ones. This is why
       // we reverse the array so we get the right functions precedence in the cli.
       functions.reverse().map(async ({ displayName, mainFile, name, runtime: runtimeName }) => {
