6b5faeb93dfa95646a69484d2b0058c752a8ca97
Sarah Etter <sarah.etter@netlify.com>
2023-05-15 16:01:00 -0400

    feat: error out on getSiteByName

--- src/utils/get-site.mjs
+++ src/utils/get-site.mjs
@@ -1,9 +1,11 @@
+import { error } from '../../utils/command-helpers.mjs'
+
 export const getSiteByName = async (api, siteName) => {
   try {
     const sites = await api.listSites({ name: siteName, filter: 'all' })
     const siteFoundByName = sites.find((filteredSite) => filteredSite.name === siteName)
     return siteFoundByName
-  } catch (error) {
-    return { error }
+  } catch {
+    error('Site not found. Please rerun "netlify link"')
   }
 }
