2528c8e0a2b723e9510b8fcb0af75a7c45dc8d15
Sarah Etter <sarah.etter@netlify.com>
2023-05-18 15:34:20 -0400

    feat: return error instead of just calling error

--- src/utils/hooks/requires-site-info.mjs
+++ src/utils/hooks/requires-site-info.mjs
@@ -8,7 +8,7 @@ const requiresSiteInfo = async (command) => {
   const siteId = site.id
   if (!siteId) {
     warn('Did you run `netlify link` yet?')
-    error(`You don't appear to be in a folder that is linked to a site`)
+    return error(`You don't appear to be in a folder that is linked to a site`)
   }
   try {
     await api.getSite({ siteId })
@@ -16,13 +16,13 @@ const requiresSiteInfo = async (command) => {
     // unauthorized
     if (error_.status === 401) {
       warn(`Log in with a different account or re-link to a site you have permission for`)
-      error(`Not authorized to view the currently linked site (${siteId})`)
+      return error(`Not authorized to view the currently linked site (${siteId})`)
     }
     // missing
     if (error_.status === 404) {
-      error(`The site this folder is linked to can't be found`)
+      return error(`The site this folder is linked to can't be found`)
     }
-    error(error_)
+    return error(error_)
   }
 }
 
