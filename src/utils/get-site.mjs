fe23ee359efc53a167c211a8dcfaf5140f15677a
Sarah Etter <sarah.etter@netlify.com>
2023-05-18 15:35:51 -0400

    fix: throw error if undefined

--- src/utils/get-site.mjs
+++ src/utils/get-site.mjs
@@ -4,6 +4,11 @@ export const getSiteByName = async (api, siteName) => {
   try {
     const sites = await api.listSites({ name: siteName, filter: 'all' })
     const siteFoundByName = sites.find((filteredSite) => filteredSite.name === siteName)
+
+    if (!siteFoundByName) {
+      throw Error
+    }
+
     return siteFoundByName
   } catch {
     error('Site not found. Please rerun "netlify link"')
