e79820b1c31d45aa297f60fcc98115bb9d1e9cd3
Sarah Etter <sarah.etter@netlify.com>
2023-05-29 15:48:38 -0400

    feat: add feature flag checking function

--- src/commands/base-command.mjs
+++ src/commands/base-command.mjs
@@ -565,4 +565,23 @@ export default class BaseCommand extends Command {
 
     return 'dev'
   }
+
+  /**
+   * Allows us to check if a feature flag is enabled for a site.
+   * Due to versioning of the cli, and the desire to remove flags from
+   * our feature flag service when they should always evaluate to true,
+   * we can't just look for the presense of {featureFlagName: true}, as
+   * the absense of a flag should also evaluate to the flag being enabled.
+   * Instead, we return that the feature flag is enabled if it isn't
+   * specifically set to false in the response
+   * @param {string} flagName
+   *
+   * @returns {boolean}
+   */
+  isFeatureFlagEnabled(flagName) {
+    if (this.netlify.siteInfo.feature_flags[flagName] !== false) {
+      return true
+    }
+    return false
+  }
 }
