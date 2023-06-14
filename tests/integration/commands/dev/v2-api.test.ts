6f8737329f523d75c8254172c00f6f677293b31c
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-06-14 17:07:18 +0200

    chore: skip v2 tests on Node.js 18.12 or lower

--- tests/integration/commands/dev/v2-api.test.ts
+++ tests/integration/commands/dev/v2-api.test.ts
@@ -1,9 +1,12 @@
+import { version } from 'process'
+
+import { gte } from 'semver'
 import { describe, expect, test } from 'vitest'
 
 import { FixtureTestContext, setupFixtureTests } from '../../utils/fixture.js'
 import got from '../../utils/got.cjs'
 
-describe('v2 api', () => {
+describe.runIf(gte(version, '18.13.0'))('v2 api', () => {
   setupFixtureTests('dev-server-with-v2-functions', { devServer: true }, () => {
     test<FixtureTestContext>('should successfully be able to run v2 functions', async ({ devServer }) => {
       const response = await got(`http://localhost:${devServer.port}/.netlify/functions/ping`, {
