1d1b7961f151a61d13d1adbde387284c9c9b2488
Angel Mendez <amendez1988@gmail.com>
2023-07-21 20:17:09 -0600

    refactor: tests/integration/310.command.dev.exec.test.cjs
    - replace ava with vitest
    - move and rename test file

---
+++ tests/integration/commands/dev/dev.exec.test.mjs
@@ -0,0 +1,21 @@
+import process from 'process'
+
+import { test } from 'vitest'
+
+import callCli from '../../utils/call-cli.cjs'
+import { withSiteBuilder } from '../../utils/site-builder.cjs'
+
+test('should pass .env variables to exec command', async (t) => {
+  await withSiteBuilder('site-env-file', async (builder) => {
+    builder.withEnvFile({ env: { VITEST_TEST: 'ENV_VAR' } })
+    await builder.buildAsync()
+
+    const cmd = process.platform === 'win32' ? 'set' : 'printenv'
+    const output = await callCli(['dev:exec', cmd], {
+      cwd: builder.directory,
+    })
+
+    t.expect(output.includes('Injected .env file env var: VITEST_TEST')).toBe(true)
+    t.expect(output.includes('VITEST_TEST=ENV_VAR')).toBe(true)
+  })
+})
