31a213ea1132b3706b8d44eeed09335e90fb5009
Simon Knott <info@simonknott.de>
2023-08-18 09:41:37 +0200

    chore: mock API to test account id (#5859)
    Co-authored-by: Sarah Etter <sarah.etter@netlify.com>

--- tests/integration/commands/dev/v2-api.test.ts
+++ tests/integration/commands/dev/v2-api.test.ts
@@ -6,8 +6,23 @@ import { describe, expect, test } from 'vitest'
 import { FixtureTestContext, setupFixtureTests } from '../../utils/fixture.js'
 import got from '../../utils/got.cjs'
 
+const siteInfo = {
+  account_id: 'mock-account-id',
+  account_slug: 'mock-account',
+  id: 'mock-site-id',
+  name: 'mock-site-name',
+}
+const routes = [
+  { path: 'sites/*/service-instances', response: [] },
+  { path: 'sites/*', response: siteInfo },
+  {
+    path: 'accounts',
+    response: [{ id: siteInfo.account_id, slug: siteInfo.account_slug }],
+  },
+]
+
 describe.runIf(gte(version, '18.13.0'))('v2 api', () => {
-  setupFixtureTests('dev-server-with-v2-functions', { devServer: true }, () => {
+  setupFixtureTests('dev-server-with-v2-functions', { devServer: true, mockApi: { routes } }, () => {
     test<FixtureTestContext>('should successfully be able to run v2 functions', async ({ devServer }) => {
       const response = await got(`http://localhost:${devServer.port}/.netlify/functions/ping`, {
         throwHttpErrors: false,
@@ -54,6 +69,8 @@ describe.runIf(gte(version, '18.13.0'))('v2 api', () => {
       expect(context.geo.city).toEqual('Mock City')
 
       expect(context.cookies).toEqual({ foo: 'bar' })
+
+      expect(context.account.id).toEqual('mock-account-id')
     })
 
     test<FixtureTestContext>('logging works', async ({ devServer }) => {
