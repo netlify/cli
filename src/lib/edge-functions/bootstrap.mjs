a60764caf22054f50c270981ced5a56af1ac8e19
Simon Knott <info@simonknott.de>
2023-09-05 16:09:34 +0200

    fix: use .com

--- src/lib/edge-functions/bootstrap.mjs
+++ src/lib/edge-functions/bootstrap.mjs
@@ -1,5 +1,5 @@
 import { env } from 'process'
 
-const latestBootstrapURL = 'https://64f73321fdd56900083fa618--edge.netlify.app/bootstrap/index-combined.ts'
+const latestBootstrapURL = 'https://64f73321fdd56900083fa618--edge.netlify.com/bootstrap/index-combined.ts'
 
 export const getBootstrapURL = () => env.NETLIFY_EDGE_BOOTSTRAP || latestBootstrapURL
