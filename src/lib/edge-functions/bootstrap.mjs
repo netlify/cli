da723a15799590797b2df93fe871f3b3b5f2d5fe
Simon Knott <info@simonknott.de>
2023-09-06 10:00:06 +0200

    fix: show proper `req.url` in HTTPS mode (#5968)
    * fix: show proper `req.url` in HTTPS mode
    
    * fix: use x-nf-passthrough-proto
    
    * refactor: use settings.https to detect https
    
    * refactor: throw 500 error in test
    
    * chore: remove unused import
    
    * fix: update bootstrap url
    
    * fix: use .com

--- src/lib/edge-functions/bootstrap.mjs
+++ src/lib/edge-functions/bootstrap.mjs
@@ -1,5 +1,5 @@
 import { env } from 'process'
 
-const latestBootstrapURL = 'https://64e7783fce8cfe0008496c72--edge.netlify.com/bootstrap/index-combined.ts'
+const latestBootstrapURL = 'https://64f73321fdd56900083fa618--edge.netlify.com/bootstrap/index-combined.ts'
 
 export const getBootstrapURL = () => env.NETLIFY_EDGE_BOOTSTRAP || latestBootstrapURL
--- src/utils/proxy.mjs
+++ src/utils/proxy.mjs
@@ -698,6 +698,7 @@ export const startProxy = async function ({
     mainPort: settings.port,
     offline,
     passthroughPort: secondaryServerPort || settings.port,
+    settings,
     projectDir,
     siteInfo,
     accountId,
