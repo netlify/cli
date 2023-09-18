1ab06f6179f943697f3c4e1bbfee057b2eb1a191
Erica Pisani <pisani.erica@gmail.com>
2023-09-18 14:14:25 -0400

    Add logic for updating the integration info on remote.
    Update the local config file for the integration when needed

--- src/commands/integrations/deploy.mjs
+++ src/commands/integrations/deploy.mjs
@@ -14,6 +14,15 @@ import yaml from 'js-yaml'
 
 const INTEGRATION_URL = process.env.INTEGRATION_URL || 'https://api.netlifysdk.com'
 
+function areScopesEqual(localScopes, remoteScopes) {
+  if (localScopes.length !== remoteScopes.length) {
+    return false
+  }
+
+  return localScopes.every((scope) => remoteScopes.includes(scope))
+}
+
+
 /**
  * The deploy command for Netlify Integrations
  * @param {import('commander').OptionValues} options
@@ -44,37 +53,60 @@ const deploy = async (options, command) => {
     siteInfo,
   })
 
-  const integration = await fetch(`${INTEGRATION_URL}/${accountId}/integrations?site_id=${siteId}`, {
+  const registeredIntegration = await fetch(`${INTEGRATION_URL}/${accountId}/integrations?site_id=${siteId}`, {
     headers: {
       "netlify-token": token
     }
   }).then(res => res.json())
   
-  const { name, description, scopes, slug } = getConfiguration();
+  let { name, description, scopes, slug } = await getConfiguration();
 
-  if (!integration) {
-    // Create the integration in Jigsaw
-  } else if (slug != integration.slug) {
-    // Update the project's integration.yaml file with the Jigsaw slug since that will
+  let integrationSlug = slug
+  if (!registeredIntegration) {
+    // Create the integration in remote
+  } else if (slug != registeredIntegration.slug) {
+    // Update the project's integration.yaml file with the remote slug since that will
     // be considered the source of truth and is a value that can't be edited by the user.
     // Let the user know they need to commit and push the changes.
-    const updatedSlug = integration.slug;
-    const updatedIntegrationConfig = yaml.dump({ config: {name, description, scopes, slug: updatedSlug }})
-    const filePath = resolve(process.cwd(), 'integration.yaml')
-    await fs.writeFile(filePath, updatedIntegrationConfig)
-    log(chalk.green(`Updated the integration.yaml file with the slug ${updatedSlug}. Please commit and push the changes.`))
+    integrationSlug = registeredIntegration.slug;
+
+    log(chalk.green(`Updated the integration.yaml file with the slug that is registed for this integration: ${integrationSlug}.`))
   }
 
-  // If the integration already exists in Jigsaw and the fields differ from what we're seeing (particularly 'scopes'),
-  // then we should prompt the user to confirm that they want to update them. Should also let them know that 
-  // this will only affect future installations of the integration.
-  const integrationScopes = integration.scopes;
+  if (!name) {
+    name = registeredIntegration.name
+  }
 
-  if (scopes) {
-    log(chalk.yellow(`This integration is already registered. The current required scopes are "${integrationScopes}" and will be updated to "${scopes}" if you continue. This will only affect future installations of the integration.`))
-    
-    // Todo: where I left off - was updating the SDK to read in the scope values
-    // correctly from the integraton.yaml so we can show this prompt properly
+  if (!description) {
+    description = registeredIntegration.description
+  }
+
+  // This is returned as a comma separated string and will be easier to manage here as an array
+  const registeredIntegrationScopes = registeredIntegration.scopes.split(',');
+
+  const scopeResources = Object.keys(scopes)
+  let localScopes = []
+
+  if (scopeResources.includes('all')) {
+    localScopes = ['all']
+  } else {
+    scopeResources.forEach((resource) => {
+      const permissionsRequested = scopes[resource]
+      permissionsRequested.forEach((permission) => localScopes.push(`${resource}:${permission}`))
+    })
+  }
+
+  if (!areScopesEqual(localScopes, registeredIntegrationScopes)) {
+    log(chalk.yellow(`This integration is already registered. The current required scopes are:`))
+    for (const scope of registeredIntegrationScopes) {
+      log(chalk.green(`- ${scope}`))
+    }
+    log(chalk.yellow("and will be updated to:"))
+    for (const scope of localScopes) {
+      log(chalk.green(`- ${scope}`))
+    }
+    log(chalk.yellow("if you continue. This will only affect future installations of the integration."));
+        
     const scopePrompt = await inquirer.prompt([
       {
         type: 'confirm',
@@ -84,7 +116,42 @@ const deploy = async (options, command) => {
       },
     ])
 
-    console.log("CONFIRMED?", scopePrompt.updateScopes)
+    let scopesToWrite = null
+    if (scopePrompt.updateScopes) {
+      // Update the scopes in remote
+      scopesToWrite = scopes
+      const updatedIntegration = await fetch(`${INTEGRATION_URL}/${accountId}/integrations/${integrationSlug}`, {
+        method: 'PUT',
+        headers: {
+          "netlify-token": token
+        },
+        body: JSON.stringify({ name, description, hostSiteId: siteId, scopes: localScopes.join(',') })
+      }).then(res => res.json())
+
+    } else {
+      // Use the scopes that are already registered
+      log(chalk.white("Saving the currently registered scopes to the integration.yaml file."));
+      for (const i=0; i<registeredIntegrationScopes.length; i++) {
+        const scope = registeredIntegrationScopes[i]
+        const [resource, permission] = scope.split(':')
+        if (resource === 'all') {
+          scopesToWrite = { all: true }
+          break;
+        } else {
+          if (!scopesToWrite[resource]) {
+            scopesToWrite[resource] = []
+          }
+          scopesToWrite[resource].push(permission)
+        }
+      }
+    }
+
+    const updatedIntegrationConfig = yaml.dump({ config: {name, description, slug: integrationSlug, scopes: scopesToWrite }})
+
+    const filePath = resolve(process.cwd(), 'integration.yaml')
+    await fs.writeFile(filePath, updatedIntegrationConfig)
+
+    log(chalk.yellow("Changes to the integration.yaml file are complete. Please commit and push these changes."));
   }
   
   // Deploy the integration to that site
