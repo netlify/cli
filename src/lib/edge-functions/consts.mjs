836054797e797d4bb6ea7d83df0869fc90c164b7
Eduardo Bouças <mail@eduardoboucas.com>
2023-09-28 13:25:00 +0100

    feat: support npm modules in edge functions locally (#6018)

--- src/lib/edge-functions/consts.mjs
+++ src/lib/edge-functions/consts.mjs
@@ -1,6 +1,7 @@
 export const DIST_IMPORT_MAP_PATH = 'edge-functions-import-map.json'
 export const INTERNAL_EDGE_FUNCTIONS_FOLDER = 'edge-functions'
 export const EDGE_FUNCTIONS_FOLDER = 'edge-functions-dist'
+export const EDGE_FUNCTIONS_SERVE_FOLDER = 'edge-functions-serve'
 export const PUBLIC_URL_PATH = '.netlify/internal/edge-functions'
 
 // Feature flags related to Edge Functions that should be passed along to
--- tests/integration/commands/dev/edge-functions.test.ts
+++ tests/integration/commands/dev/edge-functions.test.ts
@@ -1,12 +1,38 @@
+import process from 'process'
+
 import { describe, expect, expectTypeOf, test } from 'vitest'
 
 import { FixtureTestContext, setupFixtureTests } from '../../utils/fixture.js'
 import got from '../../utils/got.cjs'
 import { pause } from '../../utils/pause.cjs'
 
+// Skipping tests on Windows because of an issue with the Deno CLI throwing IO
+// errors when running in the CI.
+const isWindows = process.platform === 'win32'
+
+const siteInfo = {
+  account_slug: 'test-account',
+  id: 'foo',
+  name: 'site-name',
+  feature_flags: {
+    edge_functions_npm_support: true,
+  },
+  functions_config: { timeout: 1 },
+}
+
+const routes = [
+  { path: 'sites/foo', response: siteInfo },
+
+  { path: 'sites/foo/service-instances', response: [] },
+  {
+    path: 'accounts',
+    response: [{ slug: siteInfo.account_slug }],
+  },
+]
+
 describe('edge functions', () => {
-  setupFixtureTests('dev-server-with-edge-functions', { devServer: true }, () => {
-    test<FixtureTestContext>('should run edge functions in correct order', async ({ devServer }) => {
+  setupFixtureTests('dev-server-with-edge-functions', { devServer: true, mockApi: { routes } }, () => {
+    test.skipIf(isWindows)<FixtureTestContext>('should run edge functions in correct order', async ({ devServer }) => {
       const response = await got(`http://localhost:${devServer.port}/ordertest`, {
         throwHttpErrors: false,
         retry: { limit: 0 },
@@ -16,7 +42,7 @@ describe('edge functions', () => {
       expect(response.body).toMatchSnapshot()
     })
 
-    test<FixtureTestContext>('should provide context properties', async ({ devServer }) => {
+    test.skipIf(isWindows)<FixtureTestContext>('should provide context properties', async ({ devServer }) => {
       const response = await got(`http://localhost:${devServer.port}/context`, {
         throwHttpErrors: false,
         retry: { limit: 0 },
@@ -30,10 +56,10 @@ describe('edge functions', () => {
       expect(params).toEqual({})
       expectTypeOf(requestId).toBeString()
       expect(server).toEqual({ region: 'local' })
-      expect(site).toEqual({ id: 'foo' })
+      expect(site).toEqual({ id: 'foo', name: 'site-name' })
     })
 
-    test<FixtureTestContext>('should expose URL parameters', async ({ devServer }) => {
+    test.skipIf(isWindows)<FixtureTestContext>('should expose URL parameters', async ({ devServer }) => {
       const response = await got(`http://localhost:${devServer.port}/categories/foo/products/bar`, {
         throwHttpErrors: false,
         retry: { limit: 0 },
@@ -46,7 +72,7 @@ describe('edge functions', () => {
       })
     })
 
-    test<FixtureTestContext>('should respect config.methods field', async ({ devServer }) => {
+    test.skipIf(isWindows)<FixtureTestContext>('should respect config.methods field', async ({ devServer }) => {
       const responseGet = await got(`http://localhost:${devServer.port}/products/really-bad-product`, {
         method: 'GET',
         throwHttpErrors: false,
@@ -64,49 +90,67 @@ describe('edge functions', () => {
       expect(responseDelete.body).toEqual('Deleted item successfully: really-bad-product')
     })
 
-    test<FixtureTestContext>('should show an error page when an edge function has an uncaught exception', async ({
-      devServer,
-    }) => {
-      // Request #1: Plain text
-      const res1 = await got(`http://localhost:${devServer.port}/uncaught-exception`, {
-        method: 'GET',
-        throwHttpErrors: false,
-        retry: { limit: 0 },
-      })
+    test.skipIf(isWindows)<FixtureTestContext>(
+      'should show an error page when an edge function has an uncaught exception',
+      async ({ devServer }) => {
+        // Request #1: Plain text
+        const res1 = await got(`http://localhost:${devServer.port}/uncaught-exception`, {
+          method: 'GET',
+          throwHttpErrors: false,
+          retry: { limit: 0 },
+        })
 
-      expect(res1.statusCode).toBe(500)
-      expect(res1.body).toContain('ReferenceError: thisWillThrow is not defined')
+        expect(res1.statusCode).toBe(500)
+        expect(res1.body).toContain('ReferenceError: thisWillThrow is not defined')
 
-      // Request #2: HTML
-      const res2 = await got(`http://localhost:${devServer.port}/uncaught-exception`, {
-        method: 'GET',
-        headers: {
-          Accept: 'text/html',
-        },
-        throwHttpErrors: false,
-        retry: { limit: 0 },
-      })
+        // Request #2: HTML
+        const res2 = await got(`http://localhost:${devServer.port}/uncaught-exception`, {
+          method: 'GET',
+          headers: {
+            Accept: 'text/html',
+          },
+          throwHttpErrors: false,
+          retry: { limit: 0 },
+        })
 
-      expect(res2.body).toContain('<p>An unhandled error in the function code triggered the following message:</p>')
-    })
+        expect(res2.body).toContain('<p>An unhandled error in the function code triggered the following message:</p>')
+      },
+    )
+
+    test.skipIf(isWindows)<FixtureTestContext>(
+      'should run an edge function that imports an npm module',
+      async ({ devServer }) => {
+        const res = await got(`http://localhost:${devServer.port}/with-npm-module`, {
+          method: 'GET',
+          throwHttpErrors: false,
+          retry: { limit: 0 },
+        })
+
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toBe('Hello from an npm module!')
+      },
+    )
   })
 
-  setupFixtureTests('dev-server-with-edge-functions', { devServer: true }, () => {
-    test<FixtureTestContext>('should not remove other edge functions on change', async ({ devServer, fixture }) => {
-      // we need to wait till file watchers are loaded
-      await pause(500)
+  setupFixtureTests('dev-server-with-edge-functions', { devServer: true, mockApi: { routes } }, () => {
+    test.skipIf(isWindows)<FixtureTestContext>(
+      'should not remove other edge functions on change',
+      async ({ devServer, fixture }) => {
+        // we need to wait till file watchers are loaded
+        await pause(500)
 
-      await fixture.builder
-        .withEdgeFunction({
-          name: 'new',
-          handler: async () => new Response('hello'),
-          config: { path: ['/new'] },
-        })
-        .build()
+        await fixture.builder
+          .withEdgeFunction({
+            name: 'new',
+            handler: async () => new Response('hello'),
+            config: { path: ['/new'] },
+          })
+          .build()
 
-      await devServer.waitForLogMatching('Loaded edge function new')
+        await devServer.waitForLogMatching('Loaded edge function new')
 
-      expect(devServer.output).not.toContain('Removed edge function')
-    })
+        expect(devServer.output).not.toContain('Removed edge function')
+      },
+    )
   })
 })
