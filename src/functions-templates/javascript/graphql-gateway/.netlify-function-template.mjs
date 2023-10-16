2a688760c93fd64b60fe7db41b033aa73c6aab8d
Ivan Zarea <ivan.zarea@netlify.com>
2023-10-16 10:20:17 +0200

    chore: clean up old function templates (#6042)
    * chore: cleaned up unused function templates
    
    * chore: delete the tests using a function that doesn't exist
    
    ---------
    
    Co-authored-by: Jenae Janzen <101715009+jenae-janzen@users.noreply.github.com>
    Co-authored-by: Lukas Holzer <lukas.holzer@netlify.com>

--- src/functions-templates/javascript/graphql-gateway/.netlify-function-template.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-  name: 'graphql-gateway',
-  description: 'Apollo Server Lambda Gateway stitching schemas from other GraphQL Functions!',
-  functionType: 'serverless',
-}
--- src/functions-templates/javascript/send-email/.netlify-function-template.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-  name: 'send-email',
-  description: "Send Email: Send email with no SMTP server via 'sendmail' pkg",
-  functionType: 'serverless',
-}
