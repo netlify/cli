29bdca56ead5567a741d0c779af59cc202b3df89
Erica Pisani <pisani.erica@gmail.com>
2023-10-26 15:27:16 -0400

    fix: export

--- tests/integration/utils/got.mjs
+++ tests/integration/utils/got.mjs
@@ -5,9 +5,11 @@ const TIMEOUT = 300_000
 // Default got retry status code with the addition of 403
 const STATUS_CODE = [403, 408, 413, 429, 500, 502, 503, 504, 521, 522, 524]
 
-export default extendedGot = got.extend({
+const extendedGot = got.extend({
   retry: {
     statusCodes: STATUS_CODE,
   },
   timeout: { request: TIMEOUT },
 })
+
+export default extendedGot
