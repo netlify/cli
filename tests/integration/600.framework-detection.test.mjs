d3a413ff47a5ff710b0f0e04affe45a2585bf94b
Erica Pisani <pisani.erica@gmail.com>
2023-10-26 12:21:56 -0400

    fix: issues raised with linter

--- tests/integration/600.framework-detection.test.mjs
+++ tests/integration/600.framework-detection.test.mjs
@@ -5,7 +5,7 @@ import execa from 'execa'
 
 import cliPath from './utils/cli-path.mjs'
 import { getExecaOptions, withDevServer } from './utils/dev-server.cjs'
-import got from './utils/got.cjs'
+import got from './utils/got.mjs'
 import { DOWN, answerWithValue, handleQuestions } from './utils/handle-questions.cjs'
 import { withSiteBuilder } from './utils/site-builder.cjs'
 import { normalize } from './utils/snapshots.cjs'
--- tests/integration/utils/cli-path.mjs
+++ tests/integration/utils/cli-path.mjs
@@ -1,3 +1,5 @@
-import path from "path"
+import path from 'path'
+
+const __dirname = new URL('.', import.meta.url).pathname
 
 export const cliPath = path.resolve(__dirname, '../../../bin/run.mjs')
