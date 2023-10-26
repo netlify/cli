e912caa0c00eb73ca81bbbc749221969a298c78b
Erica Pisani <pisani.erica@gmail.com>
2023-10-26 15:10:18 -0400

    fix: missed a file extension

--- tests/integration/utils/fixture.ts
+++ tests/integration/utils/fixture.ts
@@ -7,7 +7,7 @@ import { temporaryDirectory } from 'tempy'
 import { afterAll, afterEach, beforeAll, beforeEach, describe } from 'vitest'
 
 import { callCli } from './call-cli.mjs'
-import { startDevServer } from './dev-server.cjs'
+import { startDevServer } from './dev-server.mjs'
 import { MockApi, Route, getCLIOptions, startMockApi } from './mock-api-vitest.js'
 import { SiteBuilder } from './site-builder.mjs'
 
