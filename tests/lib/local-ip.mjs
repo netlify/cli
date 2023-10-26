35590dcfb239ebb4e2fc8127eb57b87ebd09808e
Erica Pisani <pisani.erica@gmail.com>
2023-10-26 10:41:31 -0400

    refactor: convert local-ip.mjs

--- tests/lib/local-ip.mjs
+++ tests/lib/local-ip.mjs
@@ -1,10 +1,9 @@
-const process = require('process')
-
-const semver = require('semver')
+import process from 'process'
+import semver from 'semver'
 
 const version = process.version.slice(1)
 
 // This changed in Node 17 with https://github.com/nodejs/node/pull/39987.
 const [clientIP, originalIP] = semver.gte(version, '17.0.0') ? ['::1', '::1'] : ['127.0.0.1', '::ffff:127.0.0.1']
 
-module.exports = { clientIP, originalIP }
+export { clientIP, originalIP }
