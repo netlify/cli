d7779ca74e71e2270a5fbe630c31dd171737b18d
Erica Pisani <pisani.erica@gmail.com>
2023-10-27 09:46:34 -0400

    test: try different approach to getting the dirname variable

--- tests/integration/utils/cli-path.mjs
+++ tests/integration/utils/cli-path.mjs
@@ -1,5 +1,7 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __dirname = new URL('.', import.meta.url).pathname
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export const cliPath = path.resolve(__dirname, '../../../bin/run.mjs')
