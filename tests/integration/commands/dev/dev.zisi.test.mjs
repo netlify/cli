f8e445121cd54245c4f3700a63831f7d352324ea
Erica Pisani <pisani.erica@gmail.com>
2023-10-27 09:24:43 -0400

    test: see if removing the concurrency resolves the test server error

--- tests/integration/commands/dev/dev.zisi.test.mjs
+++ tests/integration/commands/dev/dev.zisi.test.mjs
@@ -17,7 +17,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const testMatrix = [{ args: [] }]
 
-describe.concurrent.each(testMatrix)('withSiteBuilder with args: $args', ({ args }) => {
+describe('withSiteBuilder with args: $args', ({ args }) => {
   test('should handle query params in redirects', async (t) => {
     await withSiteBuilder('site-with-query-redirects', async (builder) => {
       await builder
