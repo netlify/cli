be9da22e652db9990bf97cf697c9e7c1dcc134e0
Eduardo Bouças <mail@eduardoboucas.com>
2023-10-30 09:40:56 +0000

    chore: fix typo

--- src/lib/functions/runtimes/js/worker.mjs
+++ src/lib/functions/runtimes/js/worker.mjs
@@ -16,7 +16,7 @@ lambdaLocal.getLogger().level = 'alert'
 
 const { clientContext, entryFilePath, environment = {}, event, timeoutMs } = workerData
 
-// Injecting into the environmeny any properties passed in by the parent.
+// Injecting into the environment any properties passed in by the parent.
 for (const key in environment) {
   process.env[key] = environment[key]
 }
