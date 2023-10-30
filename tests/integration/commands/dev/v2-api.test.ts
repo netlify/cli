ddbebb79da5ebac81fb657c98b630bb5edd32e4f
Eduardo Bouças <mail@eduardoboucas.com>
2023-10-30 10:12:09 +0000

    chore: run npm install in test

--- tests/integration/commands/dev/v2-api.test.ts
+++ tests/integration/commands/dev/v2-api.test.ts
@@ -1,5 +1,6 @@
 import { version } from 'process'
 
+import execa from 'execa'
 import { gte } from 'semver'
 import { describe, expect, test } from 'vitest'
 
@@ -21,8 +22,12 @@ const routes = [
   },
 ]
 
+const setup = async ({ fixture }) => {
+  await execa('npm', ['install'], { cwd: fixture.directory })
+}
+
 describe.runIf(gte(version, '18.13.0'))('v2 api', () => {
-  setupFixtureTests('dev-server-with-v2-functions', { devServer: true, mockApi: { routes } }, () => {
+  setupFixtureTests('dev-server-with-v2-functions', { devServer: true, mockApi: { routes }, setup }, () => {
     test<FixtureTestContext>('should successfully be able to run v2 functions', async ({ devServer }) => {
       const response = await got(`http://localhost:${devServer.port}/.netlify/functions/ping`, {
         throwHttpErrors: false,
