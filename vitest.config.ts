185b7aab2d0e3d31872999b6217c065981dfe5a0
Erica Pisani <pisani.erica@gmail.com>
2023-10-31 15:24:21 -0400

    test: remove tools from vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['tests/**/*.test.mjs', 'tests/**/*.test.ts', 'tools/**/*.test.mjs'],
+    include: ['tests/**/*.test.mjs', 'tests/**/*.test.ts'],
     testTimeout: 60_000,
     hookTimeout: 60_000,
     deps: {
