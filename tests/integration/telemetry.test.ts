9db51b39ba9178bbef905b38cfbe76f2623ce9ef
Erica Pisani <pisani.erica@gmail.com>
2023-11-03 11:22:06 -0400

    test: port the rest of the tests over to MSW

--- tests/integration/telemetry.test.ts
+++ tests/integration/telemetry.test.ts
@@ -50,7 +50,7 @@ describe('telemetry', async () => {
     }
 
     server.use(
-      http.post(`*`, ({ request }) => {
+      http.all(`*`, ({ request }) => {
         unhandledRequestMade = true
         unhandledRequest = {
           url: request.url,
@@ -61,7 +61,7 @@ describe('telemetry', async () => {
     )
 
     server.use(
-      http.post(`http://localhost/api/v1/track`, ({request}) => {
+      http.post(`http://localhost/api/v1/track`, ({ request }) => {
         requestMade = request
         return HttpResponse.json({}, { status: 200 })
       })
@@ -108,81 +108,111 @@ describe('telemetry', async () => {
           return HttpResponse.json({}, { status: 200 })
         })
       )
-  
+
       await callCli(['api', 'listSites'], getCLIOptions(apiUrl))
-      
+
       expect(requestMade).toBeDefined()
       // example: netlify-cli/6.14.25 darwin-x64 node-v16.13.0
       const userAgent = requestMade.headers.get('user-agent')
       expect(userAgent.startsWith(`${name}/${version}`)).toEqual(true)
     })
+
+    test<MockApiTestContext>('should send correct command on success', async ({ apiUrl }) => {
+      server.use(
+        http.get(`http://localhost/api/v1/accounts`, () => {
+          return HttpResponse.json({}, { status: 200 })
+        })
+      )
+      server.use(
+        http.get(`http://localhost/api/v1/sites`, () => {
+          return HttpResponse.json({}, { status: 200 })
+        })
+      )
+
+      await callCli(['api', 'listSites'], getCLIOptions(apiUrl))
+      expect(requestMade).toBeDefined()
+
+      const body = await requestMade.json()
+
+      expect(typeof body.anonymousId).toBe('string')
+      expect(Number.isInteger(body.duration)).toBe(true)
+      expect(body.event).toBe('cli:command')
+      expect(body.status).toBe('success')
+      expect(body.properties).toStrictEqual({
+        buildSystem: [],
+        cliVersion: version,
+        command: 'api',
+        monorepo: false,
+        nodejsVersion,
+        packageManager: 'npm',
+      })
+    })
+  })
+
+  test<MockApiTestContext>('should send correct command on failure', async ({ apiUrl }) => {
+    server.use(
+      http.get(`http://localhost/api/v1/accounts`, () => {
+        return HttpResponse.json({}, { status: 200 })
+      })
+    )
+
+    await expect(callCli(['dev:exec', 'exit 1'], getCLIOptions(apiUrl))).rejects.toThrowError()
+    expect(requestMade).toBeDefined()
+
+    const body = await requestMade.json()
+
+    expect(typeof body.anonymousId).toBe('string')
+    expect(Number.isInteger(body.duration)).toBe(true)
+    expect(body.event).toBe('cli:command')
+    expect(body.status).toBe('error')
+    expect(body.properties).toStrictEqual({
+      buildSystem: [],
+      cliVersion: version,
+      command: 'dev:exec',
+      monorepo: false,
+      nodejsVersion,
+      packageManager: 'npm',
+    })
+  })
+
+  test('should add frameworks, buildSystem, and packageManager', async ({ apiUrl }) => {
+    server.use(
+      http.get(`http://localhost/api/v1/accounts`, () => {
+        return HttpResponse.json({}, { status: 200 })
+      })
+    )
+    
+    server.use(
+      http.get(`http://localhost/api/v1/sites`, () => {
+        return HttpResponse.json({}, { status: 200 })
+      })
+    )
+    
+    await withSiteBuilder('nextjs-site', async (builder) => {
+      await builder.withPackageJson({ packageJson: { dependencies: { next: '^12.13.0' } } }).buildAsync()
+
+      await execa(cliPath, ['api', 'listSites'], {
+        cwd: builder.directory,
+        ...getCLIOptions(apiUrl),
+      })
+
+      expect(requestMade).toBeDefined()
+
+      const body = await requestMade.json()
+  
+      expect(typeof body.anonymousId).toBe('string')
+      expect(Number.isInteger(body.duration)).toBe(true)
+      expect(body.event).toBe('cli:command')
+      expect(body.status).toBe('success')
+      expect(body.properties).toStrictEqual({
+        frameworks: ['next'],
+        buildSystem: [],
+        cliVersion: version,
+        command: 'api',
+        monorepo: false,
+        nodejsVersion,
+        packageManager: 'npm',
+      })
+    })
   })
 })
-
-
-//   test<MockApiTestContext>('should send correct command on success', async ({ apiUrl, requests }) => {
-//     await callCli(['api', 'listSites'], getCLIOptions(apiUrl))
-//     const request = requests.find(({ path }) => path === '/api/v1/track')
-//     expect(request).toBeDefined()
-
-//     expect(typeof request.body.anonymousId).toBe('string')
-//     expect(Number.isInteger(request.body.duration)).toBe(true)
-//     expect(request.body.event).toBe('cli:command')
-//     expect(request.body.status).toBe('success')
-//     expect(request.body.properties).toEqual({
-//       buildSystem: [],
-//       cliVersion: version,
-//       command: 'api',
-//       monorepo: false,
-//       nodejsVersion,
-//       packageManager: 'npm',
-//     })
-//   })
-
-//   test<MockApiTestContext>('should send correct command on failure', async ({ apiUrl, requests }) => {
-//     await expect(callCli(['dev:exec', 'exit 1'], getCLIOptions(apiUrl))).rejects.toThrowError()
-//     const request = requests.find(({ path }) => path === '/api/v1/track')
-//     expect(request).toBeDefined()
-
-//     expect(typeof request.body.anonymousId).toBe('string')
-//     expect(Number.isInteger(request.body.duration)).toBe(true)
-//     expect(request.body.event).toBe('cli:command')
-//     expect(request.body.status).toBe('error')
-//     expect(request.body.properties).toEqual({
-//       buildSystem: [],
-//       cliVersion: version,
-//       command: 'dev:exec',
-//       monorepo: false,
-//       nodejsVersion,
-//       packageManager: 'npm',
-//     })
-//   })
-
-//   test('should add frameworks, buildSystem, and packageManager', async ({ apiUrl, requests }) => {
-//     await withSiteBuilder('nextjs-site', async (builder) => {
-//       await builder.withPackageJson({ packageJson: { dependencies: { next: '^12.13.0' } } }).buildAsync()
-
-//       await execa(cliPath, ['api', 'listSites'], {
-//         cwd: builder.directory,
-//         ...getCLIOptions(apiUrl),
-//       })
-
-//       const request = requests.find(({ path }) => path === '/api/v1/track')
-//       expect(request).toBeDefined()
-
-//       expect(typeof request.body.anonymousId).toBe('string')
-//       expect(Number.isInteger(request.body.duration)).toBe(true)
-//       expect(request.body.event).toBe('cli:command')
-//       expect(request.body.status).toBe('success')
-//       expect(request.body.properties).toEqual({
-//         frameworks: ['next'],
-//         buildSystem: [],
-//         cliVersion: version,
-//         command: 'api',
-//         monorepo: false,
-//         nodejsVersion,
-//         packageManager: 'npm',
-//       })
-//     })
-//   })
-// })
