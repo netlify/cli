1064276ea14132fec659549e528800c2c50b6238
Sarah Etter <sarah.etter@netlify.com>
2023-11-14 09:55:32 -0500

    fix: try with env var

--- bin/run.mjs
+++ bin/run.mjs
@@ -1,11 +1,23 @@
-#!/usr/bin/env -S node --no-warnings
-import { argv } from 'process'
+#!/usr/bin/env NODE_OPTIONS=--no-warnings node
+import process, { argv } from 'process'
 
 import updateNotifier from 'update-notifier'
 
 import { createMainCommand } from '../src/commands/index.mjs'
 import { error } from '../src/utils/command-helpers.mjs'
 import getPackageJson from '../src/utils/get-package-json.mjs'
+
+const originalEmit = process.emit;
+process.emit =  (name, data, ...args) => {
+  if (
+    name === `warning` &&
+    typeof data === `object` &&
+    data.name === `ExperimentalWarning`
+  ) {
+    return false;
+  }
+  return originalEmit.apply(process, arguments);
+};
 
 // 12 hours
 const UPDATE_CHECK_INTERVAL = 432e5
