04ba3417dcca498b8fc6d03ec529b6605d79e42a
Ed Stephinson <edward.stephinson@gmail.com>
2023-11-17 10:53:08 +0000

    chore: use single threaded e2e tests

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -4,6 +4,11 @@ export default defineConfig({
   test: {
     include: ['e2e/**/*.e2e.mjs'],
     testTimeout: 600_000,
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
     deps: {
       external: ['**/fixtures/**', '**/node_modules/**'],
       interopDefault: false,
