33fe45c7f163f74b633cd5fd4995455f54893824
Sarah Etter <sarah.etter@netlify.com>
2023-11-22 14:25:01 -0500

    chore: revert dev because dev server tests are timing out, moving to another issue

--- src/commands/dev/dev.ts
+++ src/commands/dev/dev.ts
@@ -17,14 +17,17 @@ import {
 } from '../../utils/command-helpers.js'
 import detectServerSettings, { getConfigWithPlugins } from '../../utils/detect-server-settings.js'
 import { getDotEnvVariables, getSiteInformation, injectEnvVariables } from '../../utils/dev.js'
-import { getEnvelopeEnv } from '../../utils/env/index.js'
+import { getEnvelopeEnv, normalizeContext } from '../../utils/env/index.js'
 import { ensureNetlifyIgnore } from '../../utils/gitignore.js'
 import { getLiveTunnelSlug, startLiveTunnel } from '../../utils/live-tunnel.js'
 import openBrowser from '../../utils/open-browser.js'
 import { generateInspectSettings, startProxyServer } from '../../utils/proxy-server.js'
 import { getProxyUrl } from '../../utils/proxy.js'
 import { runDevTimeline } from '../../utils/run-build.js'
+import { getGeoCountryArgParser } from '../../utils/validation.js'
 import BaseCommand from '../base-command.js'
+
+import { createDevExecCommand } from './dev-exec.js'
 
 /**
  *
@@ -63,6 +66,22 @@ const handleLiveTunnel = async ({ api, options, settings, site, state }) => {
 
     return sessionUrl
   }
+}
+
+const validateShortFlagArgs = (args: string) => {
+  if (args.startsWith('=')) {
+    throw new Error(
+      `Short flag options like -e or -E don't support the '=' sign
+ ${chalk.red(BANG)}   Supported formats:
+      netlify dev -e
+      netlify dev -e 127.0.0.1:9229
+      netlify dev -e127.0.0.1:9229
+      netlify dev -E
+      netlify dev -E 127.0.0.1:9229
+      netlify dev -E127.0.0.1:9229`,
+    )
+  }
+  return args
 }
 
 export const dev = async (options: OptionValues, command: BaseCommand) => {
@@ -208,4 +227,115 @@ export const dev = async (options: OptionValues, command: BaseCommand) => {
   }
 
   printBanner({ url })
+}
+
+
+export const createDevCommand = (program: BaseCommand) => {
+  createDevExecCommand(program)
+
+  return (
+    program
+      .command('dev')
+      .alias('develop')
+      .description(
+        `Local dev server\nThe dev command will run a local dev server with Netlify's proxy and redirect rules`,
+      )
+      .option('-c ,--command <command>', 'command to run')
+      .option(
+        '--context <context>',
+        'Specify a deploy context or branch for environment variables (contexts: "production", "deploy-preview", "branch-deploy", "dev")',
+        normalizeContext,
+      )
+      // @ts-expect-error TS(7006) FIXME: Parameter 'value' implicitly has an 'any' type.
+      .option('-p ,--port <port>', 'port of netlify dev', (value) => Number.parseInt(value))
+      .addOption(
+        new Option('--targetPort <port>', 'Old, prefer --target-port. Port of target app server')
+          .argParser((value) => Number.parseInt(value))
+          .hideHelp(true),
+      )
+      .addOption(new Option('--no-open', 'disables the automatic opening of a browser window'))
+      // @ts-expect-error TS(7006) FIXME: Parameter 'value' implicitly has an 'any' type.
+      .option('--target-port <port>', 'port of target app server', (value) => Number.parseInt(value))
+      .option('--framework <name>', 'framework to use. Defaults to #auto which automatically detects a framework')
+      .option('-d ,--dir <path>', 'dir with static files')
+      .option('-f ,--functions <folder>', 'specify a functions folder to serve')
+      .option('-o ,--offline', 'disables any features that require network access')
+      .option(
+        '-l, --live [subdomain]',
+        'start a public live session; optionally, supply a subdomain to generate a custom URL',
+        false,
+      )
+      .addOption(
+        new Option('--functionsPort <port>', 'Old, prefer --functions-port. Port of functions server')
+          .argParser((value) => Number.parseInt(value))
+          .hideHelp(true),
+      )
+      // @ts-expect-error TS(7006) FIXME: Parameter 'value' implicitly has an 'any' type.
+      .option('--functions-port <port>', 'port of functions server', (value) => Number.parseInt(value))
+      .addOption(
+        new Option(
+          '--geo <mode>',
+          'force geolocation data to be updated, use cached data from the last 24h if found, or use a mock location',
+        )
+          .choices(['cache', 'mock', 'update'])
+          .default('cache'),
+      )
+      .addOption(
+        new Option(
+          '--country <geoCountry>',
+          'Two-letter country code (https://ntl.fyi/country-codes) to use as mock geolocation (enables --geo=mock automatically)',
+        ).argParser(getGeoCountryArgParser('netlify dev --geo=mock --country=FR')),
+      )
+      .addOption(
+        new Option('--staticServerPort <port>', 'port of the static app server used when no framework is detected')
+          .argParser((value) => Number.parseInt(value))
+          .hideHelp(),
+      )
+      .addOption(
+        new Option(
+          '-e, --edgeInspect [address]',
+          'Old, prefer --edge-inspect. Enable the V8 Inspector Protocol for Edge Functions, with an optional address in the host:port format',
+        )
+          .conflicts('edgeInspectBrk')
+          .argParser(validateShortFlagArgs)
+          .hideHelp(true),
+      )
+      .addOption(
+        new Option(
+          '-e, --edge-inspect [address]',
+          'enable the V8 Inspector Protocol for Edge Functions, with an optional address in the host:port format',
+        )
+          .conflicts('edgeInspectBrk')
+          .argParser(validateShortFlagArgs),
+      )
+      .addOption(
+        new Option(
+          '-E, --edgeInspectBrk [address]',
+          'Old, prefer --edge-inspect-brk. Enable the V8 Inspector Protocol for Edge Functions and pause execution on the first line of code, with an optional address in the host:port format',
+        )
+          .conflicts('edgeInspect')
+          .hideHelp(true)
+          .argParser(validateShortFlagArgs),
+      )
+      .addOption(
+        new Option(
+          '-E, --edge-inspect-brk [address]',
+          'enable the V8 Inspector Protocol for Edge Functions and pause execution on the first line of code, with an optional address in the host:port format',
+        )
+          .conflicts('edgeInspect')
+          .argParser(validateShortFlagArgs),
+      )
+      .addExamples([
+        'netlify dev',
+        'netlify dev -d public',
+        'netlify dev -c "hugo server -w" --target-port 1313',
+        'netlify dev --context production',
+        'netlify dev --edge-inspect',
+        'netlify dev --edge-inspect=127.0.0.1:9229',
+        'netlify dev --edge-inspect-brk',
+        'netlify dev --edge-inspect-brk=127.0.0.1:9229',
+        'BROWSER=none netlify dev # disable browser auto opening',
+      ])
+      .action(dev)
+  )
 }
