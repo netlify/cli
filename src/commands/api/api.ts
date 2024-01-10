5aab8c6e8d6d60db7bd8fb2bdd44a4c4eb647b2f
khen <30577427+khendrikse@users.noreply.github.com>
2024-01-10 10:07:30 +0100

    chore: convert api command to use clack

--- src/commands/api/api.ts
+++ src/commands/api/api.ts
@@ -2,32 +2,36 @@ import AsciiTable from 'ascii-table'
 import { OptionValues } from 'commander'
 import { methods } from 'netlify'
 
-import { chalk, error, exit, log, logJson } from '../../utils/command-helpers.js'
+import { chalk, exit, logJson } from '../../utils/command-helpers.js'
+import { NetlifyLog, intro, outro } from '../../utils/styles/index.js'
 import BaseCommand from '../base-command.js'
 
 export const apiCommand = async (apiMethod: string, options: OptionValues, command: BaseCommand) => {
   const { api } = command.netlify
 
   if (options.list) {
+    intro('api')
     const table = new AsciiTable(`Netlify API Methods`)
     table.setHeading('API Method', 'Docs Link')
     methods.forEach((method) => {
       const { operationId } = method
       table.addRow(operationId, `https://open-api.netlify.com/#operation/${operationId}`)
     })
-    log(table.toString())
-    log()
-    log('Above is a list of available API methods')
-    log(`To run a method use "${chalk.cyanBright('netlify api methodName')}"`)
+    NetlifyLog.message(table.toString())
+    NetlifyLog.message(
+      `Above is a list of available API methods. To run a method use "${chalk.cyanBright('netlify api methodName')}"`,
+    )
+
+    outro()
     exit()
   }
 
   if (!apiMethod) {
-    error(`You must provide an API method. Run "netlify api --list" to see available methods`)
+    NetlifyLog.error(`You must provide an API method. Run "netlify api --list" to see available methods`)
   }
 
   if (!api[apiMethod] || typeof api[apiMethod] !== 'function') {
-    error(`"${apiMethod}"" is not a valid api method. Run "netlify api --list" to see available methods`)
+    NetlifyLog.error(`"${apiMethod}"" is not a valid api method. Run "netlify api --list" to see available methods`)
   }
 
   let payload
@@ -39,8 +43,11 @@ export const apiCommand = async (apiMethod: string, options: OptionValues, comma
   try {
     const apiResponse = await api[apiMethod](payload)
     logJson(apiResponse)
-  } catch (error_) {
-    // @ts-expect-error TS(2345) FIXME: Argument of type 'unknown' is not assignable to pa... Remove this comment to see the full error message
-    error(error_)
+  } catch (error) {
+    if (error instanceof Error || typeof error === 'string') {
+      NetlifyLog.error(error)
+    } else {
+      NetlifyLog.error('An unknown error occurred')
+    }
   }
 }
