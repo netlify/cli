5d98d73c8c1afcffed743d7346e208da50d1f8b7
Karin Hendrikse <30577427+khendrikse@users.noreply.github.com>
2024-01-15 10:11:35 +0100

    chore: convert link command (#6313)
    * chore: improve API command with spinner and better formatting
    
    * chore: convert api command to use clack
    
    * chore: convert link to use NetlifyLog and clack
    
    * chore: updates for converting the api command (#6295)
    
    * chore: improve API command with spinner and better formatting
    
    * chore: add support for json only
    
    * chore: more converting of link and changed json only support
    
    * revert weird chalk helper
    
    * chore: implement some feedback

--- src/commands/link/link.ts
+++ src/commands/link/link.ts
@@ -1,106 +1,93 @@
 import { OptionValues } from 'commander'
-import inquirer from 'inquirer'
 import isEmpty from 'lodash/isEmpty.js'
 
 import { listSites } from '../../lib/api.js'
-import { chalk, error, exit, log } from '../../utils/command-helpers.js'
+import { chalk } from '../../utils/command-helpers.js'
 import getRepoData from '../../utils/get-repo-data.js'
 import { ensureNetlifyIgnore } from '../../utils/gitignore.js'
+import { NetlifyLog, intro, outro, select, spinner, text } from '../../utils/styles/index.js'
 import { track } from '../../utils/telemetry/index.js'
 import BaseCommand from '../base-command.js'
 
-/**
- *
- * @param {import('../base-command.js').default} command
- * @param {import('commander').OptionValues} options
- */
-// @ts-expect-error TS(7006) FIXME: Parameter 'command' implicitly has an 'any' type.
-const linkPrompt = async (command, options) => {
+const linkPrompt = async (command: BaseCommand, options: OptionValues) => {
   const { api, state } = command.netlify
 
   const SITE_NAME_PROMPT = 'Search by full or partial site name'
   const SITE_LIST_PROMPT = 'Choose from a list of your recently updated sites'
   const SITE_ID_PROMPT = 'Enter a site ID'
-
   let GIT_REMOTE_PROMPT = 'Use the current git remote origin URL'
   let site
   // Get git remote data if exists
   const repoData = await getRepoData({ workingDir: command.workingDir, remoteName: options.gitRemoteName })
 
-  let linkChoices = [SITE_NAME_PROMPT, SITE_LIST_PROMPT, SITE_ID_PROMPT]
+  let linkOptions = [{ value: SITE_NAME_PROMPT }, { value: SITE_LIST_PROMPT }, { value: SITE_ID_PROMPT }]
 
   if (!repoData.error) {
     // Add git GIT_REMOTE_PROMPT if in a repo
     GIT_REMOTE_PROMPT = `Use current git remote origin (${repoData.httpsUrl})`
-    linkChoices = [GIT_REMOTE_PROMPT, ...linkChoices]
+    linkOptions = [{ value: GIT_REMOTE_PROMPT }, ...linkOptions]
   }
 
-  log()
-  log(`${chalk.cyanBright('netlify link')} will connect this folder to a site on Netlify`)
-  log()
-  const { linkType } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'linkType',
-      message: 'How do you want to link this folder to a site?',
-      choices: linkChoices,
-    },
-  ])
+  NetlifyLog.message(`${chalk.cyanBright('netlify link')} will connect this folder to a site on Netlify`)
+
+  const linkType = await select({
+    message: 'How do you want to link this folder to a site?',
+    options: linkOptions,
+  })
 
   let kind
   switch (linkType) {
     case GIT_REMOTE_PROMPT: {
       kind = 'gitRemote'
-      log()
-      log(`Looking for sites connected to '${repoData.httpsUrl}'...`)
-      log()
+      const loading = spinner()
+      loading.start(`Looking for sites connected to '${repoData.httpsUrl}' (this can take a bit).`)
       const sites = await listSites({ api, options: { filter: 'all' } })
-
       if (sites.length === 0) {
-        error(`You don't have any sites yet. Run ${chalk.cyanBright('netlify sites:create')} to create a site.`)
+        loading.stop(
+          `You don't have any sites yet. Run ${chalk.cyanBright('netlify sites:create')} to create a site.`,
+          1,
+        )
+        outro({ exit: true })
       }
 
       const matchingSites = sites.filter(
-        // @ts-expect-error TS(2339) FIXME: Property 'repo_url' does not exist on type '{}'.
-        ({ build_settings: buildSettings = {} }) => repoData.httpsUrl === buildSettings.repo_url,
+        ({ build_settings: buildSettings = {} }: { build_settings: { repo_url?: string } }) =>
+          repoData.httpsUrl === buildSettings.repo_url,
       )
 
       // If no remote matches. Throw error
       if (matchingSites.length === 0) {
-        log(chalk.redBright.bold.underline(`No Matching Site Found`))
-        log()
-        log(`No site found with the remote ${repoData.httpsUrl}.
+        NetlifyLog.warn(chalk.bold(`No matching Site Found`))
 
-Double check you are in the correct working directory and a remote origin repo is configured.
+        NetlifyLog.message(`We couldn't find a site with the remote ${repoData.httpsUrl}.
+            
+            Double check you are in the correct working directory and a remote origin repository is configured.
+            
+            Run ${chalk.cyanBright('git remote -v')} to see a list of your git remotes.`)
 
-Run ${chalk.cyanBright('git remote -v')} to see a list of your git remotes.`)
-
-        exit()
+        outro({ exit: true })
       }
 
       // Matches a single site hooray!
       if (matchingSites.length === 1) {
+        loading.stop('Found a matching site!')
         const [firstSite] = matchingSites
         site = firstSite
       } else if (matchingSites.length > 1) {
         // Matches multiple sites. Users must choose which to link.
-        log(`Found ${matchingSites.length} matching sites!`)
+        loading.stop(`Found ${matchingSites.length} matching sites!`)
 
         // Prompt which options
-        const { selectedSite } = await inquirer.prompt([
-          {
-            type: 'list',
-            name: 'selectedSite',
-            message: 'Which site do you want to link?',
-            // @ts-expect-error TS(7006) FIXME: Parameter 'matchingSite' implicitly has an 'any' t... Remove this comment to see the full error message
-            choices: matchingSites.map((matchingSite) => ({
-              name: `${matchingSite.name} - ${matchingSite.ssl_url}`,
-              value: matchingSite,
-            })),
-          },
-        ])
+        const selectedSite = await select({
+          message: 'Which site do you want to link?',
+          options: matchingSites.map((matchingSite: { name: string; ssl_url: string }) => ({
+            label: `${matchingSite.name} - ${matchingSite.ssl_url}`,
+            value: matchingSite,
+          })),
+        })
+
         if (!selectedSite) {
-          error('No site selected')
+          NetlifyLog.error('No site selected')
         }
         site = selectedSite
       }
@@ -108,15 +95,14 @@ Run ${chalk.cyanBright('git remote -v')} to see a list of your git remotes.`)
     }
     case SITE_NAME_PROMPT: {
       kind = 'byName'
-      const { searchTerm } = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'searchTerm',
-          message: 'Enter the site name (or just part of it):',
+      const searchTerm = await text({
+        message: 'Enter the site name (or just part of it):',
+        validate: (value) => {
+          if (!value) return 'Please enter a site name'
         },
-      ])
-      log(`Looking for sites with names containing '${searchTerm}'...`)
-      log()
+      })
+      const loading = spinner()
+      loading.start(`Looking for sites with names containing '${searchTerm}'`)
 
       let matchingSites
       try {
@@ -124,40 +110,41 @@ Run ${chalk.cyanBright('git remote -v')} to see a list of your git remotes.`)
           api,
           options: { name: searchTerm, filter: 'all' },
         })
-      } catch (error_) {
-        // @ts-expect-error TS(2571) FIXME: Object is of type 'unknown'.
-        if (error_.status === 404) {
-          error(`'${searchTerm}' not found`)
+      } catch (error: unknown) {
+        if ((error as { status: number }).status === 404) {
+          NetlifyLog.error(`'${searchTerm}' not found`)
         } else {
-          // @ts-expect-error TS(2345) FIXME: Argument of type 'unknown' is not assignable to pa... Remove this comment to see the full error message
-          error(error_)
+          NetlifyLog.error(error)
         }
       }
 
       if (!matchingSites || matchingSites.length === 0) {
-        error(`No site names found containing '${searchTerm}'.
-
-Run ${chalk.cyanBright('netlify link')} again to try a new search,
-or run ${chalk.cyanBright('netlify sites:create')} to create a site.`)
+        loading.stop(`No site names found containing '${searchTerm}'.`, 1)
+        outro({
+          message: `Run ${chalk.cyanBright('netlify link')} again to try a new search, or run ${chalk.cyanBright(
+            'netlify sites:create',
+          )} to create a site.`,
+          exit: true,
+        })
       }
 
       if (matchingSites.length > 1) {
-        log(`Found ${matchingSites.length} matching sites!`)
-        const { selectedSite } = await inquirer.prompt([
-          {
-            type: 'list',
-            name: 'selectedSite',
-            message: 'Which site do you want to link?',
-            paginated: true,
-            // @ts-expect-error TS(7006) FIXME: Parameter 'matchingSite' implicitly has an 'any' t... Remove this comment to see the full error message
-            choices: matchingSites.map((matchingSite) => ({ name: matchingSite.name, value: matchingSite })),
-          },
-        ])
+        loading.stop(`Found ${matchingSites.length} matching sites!`)
+
+        const selectedSite = await select({
+          message: 'Which site do you want to link?',
+          options: matchingSites.map((matchingSite: { name: string; ssl_url: string }) => ({
+            label: matchingSite.name,
+            value: matchingSite,
+          })),
+        })
+
         if (!selectedSite) {
-          error('No site selected')
+          NetlifyLog.error('No site selected')
         }
         site = selectedSite
       } else {
+        loading.stop('Found a matching site!')
         const [firstSite] = matchingSites
         site = firstSite
       }
@@ -165,56 +152,54 @@ or run ${chalk.cyanBright('netlify sites:create')} to create a site.`)
     }
     case SITE_LIST_PROMPT: {
       kind = 'fromList'
-      log(`Fetching recently updated sites...`)
-      log()
+      const loading = spinner()
+      loading.start(`Fetching recently updated sites...`)
 
       let sites
       try {
         sites = await listSites({ api, options: { maxPages: 1, filter: 'all' } })
       } catch (error_) {
-        // @ts-expect-error TS(2345) FIXME: Argument of type 'unknown' is not assignable to pa... Remove this comment to see the full error message
-        error(error_)
+        NetlifyLog.error(error_)
       }
 
       if (!sites || sites.length === 0) {
-        error(`You don't have any sites yet. Run ${chalk.cyanBright('netlify sites:create')} to create a site.`)
+        NetlifyLog.error(
+          `You don't have any sites yet. Run ${chalk.cyanBright('netlify sites:create')} to create a site.`,
+        )
       }
 
-      const { selectedSite } = await inquirer.prompt([
-        {
-          type: 'list',
-          name: 'selectedSite',
-          message: 'Which site do you want to link?',
-          paginated: true,
-          // @ts-expect-error TS(7006) FIXME: Parameter 'matchingSite' implicitly has an 'any' t... Remove this comment to see the full error message
-          choices: sites.map((matchingSite) => ({ name: matchingSite.name, value: matchingSite })),
-        },
-      ])
+      loading.stop(`Found ${sites.length} sites!`)
+      const selectedSite = await select({
+        message: 'Which site do you want to link?',
+        maxItems: 7,
+        options: sites.map((matchingSite: { name: string; ssl_url: string }) => ({
+          label: matchingSite.name,
+          value: matchingSite,
+        })),
+      })
+
       if (!selectedSite) {
-        error('No site selected')
+        NetlifyLog.error('No site selected')
       }
       site = selectedSite
       break
     }
     case SITE_ID_PROMPT: {
       kind = 'bySiteId'
-      const { siteId } = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'siteId',
-          message: 'What is the site ID?',
+      const siteId = await text({
+        message: 'What is the site ID?',
+        validate: (value) => {
+          if (!value) return 'Please enter a site ID'
         },
-      ])
+      })
 
       try {
         site = await api.getSite({ siteId })
-      } catch (error_) {
-        // @ts-expect-error TS(2571) FIXME: Object is of type 'unknown'.
-        if (error_.status === 404) {
-          error(new Error(`Site ID '${siteId}' not found`))
+      } catch (error: unknown) {
+        if ((error as { status: number }).status === 404) {
+          NetlifyLog.error(new Error(`Site ID '${siteId}' not found`))
         } else {
-          // @ts-expect-error TS(2345) FIXME: Argument of type 'unknown' is not assignable to pa... Remove this comment to see the full error message
-          error(error_)
+          NetlifyLog.error(error)
         }
       }
       break
@@ -224,7 +209,7 @@ or run ${chalk.cyanBright('netlify sites:create')} to create a site.`)
   }
 
   if (!site) {
-    error(new Error(`No site found`))
+    NetlifyLog.error(new Error(`No site found`))
   }
 
   // Save site ID to config
@@ -237,20 +222,18 @@ or run ${chalk.cyanBright('netlify sites:create')} to create a site.`)
   })
 
   // Log output
-  log()
-  log(chalk.greenBright.bold.underline(`Directory Linked`))
-  log()
-  log(`Admin url: ${chalk.magentaBright(site.admin_url)}`)
-  log(`Site url:  ${chalk.cyanBright(site.ssl_url || site.url)}`)
-  log()
-  log(`You can now run other \`netlify\` cli commands in this directory`)
+  NetlifyLog.success('Directory Linked')
+  NetlifyLog.message(`Admin URL: ${chalk.magentaBright(site.admin_url)}`)
+  NetlifyLog.message(`Site URL:  ${chalk.cyanBright(site.ssl_url || site.url)}`)
+
+  outro({ message: `You can now run other \`netlify\` cli commands in this directory` })
 
   return site
 }
 
 export const link = async (options: OptionValues, command: BaseCommand) => {
   await command.authenticate()
-
+  intro('link')
   const {
     api,
     repositoryRoot,
@@ -266,33 +249,33 @@ export const link = async (options: OptionValues, command: BaseCommand) => {
 
   // Site id is incorrect
   if (siteId && isEmpty(siteData)) {
-    log(`"${siteId}" was not found in your Netlify account.`)
-    log(`Please double check your siteID and which account you are logged into via \`netlify status\`.`)
-    return exit()
+    NetlifyLog.error(`"${siteId}" was not found in your Netlify account.`)
+    outro({
+      message: `Please double check your site ID and which account you are logged into via \`netlify status\`.`,
+    })
   }
 
   if (!isEmpty(siteInfo)) {
     // If already linked to site. exit and prompt for unlink
-    log(`Site already linked to "${siteData.name}"`)
-    log(`Admin url: ${siteData.admin_url}`)
-    log()
-    log(`To unlink this site, run: ${chalk.cyanBright('netlify unlink')}`)
+    NetlifyLog.info(`Site already linked to "${siteData.name}"`)
+    NetlifyLog.message(`Admin URL: ${siteData.admin_url}`)
+    outro({ message: `To unlink this site, run: ${chalk.cyanBright('netlify unlink')}` })
   } else if (options.id) {
     try {
       siteData = await api.getSite({ site_id: options.id })
-    } catch (error_) {
-      // @ts-expect-error TS(2571) FIXME: Object is of type 'unknown'.
-      if (error_.status === 404) {
-        error(new Error(`Site id ${options.id} not found`))
+    } catch (error: unknown) {
+      if ((error as { status: number }).status === 404) {
+        NetlifyLog.error(new Error(`Site with ID ${options.id} has not been found`))
       } else {
-        // @ts-expect-error TS(2345) FIXME: Argument of type 'unknown' is not assignable to pa... Remove this comment to see the full error message
-        error(error_)
+        NetlifyLog.error(error)
       }
     }
 
     // Save site ID
     state.set('siteId', siteData.id)
-    log(`Linked to ${siteData.name}`)
+    NetlifyLog.success(`Linked to ${siteData.name}`)
+    NetlifyLog.message(`Admin URL: ${siteData.admin_url}`)
+    outro({ message: `To unlink this site, run: ${chalk.cyanBright('netlify unlink')}` })
 
     await track('sites_linked', {
       siteId: siteData.id,
@@ -309,23 +292,22 @@ export const link = async (options: OptionValues, command: BaseCommand) => {
           filter: 'all',
         },
       })
-    } catch (error_) {
-      // @ts-expect-error TS(2571) FIXME: Object is of type 'unknown'.
-      if (error_.status === 404) {
-        error(new Error(`${options.name} not found`))
+    } catch (error: unknown) {
+      if ((error as { status: number }).status === 404) {
+        NetlifyLog.error(new Error(`${options.name} not found`))
       } else {
-        // @ts-expect-error TS(2345) FIXME: Argument of type 'unknown' is not assignable to pa... Remove this comment to see the full error message
-        error(error_)
+        NetlifyLog.error(error)
       }
     }
 
     if (results.length === 0) {
-      error(new Error(`No sites found named ${options.name}`))
+      NetlifyLog.error(new Error(`No sites found named ${options.name}`))
     }
     const [firstSiteData] = results
     state.set('siteId', firstSiteData.id)
-
-    log(`Linked to ${firstSiteData.name}`)
+    NetlifyLog.success(`Linked to ${firstSiteData.name}`)
+    NetlifyLog.message(`Admin URL: ${firstSiteData.admin_url}`)
+    outro({ message: `To unlink this site, run: ${chalk.cyanBright('netlify unlink')}` })
 
     await track('sites_linked', {
       siteId: (firstSiteData && firstSiteData.id) || siteId,
