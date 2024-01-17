db13115791e194ba11a283de6cba36418e1f32a6
Lewis Thorley <lewis.thorley@netlify.com>
2024-01-17 16:58:35 +0000

    chore: convert switch command

--- src/commands/switch/switch.ts
+++ src/commands/switch/switch.ts
@@ -1,41 +1,45 @@
 import { OptionValues } from 'commander'
 import inquirer from 'inquirer'
 
-import { chalk, log } from '../../utils/command-helpers.js'
+import { chalk } from '../../utils/command-helpers.js'
 import BaseCommand from '../base-command.js'
 import { login } from '../login/login.js'
+import { NetlifyLog, SelectOptions, intro, outro, select } from '../../utils/styles/index.js'
 
 const LOGIN_NEW = 'I would like to login to a new account'
 
-export const switchCommand = async (options: OptionValues, command: BaseCommand) => {
-  const availableUsersChoices = Object.values(command.netlify.globalConfig.get('users') || {}).reduce(
+type User = { id: string; name: string; email: string }
+type UserMap = { [key: string]: User }
+export const switchCommand = async (_: OptionValues, command: BaseCommand) => {
+  intro('switch')
+  const users = (command.netlify.globalConfig.get('users') as UserMap | undefined) || {}
+  const availableUsersChoices = Object.values(users).reduce<Record<string, string>>(
     (prev, current) =>
-      // @ts-expect-error TS(2769) FIXME: No overload matches this call.
-      Object.assign(prev, { [current.id]: current.name ? `${current.name} (${current.email})` : current.email }),
+      Object.assign(prev, {
+        [current.id]: current.name ? `${current.name} (${current.email})` : (current.email as string),
+      }),
     {},
   )
-
-  const { accountSwitchChoice } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'accountSwitchChoice',
-      message: 'Please select the account you want to use:',
-      // @ts-expect-error TS(2769) FIXME: No overload matches this call.
-      choices: [...Object.entries(availableUsersChoices).map(([, val]) => val), LOGIN_NEW],
-    },
-  ])
+  const accountSelectOptions: SelectOptions<string> = {
+    options: [
+      ...Object.entries(availableUsersChoices).map(([, val]) => ({ label: val, value: val })),
+      { label: LOGIN_NEW, value: LOGIN_NEW },
+    ],
+    message: 'Please select the account you want to use:',
+  }
+  const accountSwitchChoice = await select(accountSelectOptions)
 
   if (accountSwitchChoice === LOGIN_NEW) {
     await login({ new: true }, command)
   } else {
-    // @ts-expect-error TS(2769) FIXME: No overload matches this call.
     const selectedAccount = Object.entries(availableUsersChoices).find(
       ([, availableUsersChoice]) => availableUsersChoice === accountSwitchChoice,
     )
     // @ts-expect-error TS(2532) FIXME: Object is possibly 'undefined'.
     command.netlify.globalConfig.set('userId', selectedAccount[0])
-    log('')
     // @ts-expect-error TS(2532) FIXME: Object is possibly 'undefined'.
-    log(`You're now using ${chalk.bold(selectedAccount[1])}.`)
+    outro({ exit: true, message: `You're now using ${chalk.bold(selectedAccount[1])}.` })
   }
+
+  outro({ exit: true })
 }
