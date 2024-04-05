12c718ec3492f2aff3aa823e79ebd1ff7d041ba5
Simon Knott <info@simonknott.de>
2024-04-05 10:35:51 +0200

    feat: make edge functions proxy optional

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -680,7 +680,10 @@ const onRequest = async (
     rewriter,
     settings,
     siteInfo,
-  }: { rewriter: Rewriter; settings: ServerSettings } & Record<string, $TSFixMe>,
+  }: { rewriter: Rewriter; settings: ServerSettings; edgeFunctionsProxy?: EdgeFunctionsProxy } & Record<
+    string,
+    $TSFixMe
+  >,
   req: Request,
   res: ServerResponse,
 ) => {
@@ -691,7 +694,7 @@ const onRequest = async (
     return imageProxy(req, res)
   }
 
-  const edgeFunctionsProxyURL = await edgeFunctionsProxy(req, res)
+  const edgeFunctionsProxyURL = await edgeFunctionsProxy?.(req as any)
 
   if (edgeFunctionsProxyURL !== undefined) {
     return proxy.web(req, res, { target: edgeFunctionsProxyURL })
@@ -776,6 +779,8 @@ export const getProxyUrl = function (settings: Pick<ServerSettings, 'https' | 'p
   return `${scheme}://localhost:${settings.port}`
 }
 
+type EdgeFunctionsProxy = Awaited<ReturnType<typeof initializeEdgeFunctionsProxy>>
+
 export const startProxy = async function ({
   accountId,
   addonsUrls,
@@ -799,27 +804,31 @@ export const startProxy = async function ({
 }: { command: BaseCommand; settings: ServerSettings } & Record<string, $TSFixMe>) {
   const secondaryServerPort = settings.https ? await getAvailablePort() : null
   const functionsServer = settings.functionsPort ? `http://127.0.0.1:${settings.functionsPort}` : null
-  const edgeFunctionsProxy = await initializeEdgeFunctionsProxy({
-    command,
-    blobsContext,
-    config,
-    configPath,
-    debug,
-    env,
-    geolocationMode,
-    geoCountry,
-    getUpdatedConfig,
-    inspectSettings,
-    mainPort: settings.port,
-    offline,
-    passthroughPort: secondaryServerPort || settings.port,
-    settings,
-    projectDir,
-    repositoryRoot,
-    siteInfo,
-    accountId,
-    state,
-  })
+
+  let edgeFunctionsProxy: EdgeFunctionsProxy | undefined
+  if (true) {
+    edgeFunctionsProxy = await initializeEdgeFunctionsProxy({
+      command,
+      blobsContext,
+      config,
+      configPath,
+      debug,
+      env,
+      geolocationMode,
+      geoCountry,
+      getUpdatedConfig,
+      inspectSettings,
+      mainPort: settings.port,
+      offline,
+      passthroughPort: secondaryServerPort || settings.port,
+      settings,
+      projectDir,
+      repositoryRoot,
+      siteInfo,
+      accountId,
+      state,
+    })
+  }
 
   const imageProxy = await initializeImageProxy({
     config,
