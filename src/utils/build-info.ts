827e25c634bfeea18988e6313e93e90faac124e4
Lukas Holzer <lukas.holzer@netlify.com>
2024-07-12 11:29:00 +0200

    chore: fix case where no plugins recommended are in the settings

--- src/utils/build-info.ts
+++ src/utils/build-info.ts
@@ -148,7 +148,7 @@ export const getDefaultConfig = (settings?: Settings): $TSFixMe | undefined => {
     config.build.publishOrigin = 'default'
   }
 
-  config.plugins = settings.plugins_recommended.map((plugin) => ({ package: plugin }))
+  config.plugins = settings.plugins_recommended?.map((plugin) => ({ package: plugin })) || []
 
   return config
 }
