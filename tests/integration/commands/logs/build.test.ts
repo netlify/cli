4175d897f7b39f6eb0c6e60808f56bde831c3625
benhancock <benhancock859@gmail.com>
2024-10-10 10:31:29 -0400

    fix: logs: deploy command instructs user to link to a site if one is not linked
    Co-authored-by: Dylan Spyer <dylanspyer@gmail.com>

--- tests/integration/commands/logs/build.test.ts
+++ tests/integration/commands/logs/build.test.ts
@@ -6,6 +6,9 @@ import { getWebSocket } from '../../../../src/utils/websockets/index.js'
 import { startMockApi } from '../../utils/mock-api-vitest.js'
 import { getEnvironmentVariables } from '../../utils/mock-api.js'
 import { callCli } from '../../utils/call-cli.js'
+import { getCLIOptions, withMockApi } from '../../utils/mock-api.js'
+import { withSiteBuilder } from '../../utils/site-builder.ts'
+import { join } from 'path'
 
 vi.mock('../../../../src/utils/websockets/index.js', () => ({
   getWebSocket: vi.fn(),
@@ -111,9 +114,20 @@ describe('logs:deploy command', () => {
     expect(body.access_token).toEqual(env.NETLIFY_AUTH_TOKEN)
   })
 
-  test('should instruct user to link a site if one is not linked', async () => {
-    expect(process.env.NETLIFY_SITE_ID).toBe(undefined)
-    const stdout = await callCli(['logs:deploy'])
-    expect(stdout).toContain('You must link a site before attempting to view deploy logs')
+  test('should instruct user to link a site if one is not linked', async (t) => {
+    await withSiteBuilder(t, async (builder) => {
+      const projectPath = join('projects', 'project1')
+      await builder.withNetlifyToml({ config: {}, pathPrefix: projectPath }).build()
+
+      await withMockApi(
+        routes,
+        async ({ apiUrl }) => {
+          const options = getCLIOptions({ builder, apiUrl, env: { NETLIFY_SITE_ID: '' }})
+          const stdout = await callCli(['logs:deploy'], { ...options, cwd: join(builder.directory, projectPath) })
+          expect(stdout).toContain('You must link a site before attempting to view deploy logs')
+        },
+        true,
+      )
+    })
   })
 })
