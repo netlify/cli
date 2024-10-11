cf11d730612f9c2c07031f87348c2be47c893401
Dylan Spyer <dylanspyer@gmail.com>
2024-10-11 13:31:23 -0500

    fix: link --name prefers exact match if exists, first match otherwise
    Co-authored-by: Ben Hancock <benhancock859@gmail.com>

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,7 @@
 import { warn } from '../utils/command-helpers.js'
 
+import { SiteInfo } from './types.js'
+
 // @ts-expect-error TS(7031) FIXME: Binding element 'api' implicitly has an 'any' type... Remove this comment to see the full error message
 export const cancelDeploy = async ({ api, deployId }) => {
   try {
@@ -13,8 +15,9 @@ export const cancelDeploy = async ({ api, deployId }) => {
 const FIRST_PAGE = 1
 const MAX_PAGES = 10
 const MAX_PER_PAGE = 100
+
 // @ts-expect-error TS(7023) FIXME: 'listSites' implicitly has return type 'any' becau... Remove this comment to see the full error message
-export const listSites = async ({ api, options }) => {
+export const listSites = async ({ api, options }): SiteInfo[] => {
   const { maxPages = MAX_PAGES, page = FIRST_PAGE, ...rest } = options
   const sites = await api.listSites({ page, per_page: MAX_PER_PAGE, ...rest })
   // TODO: use pagination headers when js-client returns them
