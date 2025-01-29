02f45d98e1febd61b10c23531b9ec9453b48efa1
v1rtl <hi@v1rtl.site>
2025-01-29 17:11:10 +0200

    chore(deps): remove hasha (#7017)

--- src/utils/deploy/hash-config.ts
+++ src/utils/deploy/hash-config.ts
@@ -1,4 +1,5 @@
-import hasha from 'hasha'
+import { createHash } from 'node:crypto'
+
 // @ts-expect-error TS(7016) FIXME: Could not find a declaration file for module 'toml... Remove this comment to see the full error message
 import tomlify from 'tomlify-j0.4'
 
@@ -7,7 +8,7 @@ export const hashConfig = ({ config }) => {
   if (!config) throw new Error('Missing config option')
   const configString = serializeToml(config)
 
-  const hash = hasha(configString, { algorithm: 'sha1' })
+  const hash = createHash('sha1').update(configString).digest('hex')
 
   return {
     assetType: 'file',
