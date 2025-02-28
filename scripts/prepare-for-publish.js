94cb80acfb76260f39270f041ba0d8db38c2991d
Philippe Serhal <philippe.serhal@netlify.com>
2025-02-28 06:41:59 -0500

    chore: simplify and standardize `package.json` scripts and CI setup (#7069)
    * chore(package.json): simplify site build
    
    There were two separate scripts for doing the same thing.
    
    * refactor(scripts): split up prepare-for-publish
    
    No behavioural changes in this commit.
    
    * chore: remove bizarre postinstall hook that builds
    
    - It's nonstandard
    - It's surprising
    - Since we use `tsc` to build, it causes every CI check to fail if you have type errors
    - It causes issues when rewriting git history
    - It's a bit magical
    
    * chore: remove leftover refs to ava
    
    ava was removed a couple years ago
    
    * ci: rename misleading jobs
    
    They handle two kinds of benchmarks, only one of which is package size
    
    * chore: split formating, linting, and unit testing
    
    * ci: simplify workflow skipping for release branches
    
    * chore(package.json): sort scripts
    
    * chore(deps): add missing dependency on npm-run-all
    
    We're using `run-s` which comes from this dep but it only happens to work because one of
    our dependencies has a transitive dependency on `npm-run-all`.
    
    IMO we should kill this, but for now this just adds the missing dep.
    
    Note: npm-run-all2 is a maintained fork of the unmaintained npm-run-all

--- scripts/prepare-for-publish.js
+++ scripts/prepare-for-publish.js
@@ -5,8 +5,29 @@ import { fileURLToPath } from 'url'
 import execa from 'execa'
 import ora from 'ora'
 
-// These scripts from package.json need to be preserved on publish
-const preserveScripts = new Set([
+const insertUserPostinstall = (pkgJson) => {
+  pkgJson.scripts.postinstall = pkgJson.scripts['postinstall-pack']
+  delete pkgJson.scripts['postinstall-pack']
+}
+
+const installDeps = async () => {
+  let spinner = ora('Running `npm install --no-audit`').start()
+  await execa('npm', ['install', '--no-audit'])
+  spinner.succeed()
+
+  spinner = ora('Running `npm shrinkwrap`').start()
+  await execa('npm', ['shrinkwrap'])
+  spinner.succeed()
+}
+
+// NOTE: this must run before `cleanPackageJson`, otherwise the `build` script has been deleted
+const buildPackageForPublish = async () => {
+  const spinner = ora('Running `npm run build`').start()
+  await execa('npm', ['run', 'build'])
+  spinner.succeed()
+}
+
+const SCRIPTS_TO_KEEP_ON_PUBLISHED_PKG = new Set([
   'postinstall-pack',
   'postpack',
   'preinstall',
@@ -15,42 +36,35 @@ const preserveScripts = new Set([
   'prepublishOnly',
 ])
 
-let spinner = ora({
-  spinner: 'star',
-  text: 'Patching package.json (removing devDependencies, scripts, etc)',
-}).start()
+const cleanPackageJson = (pkgJson) => {
+  delete pkgJson.devDependencies
+  delete pkgJson.config
 
-const dir = dirname(fileURLToPath(import.meta.url))
-const packageJsonPath = join(dir, '../package.json')
+  for (const scriptName in pkgJson.scripts) {
+    if (SCRIPTS_TO_KEEP_ON_PUBLISHED_PKG.has(scriptName)) continue
 
-const pkgJson = JSON.parse(await readFile(packageJsonPath))
-
-delete pkgJson.devDependencies
-delete pkgJson.ava
-delete pkgJson.config
-
-for (const scriptName in pkgJson.scripts) {
-  if (preserveScripts.has(scriptName)) continue
-
-  delete pkgJson.scripts[scriptName]
+    delete pkgJson.scripts[scriptName]
+  }
 }
 
-pkgJson.scripts.postinstall = pkgJson.scripts['postinstall-pack']
-delete pkgJson.scripts['postinstall-pack']
+const writeUpdatedPackageJson = async (pkgJson, packageJsonPath) => {
+  const spinner = ora('Patching package.json (removing devDependencies, scripts, etc').start()
+  await writeFile(packageJsonPath, JSON.stringify(pkgJson, null, 2))
+  spinner.succeed()
+}
 
-await writeFile(packageJsonPath, JSON.stringify(pkgJson, null, 2))
-spinner.succeed()
+const main = async () => {
+  const dir = dirname(fileURLToPath(import.meta.url))
+  const pkgJsonPath = join(dir, '../package.json')
+  const pkgJson = JSON.parse(await readFile(pkgJsonPath))
 
-spinner = ora({
-  spinner: 'star',
-  text: 'Running `npm install --no-audit`',
-}).start()
-await execa('npm', ['install', '--no-audit'])
-spinner.succeed()
+  await installDeps()
 
-spinner = ora({
-  spinner: 'star',
-  text: 'Running `npm shrinkwrap`',
-}).start()
-await execa('npm', ['shrinkwrap'])
-spinner.succeed()
+  await buildPackageForPublish()
+
+  insertUserPostinstall(pkgJson)
+  cleanPackageJson(pkgJson)
+  await writeUpdatedPackageJson(pkgJson, pkgJsonPath)
+}
+
+await main()
