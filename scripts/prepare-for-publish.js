4571be31c1ec29460327bc5f7061406729762c16
Nathan Houle <nathan@nathanhoule.com>
2025-03-04 12:18:11 -0800

    revert: "chore: simplify and standardize `package.json` scripts and CI setup (#7069)"
    This reverts commit 94cb80acfb76260f39270f041ba0d8db38c2991d.

--- scripts/prepare-for-publish.js
+++ scripts/prepare-for-publish.js
@@ -5,29 +5,8 @@ import { fileURLToPath } from 'url'
 import execa from 'execa'
 import ora from 'ora'
 
-const insertUserPostinstall = (pkgJson) => {
-  pkgJson.scripts.postinstall = pkgJson.scripts['postinstall-pack']
-  delete pkgJson.scripts['postinstall-pack']
-}
-
-const installDeps = async () => {
-  let spinner = ora('Running `npm install --no-audit`').start()
-  await execa('npm', ['install', '--no-audit'])
-  spinner.succeed()
-
-  spinner = ora('Running `npm shrinkwrap`').start()
-  await execa('npm', ['shrinkwrap'])
-  spinner.succeed()
-}
-
-// NOTE: this must run before `cleanPackageJson`, otherwise the `build` script has been deleted
-const buildPackageForPublish = async () => {
-  const spinner = ora('Running `npm run build`').start()
-  await execa('npm', ['run', 'build'])
-  spinner.succeed()
-}
-
-const SCRIPTS_TO_KEEP_ON_PUBLISHED_PKG = new Set([
+// These scripts from package.json need to be preserved on publish
+const preserveScripts = new Set([
   'postinstall-pack',
   'postpack',
   'preinstall',
@@ -36,35 +15,42 @@ const SCRIPTS_TO_KEEP_ON_PUBLISHED_PKG = new Set([
   'prepublishOnly',
 ])
 
-const cleanPackageJson = (pkgJson) => {
-  delete pkgJson.devDependencies
-  delete pkgJson.config
+let spinner = ora({
+  spinner: 'star',
+  text: 'Patching package.json (removing devDependencies, scripts, etc)',
+}).start()
 
-  for (const scriptName in pkgJson.scripts) {
-    if (SCRIPTS_TO_KEEP_ON_PUBLISHED_PKG.has(scriptName)) continue
+const dir = dirname(fileURLToPath(import.meta.url))
+const packageJsonPath = join(dir, '../package.json')
 
-    delete pkgJson.scripts[scriptName]
-  }
+const pkgJson = JSON.parse(await readFile(packageJsonPath))
+
+delete pkgJson.devDependencies
+delete pkgJson.ava
+delete pkgJson.config
+
+for (const scriptName in pkgJson.scripts) {
+  if (preserveScripts.has(scriptName)) continue
+
+  delete pkgJson.scripts[scriptName]
 }
 
-const writeUpdatedPackageJson = async (pkgJson, packageJsonPath) => {
-  const spinner = ora('Patching package.json (removing devDependencies, scripts, etc').start()
-  await writeFile(packageJsonPath, JSON.stringify(pkgJson, null, 2))
-  spinner.succeed()
-}
+pkgJson.scripts.postinstall = pkgJson.scripts['postinstall-pack']
+delete pkgJson.scripts['postinstall-pack']
 
-const main = async () => {
-  const dir = dirname(fileURLToPath(import.meta.url))
-  const pkgJsonPath = join(dir, '../package.json')
-  const pkgJson = JSON.parse(await readFile(pkgJsonPath))
+await writeFile(packageJsonPath, JSON.stringify(pkgJson, null, 2))
+spinner.succeed()
 
-  await installDeps()
+spinner = ora({
+  spinner: 'star',
+  text: 'Running `npm install --no-audit`',
+}).start()
+await execa('npm', ['install', '--no-audit'])
+spinner.succeed()
 
-  await buildPackageForPublish()
-
-  insertUserPostinstall(pkgJson)
-  cleanPackageJson(pkgJson)
-  await writeUpdatedPackageJson(pkgJson, pkgJsonPath)
-}
-
-await main()
+spinner = ora({
+  spinner: 'star',
+  text: 'Running `npm shrinkwrap`',
+}).start()
+await execa('npm', ['shrinkwrap'])
+spinner.succeed()
