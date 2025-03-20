5d42ca0b55372525300ce3e252b8f384cab4e084
Caleb Barnes <caleb.barnes@netlify.com>
2025-03-19 21:16:27 -0700

    POC adding database init command / drizzle-kit as an external subcommand

--- src/commands/main.ts
+++ src/commands/main.ts
@@ -46,6 +46,7 @@ import { createSwitchCommand } from './switch/index.js'
 import { AddressInUseError } from './types.js'
 import { createUnlinkCommand } from './unlink/index.js'
 import { createWatchCommand } from './watch/index.js'
+import { createDatabaseCommand } from './database/database.js'
 
 const SUGGESTION_TIMEOUT = 1e4
 
@@ -240,6 +241,7 @@ export const createMainCommand = (): BaseCommand => {
   createUnlinkCommand(program)
   createWatchCommand(program)
   createLogsCommand(program)
+  createDatabaseCommand(program)
 
   program.setAnalyticsPayload({ didEnableCompileCache })
 
