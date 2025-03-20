3b687f26fbf4ed2c919b6c933233a3e41be3711f
v1rtl <hi@v1rtl.site>
2025-03-21 01:09:13 +0200

    chore: fmt

--- src/utils/is-stream.ts
+++ src/utils/is-stream.ts
@@ -1,5 +1,4 @@
-import type { Stream } from "node:stream";
+import type { Stream } from 'node:stream'
 
-export const isStream = (stream: unknown): stream is Stream => stream !== null
-		&& typeof stream === 'object'
-		&& typeof (stream as Stream).pipe === 'function'
+export const isStream = (stream: unknown): stream is Stream =>
+  stream !== null && typeof stream === 'object' && typeof (stream as Stream).pipe === 'function'
