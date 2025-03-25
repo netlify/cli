netlify/cli

commit 32ee8c0186a7d5ec5cd65f756ae76a6f69fe6430

fix(deps): replace `ora` and `log-symbols` with tiny dependency `picospinner` (#7026)
* fix(deps): replace ora and log-symbols with tiny dep

This should reduce our package size.

I also added some missing types along the way.

* fix(deps): use nanospinner instead

src/utils/shell.ts
@@ -4,6 +4,7 @@ import execa from 'execa'
 // @ts-expect-error TS(7016) FIXME: Could not find a declaration file for module 'stri... Remove this comment to see the full error message
 import stripAnsiCc from 'strip-ansi-control-characters'
 
+import type { Spinner } from '../lib/spinner.js'
 import { chalk, log, NETLIFYDEVERR, NETLIFYDEVWARN } from './command-helpers.js'
 import { processOnExit } from './dev.js'
 
@@ -33,19 +34,15 @@ const cleanupBeforeExit = async ({ exitCode }) => {
   }
 }
 
-/**
- * Run a command and pipe stdout, stderr and stdin
- * @param {string} command
- * @param {object} options
- * @param {import('ora').Ora|null} [options.spinner]
- * @param {NodeJS.ProcessEnv} [options.env]
- * @param {string} [options.cwd]
- * @returns {execa.ExecaChildProcess<string>}
- */
-// @ts-expect-error TS(7006) FIXME: Parameter 'command' implicitly has an 'any' type.
-export const runCommand = (command, options = {}) => {
-  // @ts-expect-error TS(2339) FIXME: Property 'cwd' does not exist on type '{}'.
-  const { cwd, env = {}, spinner = null } = options
+export const runCommand = (
+  command: string,
+  options: {
+    spinner?: Spinner
+    env?: NodeJS.ProcessEnv
+    cwd: string
+  },
+) => {
+  const { cwd, env = {}, spinner } = options
   const commandProcess = execa.command(command, {
     preferLocal: true,
     // we use reject=false to avoid rejecting synchronously when the command doesn't exist
@@ -62,16 +59,13 @@ export const runCommand = (command, options = {}) => {
 
   // This ensures that an active spinner stays at the bottom of the commandline
   // even though the actual framework command might be outputting stuff
-  // @ts-expect-error TS(7006) FIXME: Parameter 'writeStream' implicitly has an 'any' ty... Remove this comment to see the full error message
-  const pipeDataWithSpinner = (writeStream, chunk) => {
-    if (spinner && spinner.isSpinning) {
+  const pipeDataWithSpinner = (writeStream: NodeJS.WriteStream, chunk: any) => {
+    if (spinner?.isSpinning) {
       spinner.clear()
-      spinner.isSilent = true
     }
     writeStream.write(chunk, () => {
-      if (spinner && spinner.isSpinning) {
-        spinner.isSilent = false
-        spinner.render()
+      if (spinner?.isSpinning) {
+        spinner.start()
       }
     })
   }

scripts/prepare-for-publish.js
@@ -3,7 +3,7 @@ import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
 import execa from 'execa'
-import ora from 'ora'
+import { createSpinner } from 'nanospinner'
 
 // These scripts from package.json need to be preserved on publish
 const preserveScripts = new Set([
@@ -15,10 +15,7 @@ const preserveScripts = new Set([
   'prepublishOnly',
 ])
 
-let spinner = ora({
-  spinner: 'star',
-  text: 'Patching package.json (removing devDependencies, scripts, etc)',
-}).start()
+let spinner = createSpinner('Patching package.json (removing devDependencies, scripts, etc)').start()
 
 const dir = dirname(fileURLToPath(import.meta.url))
 const packageJsonPath = join(dir, '../package.json')
@@ -41,18 +38,12 @@ pkgJson.scripts.postinstall = pkgJson.scripts['postinstall-pack']
 delete pkgJson.scripts['postinstall-pack']
 
 await writeFile(packageJsonPath, JSON.stringify(pkgJson, null, 2))
-spinner.succeed()
+spinner.success()
 
-spinner = ora({
-  spinner: 'star',
-  text: 'Running `npm install --no-audit`',
-}).start()
+spinner = createSpinner('Running `npm install --no-audit`').start()
 await execa('npm', ['install', '--no-audit'])
-spinner.succeed()
+spinner.success()
 
-spinner = ora({
-  spinner: 'star',
-  text: 'Running `npm shrinkwrap`',
-}).start()
+spinner = createSpinner('Running `npm shrinkwrap`').start()
 await execa('npm', ['shrinkwrap'])
-spinner.succeed()
+spinner.success()

src/commands/deploy/deploy.ts
@@ -17,7 +17,7 @@ import { getBootstrapURL } from '../../lib/edge-functions/bootstrap.js'
 import { featureFlags as edgeFunctionsFeatureFlags } from '../../lib/edge-functions/consts.js'
 import { normalizeFunctionsConfig } from '../../lib/functions/config.js'
 import { BACKGROUND_FUNCTIONS_WARNING } from '../../lib/log.js'
-import { startSpinner, stopSpinner } from '../../lib/spinner.js'
+import { type Spinner, startSpinner, stopSpinner } from '../../lib/spinner.js'
 import { detectFrameworkSettings, getDefaultConfig } from '../../utils/build-info.js'
 import {
   NETLIFYDEV,
@@ -33,7 +33,7 @@ import {
   APIError,
 } from '../../utils/command-helpers.js'
 import { DEFAULT_DEPLOY_TIMEOUT } from '../../utils/deploy/constants.js'
-import { deploySite } from '../../utils/deploy/deploy-site.js'
+import { type DeployEvent, deploySite } from '../../utils/deploy/deploy-site.js'
 import { getEnvelopeEnv } from '../../utils/env/index.js'
 import { getFunctionsManifestPath, getInternalFunctionsDir } from '../../utils/functions/index.js'
 import openBrowser from '../../utils/open-browser.js'
@@ -309,40 +309,30 @@ const reportDeployError = ({ error_, failAndExit }) => {
 }
 
 const deployProgressCb = function () {
-  /**
-   * @type {Record<string, import('ora').Ora>}
-   */
-  const events = {}
-  // @ts-expect-error TS(7006) FIXME: Parameter 'event' implicitly has an 'any' type.
-  return (event) => {
+  const spinnersByType: Record<DeployEvent['type'], Spinner> = {}
+  return (event: DeployEvent) => {
     switch (event.phase) {
       case 'start': {
-        // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        events[event.type] = startSpinner({
+        spinnersByType[event.type] = startSpinner({
           text: event.msg,
         })
         return
       }
       case 'progress': {
-        // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        const spinner = events[event.type]
+        const spinner = spinnersByType[event.type]
         if (spinner) {
-          spinner.text = event.msg
+          spinner.update({ text: event.msg })
         }
         return
       }
       case 'error':
-        // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        stopSpinner({ error: true, spinner: events[event.type], text: event.msg })
-        // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        delete events[event.type]
+        stopSpinner({ error: true, spinner: spinnersByType[event.type], text: event.msg })
+        delete spinnersByType[event.type]
         return
       case 'stop':
       default: {
-        // @ts-expect-error TS(2345) FIXME: Argument of type '{ spinner: any; text: any; }' is... Remove this comment to see the full error message
-        stopSpinner({ spinner: events[event.type], text: event.msg })
-        // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        delete events[event.type]
+        stopSpinner({ spinner: spinnersByType[event.type], text: event.msg })
+        delete spinnersByType[event.type]
       }
     }
   }

src/commands/functions/functions-create.ts
@@ -11,7 +11,7 @@ import { findUp } from 'find-up'
 import fuzzy from 'fuzzy'
 import inquirer from 'inquirer'
 import fetch from 'node-fetch'
-import ora from 'ora'
+import { createSpinner } from 'nanospinner'
 
 import { fileExistsAsync } from '../../lib/fs.js'
 import { getAddons, getCurrentAddon, getSiteData } from '../../utils/addons/prepare.js'
@@ -44,6 +44,11 @@ const languages = [
   { name: 'Go', value: 'go' },
   { name: 'Rust', value: 'rust' },
 ]
+
+const MOON_SPINNER = {
+  interval: 80,
+  frames: ['🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 '],
+}
 
 /**
  * prompt for a name if name not supplied
@@ -541,12 +546,9 @@ const scaffoldFromTemplate = async function (command, options, argumentName, fun
 
     // npm install
     if (functionPackageJson !== undefined) {
-      const spinner = ora({
-        text: `Installing dependencies for ${name}`,
-        spinner: 'moon',
-      }).start()
+      const spinner = createSpinner(`Installing dependencies for ${name}`, MOON_SPINNER).start()
       await installDeps({ functionPackageJson, functionPath, functionsDir })
-      spinner.succeed(`Installed dependencies for ${name}`)
+      spinner.success(`Installed dependencies for ${name}`)
     }
 
     if (funcType === 'edge') {

src/commands/sites/sites-list.ts
@@ -8,7 +8,6 @@ import BaseCommand from '../base-command.js'
 
 export const sitesList = async (options: OptionValues, command: BaseCommand) => {
   const { api } = command.netlify
-  /** @type {import('ora').Ora} */
   let spinner
   if (!options.json) {
     spinner = startSpinner({ text: 'Loading your sites' })
@@ -16,8 +15,7 @@ export const sitesList = async (options: OptionValues, command: BaseCommand) => 
   await command.authenticate()
 
   const sites = await listSites({ api, options: { filter: 'all' } })
-  if (!options.json) {
-    // @ts-expect-error TS(2345) FIXME: Argument of type '{ spinner: Ora | undefined; }' i... Remove this comment to see the full error message
+  if (spinner) {
     stopSpinner({ spinner })
   }
 

src/commands/watch/watch.ts
@@ -1,8 +1,9 @@
 import { OptionValues } from 'commander'
 import pWaitFor from 'p-wait-for'
 import prettyjson from 'prettyjson'
+import type { NetlifyAPI } from 'netlify'
 
-import { startSpinner, stopSpinner } from '../../lib/spinner.js'
+import { type Spinner, startSpinner, stopSpinner } from '../../lib/spinner.js'
 import { chalk, error, log } from '../../utils/command-helpers.js'
 import BaseCommand from '../base-command.js'
 import { init } from '../init/init.js'
@@ -15,28 +16,18 @@ const BUILD_FINISH_INTERVAL = 1e3
 // 20 minutes
 const BUILD_FINISH_TIMEOUT = 12e5
 
-/**
- *
- * @param {import('netlify').NetlifyAPI} api
- * @param {string} siteId
- * @param {import('ora').Ora} spinner
- * @returns {Promise<boolean>}
- */
-// @ts-expect-error TS(7006) FIXME: Parameter 'api' implicitly has an 'any' type.
-const waitForBuildFinish = async function (api, siteId, spinner) {
+const waitForBuildFinish = async function (api: NetlifyAPI, siteId: string, spinner: Spinner) {
   let firstPass = true
 
   const waitForBuildToFinish = async function () {
     const builds = await api.listSiteBuilds({ siteId })
     // build.error
-    // @ts-expect-error TS(7006) FIXME: Parameter 'build' implicitly has an 'any' type.
     const currentBuilds = builds.filter((build) => !build.done)
 
     // if build.error
     // @TODO implement build error messages into this
 
     if (!currentBuilds || currentBuilds.length === 0) {
-      // @ts-expect-error TS(2345) FIXME: Argument of type '{ spinner: any; }' is not assign... Remove this comment to see the full error message
       stopSpinner({ spinner })
       return true
     }
@@ -64,7 +55,7 @@ export const watch = async (options: OptionValues, command: BaseCommand) => {
   if (!siteId) {
     // TODO: build init command
     const siteData = await init({}, command)
-    siteId = siteData.id
+    siteId = siteData.id as string
   }
 
   // wait for 1 sec for everything to kickoff
@@ -101,7 +92,7 @@ export const watch = async (options: OptionValues, command: BaseCommand) => {
 
     const noActiveBuilds = await waitForBuildFinish(client, siteId, spinner)
 
-    const siteData = await client.getSite({ siteId: siteId as string })
+    const siteData = await client.getSite({ siteId })
 
     const message = chalk.cyanBright.bold.underline(noActiveBuilds ? 'Last build' : 'Deploy complete')
     log()

src/lib/api.ts
@@ -16,7 +16,7 @@ const MAX_PAGES = 10
 const MAX_PER_PAGE = 100
 
 // @ts-expect-error TS(7023) FIXME: 'listSites' implicitly has return type 'any' becau... Remove this comment to see the full error message
-export const listSites = async ({ api, options }): SiteInfo[] => {
+export const listSites = async ({ api, options }): Promise<SiteInfo[]> => {
   const { maxPages = MAX_PAGES, page = FIRST_PAGE, ...rest } = options
   const sites = await api.listSites({ page, per_page: MAX_PER_PAGE, ...rest })
   // TODO: use pagination headers when js-client returns them

src/lib/edge-functions/proxy.ts
@@ -13,7 +13,7 @@ import { FeatureFlags, getFeatureFlagsFromSiteInfo } from '../../utils/feature-f
 import { BlobsContextWithEdgeAccess } from '../blobs/blobs.js'
 import { getGeoLocation } from '../geo-location.js'
 import { getPathInProject } from '../settings.js'
-import { startSpinner, stopSpinner } from '../spinner.js'
+import { type Spinner, startSpinner, stopSpinner } from '../spinner.js'
 
 import { getBootstrapURL } from './bootstrap.js'
 import { DIST_IMPORT_MAP_PATH, EDGE_FUNCTIONS_SERVE_FOLDER } from './consts.js'
@@ -25,7 +25,7 @@ const headersSymbol = Symbol('Edge Functions Headers')
 const LOCAL_HOST = '127.0.0.1'
 
 const getDownloadUpdateFunctions = () => {
-  let spinner: ReturnType<typeof startSpinner>
+  let spinner: Spinner
 
   const onAfterDownload = (error_: unknown) => {
     stopSpinner({ error: Boolean(error_), spinner })

commit 4adc5b7a8d93962ccc10fa461e9b73a420693048

fix: avoid starting and stopping spinner loops
This was a small regression introduced in https://github.com/netlify/cli/pull/7026.

I didn't quite map the previous lib's API to nanospinner's correctly.

The intent here is to manually control each frame render and each clear. Instead, the
spinner here was already started before it's passed in to this method, then it was cleared
and started again on each chunk - but `start()` here would start a new interval loop and
set up new node.js exit listeners but `clear()` wouldn't clear them (`reset()` does that).

This was leading to unnecessary resource utilization and printing warnings like this:

```
MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 SIGINT listeners added to [process]. MaxListeners is 10. Use emitter.setMaxListeners() to increase limit
(Use node --trace-warnings ... to show where the warning was created)
(node:61086) MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 11 SIGTERM listeners added to [process]. MaxListeners is 10. Use emitter.setMaxListeners() to increase limit
```

The fix here is to just manually render a single frame (`.spin()`) at a time (and `stop()`
the initial loop).

src/utils/shell.ts
@@ -4,7 +4,7 @@ import execa from 'execa'
 // @ts-expect-error TS(7016) FIXME: Could not find a declaration file for module 'stri... Remove this comment to see the full error message
 import stripAnsiCc from 'strip-ansi-control-characters'
 
-import type { Spinner } from '../lib/spinner.js'
+import { stopSpinner, type Spinner } from '../lib/spinner.js'
 import { chalk, log, NETLIFYDEVERR, NETLIFYDEVWARN } from './command-helpers.js'
 import { processOnExit } from './dev.js'
 
@@ -57,16 +57,19 @@ export const runCommand = (
     cwd,
   })
 
-  // This ensures that an active spinner stays at the bottom of the commandline
+  // Ensure that an active spinner stays at the bottom of the commandline
   // even though the actual framework command might be outputting stuff
+  if (spinner?.isSpinning) {
+    // The spinner is initially "started" in the usual sense (rendering frames on an interval).
+    // In this case, we want to manually control when to clear and when to render a frame, so we turn this off.
+    stopSpinner({ error: false, spinner })
+  }
   const pipeDataWithSpinner = (writeStream: NodeJS.WriteStream, chunk: any) => {
     if (spinner?.isSpinning) {
       spinner.clear()
     }
     writeStream.write(chunk, () => {
-      if (spinner?.isSpinning) {
-        spinner.start()
-      }
+      spinner?.spin()
     })
   }
 