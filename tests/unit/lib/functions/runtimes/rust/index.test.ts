b33a8154330af0d21986d35e0a5447601caf51be
Philippe Serhal <philippe.serhal@netlify.com>
2025-04-02 17:15:08 -0400

    refactor: port go/rust unit tests to TS

---
+++ tests/unit/lib/functions/runtimes/rust/index.test.ts
@@ -0,0 +1,20 @@
+import { expect, test, vi } from 'vitest'
+
+import { runFunctionsProxy } from '../../../../../../dist/lib/functions/local-proxy.js'
+import { invokeFunction } from '../../../../../../dist/lib/functions/runtimes/rust/index.js'
+
+vi.mock('../../../../../../dist/lib/functions/local-proxy.js', () => ({ runFunctionsProxy: vi.fn() }))
+
+test.each([
+  ['body', 'thebody'] as const,
+  ['headers', { 'X-Single': 'A' }] as const,
+  ['multiValueHeaders', { 'X-Multi': ['B', 'C'] }] as const,
+  ['statusCode', 200] as const,
+])('should return %s', async (prop, expected) => {
+  // @ts-expect-error -- TODO(serhalp) Lazy test type. Create a factory and use it here.
+  vi.mocked(runFunctionsProxy).mockResolvedValue({ stdout: JSON.stringify({ [prop]: expected }) })
+
+  // @ts-expect-error -- TODO(serhalp) Lazy test type. Create a factory and use it here.
+  const match = await invokeFunction({ func: { mainFile: '', buildData: { binaryPath: 'foo' } } })
+  expect(match[prop]).toEqual(expected)
+})
