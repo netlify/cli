4877253589e6a17134ea4254140160bd9ec761c7
Philippe Serhal <philippe.serhal@netlify.com>
2025-04-03 17:41:20 -0400

    refactor: port functions-serve test file to TS

--- tests/integration/commands/functions-serve/functions-serve.test.ts
+++ tests/integration/commands/functions-serve/functions-serve.test.ts
@@ -1,3 +1,4 @@
+import dedent from 'dedent'
 import execa from 'execa'
 import getPort from 'get-port'
 import fetch from 'node-fetch'
@@ -6,20 +7,36 @@ import waitPort from 'wait-port'
 
 import { cliPath } from '../../utils/cli-path.js'
 import { killProcess } from '../../utils/process.js'
-import { withSiteBuilder } from '../../utils/site-builder.ts'
+import { type SiteBuilder, withSiteBuilder } from '../../utils/site-builder.js'
+import { InvokeFunctionResult } from '../../../../src/lib/functions/runtimes/index.js'
 
 const DEFAULT_PORT = 9999
 const SERVE_TIMEOUT = 180_000
 
-const withFunctionsServer = async ({ args = [], builder, port = DEFAULT_PORT }, testHandler) => {
+const withFunctionsServer = async (
+  {
+    args = [],
+    builder,
+    port = DEFAULT_PORT,
+  }: {
+    args?: string[]
+    builder: SiteBuilder
+    port?: number
+  },
+  testHandler: () => Promise<unknown>,
+) => {
   let ps
   try {
     ps = execa(cliPath, ['functions:serve', ...args], {
       cwd: builder.directory,
     })
 
-    ps.stdout.on('data', (data) => console.log(data.toString()))
-    ps.stderr.on('data', (data) => console.log(data.toString()))
+    ps.stdout?.on('data', (data: Buffer) => {
+      console.log(data.toString())
+    })
+    ps.stderr?.on('data', (data: Buffer) => {
+      console.log(data.toString())
+    })
 
     const { open } = await waitPort({
       port,
@@ -42,10 +59,10 @@ describe.concurrent('functions:serve command', () => {
         .withNetlifyToml({ config: { functions: { directory: 'functions' } } })
         .withFunction({
           path: 'ping.js',
-          handler: async () => ({
+          handler: dedent/* js */ `async () => ({
             statusCode: 200,
             body: 'ping',
-          }),
+          })`,
         })
         .build()
 
@@ -62,16 +79,16 @@ describe.concurrent('functions:serve command', () => {
         .withNetlifyToml({ config: { functions: { directory: 'functions' } } })
         .withFunction({
           path: 'ping.js',
-          handler: async () => ({
+          handler: dedent/* js */ `async () => ({
             statusCode: 200,
             body: 'ping',
-          }),
+          })`,
         })
         .build()
 
       const port = await getPort()
-      await withFunctionsServer({ builder, args: ['--port', port], port }, async () => {
-        const response = await fetch(`http://localhost:${port}/.netlify/functions/ping`)
+      await withFunctionsServer({ builder, args: ['--port', port.toString()], port }, async () => {
+        const response = await fetch(`http://localhost:${port.toString()}/.netlify/functions/ping`)
         t.expect(await response.text()).toEqual('ping')
       })
     })
@@ -87,15 +104,15 @@ describe.concurrent('functions:serve command', () => {
         .withFunction({
           pathPrefix: 'other',
           path: 'ping.js',
-          handler: async () => ({
+          handler: dedent/* js */ `async () => ({
             statusCode: 200,
             body: 'ping',
-          }),
+          })`,
         })
         .build()
 
       await withFunctionsServer({ builder, port }, async () => {
-        const response = await fetch(`http://localhost:${port}/.netlify/functions/ping`)
+        const response = await fetch(`http://localhost:${port.toString()}/.netlify/functions/ping`)
         t.expect(await response.text()).toEqual('ping')
       })
     })
@@ -112,16 +129,17 @@ describe.concurrent('functions:serve command', () => {
         })
         .withFunction({
           path: 'echo-env.js',
-          handler: async () => ({
-            statusCode: 200,
-            body: `${process.env.MY_CONFIG}`,
-          }),
+          handler: () =>
+            Promise.resolve({
+              statusCode: 200,
+              body: process.env.MY_CONFIG,
+            }),
         })
         .build()
 
       const port = await getPort()
-      await withFunctionsServer({ builder, args: ['--port', port], port }, async () => {
-        const response = await fetch(`http://localhost:${port}/.netlify/functions/echo-env`)
+      await withFunctionsServer({ builder, args: ['--port', port.toString()], port }, async () => {
+        const response = await fetch(`http://localhost:${port.toString()}/.netlify/functions/echo-env`)
         t.expect(await response.text()).toEqual('FROM_CONFIG_FILE')
       })
     })
@@ -135,19 +153,20 @@ describe.concurrent('functions:serve command', () => {
         })
         .withFunction({
           path: 'echo-event.js',
-          handler: async (event) => ({
+          handler: dedent/* js */ `
+          async (event) => ({
             statusCode: 200,
             body: JSON.stringify(event),
-          }),
+          })`,
         })
         .build()
 
       const port = await getPort()
-      await withFunctionsServer({ builder, args: ['--port', port], port }, async () => {
-        const response = await fetch(`http://localhost:${port}/.netlify/functions/echo-event`, {
+      await withFunctionsServer({ builder, args: ['--port', port.toString()], port }, async () => {
+        const response = await fetch(`http://localhost:${port.toString()}/.netlify/functions/echo-event`, {
           headers: { 'content-type': 'application/json; charset=utf-8' },
         })
-        const jsonResponse = await response.json()
+        const jsonResponse = (await response.json()) as InvokeFunctionResult
         t.expect(jsonResponse.isBase64Encoded).toBe(false)
       })
     })
@@ -166,8 +185,8 @@ describe.concurrent('functions:serve command', () => {
         })
         .build()
 
-      await withFunctionsServer({ builder, args: ['--port', port], port }, async () => {
-        const response = await fetch(`http://localhost:${port}/ping`)
+      await withFunctionsServer({ builder, args: ['--port', port.toString()], port }, async () => {
+        const response = await fetch(`http://localhost:${port.toString()}/ping`)
         t.expect(await response.text()).toEqual('ping')
       })
     })
