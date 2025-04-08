d123077b232d83e7014599cbadc829e935f06245
Philippe Serhal <philippe.serhal@netlify.com>
2025-04-08 14:43:59 -0400

    perf: defer more imports in bin/run

--- bin/run.js
+++ bin/run.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 import { argv } from 'process'
 
-import updateNotifier from 'update-notifier'
-
-import getPackageJson from '../dist/utils/get-cli-package-json.js'
 import { maybeEnableCompileCache } from '../dist/utils/nodejs-compile-cache.js'
 
 // 12 hours
 const UPDATE_CHECK_INTERVAL = 432e5
 
 const main = async () => {
+  const { default: updateNotifier } = await import('update-notifier')
   const { createMainCommand } = await import('../dist/commands/main.js')
   const { logError } = await import('../dist/utils/command-helpers.js')
+  const { default: getPackageJson } = await import('../dist/utils/get-cli-package-json.js')
   const { runProgram } = await import('../dist/utils/run-program.js')
 
   const pkg = await getPackageJson()
