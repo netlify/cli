272f6586b5a394cf7c2a2890e23da3c9e085b610
Philippe Serhal <philippe.serhal@netlify.com>
2025-04-15 15:34:12 -0400

    Revert "fix(deps): remove `is-stream` micro-utility"
    This reverts commit 5d6dd8f6f8a06c760c0c4100690c3a15173bac0c.

--- src/utils/is-stream.ts
+++
@@ -1,4 +0,0 @@
-import type { Stream } from 'node:stream'
-
-export const isStream = (stream: unknown): stream is Stream =>
-  stream !== null && typeof stream === 'object' && typeof (stream as Stream).pipe === 'function'
