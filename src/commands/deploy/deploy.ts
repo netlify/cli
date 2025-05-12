6eecb4e3a233642e6ae1d49b676ea12878522729
Philippe Serhal <philippe.serhal@netlify.com>
2025-05-12 17:08:13 -0400

    fix(deploy): fix typo in post-deploy message (#7289)
    Fix missing space in message shown after non-prod deploy.
    
    Fixes https://github.com/netlify/cli/issues/7285.

--- src/commands/deploy/deploy.ts
+++ src/commands/deploy/deploy.ts
@@ -706,7 +706,7 @@ const printResults = ({
     if (!deployToProduction) {
       log()
       log('If everything looks good on your draft URL, deploy it to your main site URL with the --prod flag:')
-      log(chalk.cyanBright.bold(`netlify deploy${runBuildCommand ? '' : '--no-build'} --prod`))
+      log(chalk.cyanBright.bold(`netlify deploy${runBuildCommand ? '' : ' --no-build'} --prod`))
       log()
     }
   }
