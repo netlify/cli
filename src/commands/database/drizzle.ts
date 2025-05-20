255457f9975317628b3f7783c14b8fb017854e58
Caleb Barnes <caleb.barnes@netlify.com>
2025-05-20 13:53:14 -0700

    add comment to drizzle config boilerplate for context

--- src/commands/database/drizzle.ts
+++ src/commands/database/drizzle.ts
@@ -79,6 +79,10 @@ export default defineConfig({
         url: process.env.NETLIFY_DATABASE_URL!
     },
     schema: './db/schema.ts',
+    /**
+     * Never edit the migrations directly, only use drizzle.
+     * There are scripts in the package.json "db:generate" and "db:migrate" to handle this.
+     */
     out: './migrations'
 });`
 
