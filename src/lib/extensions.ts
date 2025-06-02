04373aede3c0e5f1bde08dee68812573c944871e
Sarah Etter <sarah.etter@netlify.com>
2025-06-02 18:35:04 -0400

    chore: better comment

--- src/lib/extensions.ts
+++ src/lib/extensions.ts
@@ -9,10 +9,11 @@ export const packagesThatNeedSites = ['@netlify/neon']
 export async function handleExtensionRequirements(options: OptionValues, command: BaseCommand) {
     const { project } = command
     const { site, siteInfo } = command.netlify
-    // Go through all the packages that need sites, see if they exist in the package.json
-    // and if they do then we have to do a site init or whatever
+
     const packageJson = await project.getPackageJSON()
     const dependencies = packageJson.dependencies ?? {}
+
+    // If we don't have a site, these extensions need one initialized
     const hasSiteData = Boolean(site.id ?? options.site) && !isEmpty(siteInfo)
     if (!hasSiteData) {
         for (const packageName of packagesThatNeedSites) {
