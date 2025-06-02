9a4e9e66953a6fc03ba667e8762382f7a057fb1b
Sarah Etter <sarah.etter@netlify.com>
2025-06-02 18:37:40 -0400

    chore: format

--- src/lib/extensions.ts
+++ src/lib/extensions.ts
@@ -1,27 +1,27 @@
-import { OptionValues } from "commander";
+import { OptionValues } from 'commander'
 import isEmpty from 'lodash/isEmpty.js'
-import BaseCommand from "../commands/base-command.js";
-import { init } from "../commands/init/init.js";
+import BaseCommand from '../commands/base-command.js'
+import { init } from '../commands/init/init.js'
 import { log } from '../utils/command-helpers.js'
 
 export const packagesThatNeedSites = ['@netlify/neon']
 
 export async function handleExtensionRequirements(options: OptionValues, command: BaseCommand) {
-    const { project } = command
-    const { site, siteInfo } = command.netlify
+  const { project } = command
+  const { site, siteInfo } = command.netlify
 
-    const packageJson = await project.getPackageJSON()
-    const dependencies = packageJson.dependencies ?? {}
+  const packageJson = await project.getPackageJSON()
+  const dependencies = packageJson.dependencies ?? {}
 
-    // If we don't have a site, these extensions need one initialized
-    const hasSiteData = Boolean(site.id ?? options.site) && !isEmpty(siteInfo)
-    if (!hasSiteData) {
-        for (const packageName of packagesThatNeedSites) {
-            if (dependencies[packageName]) {
-              log(`Found ${packageName} in package.json, initializing a site`)
-              await init(options, command)
-              return
-            }
-          }
+  // If we don't have a site, these extensions need one initialized
+  const hasSiteData = Boolean(site.id ?? options.site) && !isEmpty(siteInfo)
+  if (!hasSiteData) {
+    for (const packageName of packagesThatNeedSites) {
+      if (dependencies[packageName]) {
+        log(`Found ${packageName} in package.json, initializing a site`)
+        await init(options, command)
+        return
+      }
     }
+  }
 }
