144d8b163e58f45b31604f613a8b03a8d44508f8
Nathan Houle <nathan@nathanhoule.com>
2025-06-04 16:27:42 -0700

    Update src/commands/database/constants.ts

--- src/commands/database/constants.ts
+++ src/commands/database/constants.ts
@@ -1,3 +1,3 @@
 export const NEON_DATABASE_EXTENSION_SLUG = process.env.NEON_DATABASE_EXTENSION_SLUG ?? 'neon'
-export const JIGSAW_URL = process.env.JIGSAW_URL ?? 'https://api.netlifysdk.com'
+export const JIGSAW_URL = process.env.JIGSAW_URL ?? 'https://jigsaw.services-prod.nsvcs.net'
 export const NETLIFY_NEON_PACKAGE_NAME = '@netlify/neon'
