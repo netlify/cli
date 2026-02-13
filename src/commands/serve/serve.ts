d322dbb8a0d6ab1004525e7689a25e53a7a35e32
Nathan Houle <nathan@nathanhoule.com>
2026-02-13 15:10:32 -0800

    feat: add deploy-specific environment variables to `netlify dev`

--- src/commands/serve/serve.ts
+++ src/commands/serve/serve.ts
@@ -170,6 +170,7 @@ export const serve = async (options: OptionValues, command: BaseCommand) => {
     offline: options.offline,
     state,
     accountId,
+    deployEnvironment: [],
   })
 
   // Try to add `.netlify` to `.gitignore`.
@@ -209,6 +210,7 @@ export const serve = async (options: OptionValues, command: BaseCommand) => {
     siteInfo,
     state,
     accountId,
+    deployEnvironment: [],
   })
 
   if (devConfig.autoLaunch !== false) {
