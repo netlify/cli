bd20f1ca43e56e159e0830a5215430a0fb281076
Eduardo Bouças <mail@eduardoboucas.com>
2026-02-17 22:52:47 +0000

    chore: update deps

--- tests/integration/commands/dev/dev.programmatic-netlify-dev.test.ts
+++ tests/integration/commands/dev/dev.programmatic-netlify-dev.test.ts
@@ -10,7 +10,7 @@ describe('@netlify/dev integration', () => {
       builder
         .withPackageJson({
           packageJson: {
-            dependencies: { '@netlify/db': '0.1.0' },
+            dependencies: { '@netlify/db': '0.1.0', '@netlify/db-dev': '0.2.0' },
           },
         })
         .withCommand({ command: ['npm', 'install'] })
