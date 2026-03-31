682866281e4862fec2585d80620248fde9026b5b
Sean Roberts <sean.roberts@netlify.com>
2026-03-31 11:24:45 -0400

    fix: --filter is not needed

--- site/scripts/docs.js
+++ site/scripts/docs.js
@@ -45,7 +45,7 @@ const config = {
           md += formatDescription(stripAnsi(subCmd.description))
           md += formatUsage(subCmd.name, subCmd)
           md += formatArgs(subCmd.args)
-          md += formatFlags(subCmd.flags)
+          md += formatFlags(subCmd.flags, subCmd.name)
           md += commandExamples(subCmd.examples)
           md += `---\n`
         })
@@ -134,7 +134,13 @@ const formatFlags = function (cmdFlags, command) {
   if (!cmdFlags) {
     return ''
   }
-  const flagArray = Object.keys(cmdFlags)
+  let flagArray = Object.keys(cmdFlags)
+
+  // Exclude --filter flag for sites:search as it's not applicable
+  if (command === 'sites:search') {
+    flagArray = flagArray.filter(flag => flag !== 'filter')
+  }
+
   if (flagArray.length === 0) {
     return ''
   }
